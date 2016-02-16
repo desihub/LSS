5662c9f3707ccdebb61b135364794335f5484e9e
ladosamushia <colkhis@gmail.com>
2016-02-16 13:37:36 -0800

    LSScat now also includes objects without redshifts

--- FAtoCat.py
+++ FAtoCat.py
@@ -3,13 +3,17 @@ Lado Samushia: lado@phys.ksu.edu February 2016
 
 This script will take tiles files and a secret file resulting from a fiber
 assignment algorithm and will create a large scale structure catalogue
-containing all objects that have been assigned a fiber at least once.
+containing all objects. 
 
 Usage:
 FAtoCat.py $SCRATCH/tiles*.txt $SCRATCH/secret.txt catalogue.txt
 
 catalogue.txt is an ASCII file with columns:
-target_id, ra, dec, redshift, target_type
+target_id, ra, dec, redshift, target_type, observed
+
+observed is either 1 or 0 depending on whether the target was observed or not.
+Since at the moment the code is run on simulations the correct redshift is
+printed anyway.
 
 For the format of tiles*.txt and secret.txt see
 https://github.com/desihub/fiberassign/blob/master/doc/new_instructions.txt
@@ -24,22 +28,26 @@ tile_files = sys.argv[1:-2]
 fcat = open(catalogue_file,'w')
 fsec = open(secret_file,'r')
 
+ra = []
+dec = []
 red = []
 type = []
 
 for line in fsec:
     columns = line.split()
+    ra.append(columns[2])
+    dec.append(columns[3])
     red.append(columns[4])
     type.append(columns[5])
 fsec.close()
 
 tar_size = len(type)
-print(tar_size)
+print(tar_size, len(red), len(dec), len(ra))
 #Is the target already in the catalogue?
 duplicated = np.zeros(tar_size, dtype=bool)
 
 for file in tile_files:
-    print(file)
+    #print(file)
     with open(file,'r') as f:
         for line in f:
             columns = line.split()
@@ -53,7 +61,20 @@ for file in tile_files:
             if not(tt == 0 or tt == 1 or tt == 2 or tt == 3) or duplicated[id]:
                 continue
             duplicated[id] = True
-            ra = columns[-4]
-            dec = columns[-3]
-            fcat.write('%d %s %s %s %d\n'%(id, ra, dec, red[id], tt))
+            ra_id = columns[-4]
+            dec_id = columns[-3]
+            fcat.write('%d %s %s %s %d 1\n'%(id, ra_id, dec_id, red[id], tt))
+        
+#Targets that don't have observed redshifts
+for i in range(tar_size):
+#    print(i)
+    if duplicated[i] != True:
+#        print(i)
+#        print(duplicated[i])
+#        print(ra[i])
+#        print(dec[i])
+#        print(red[i])
+#        print(type[i])
+        fcat.write('%d %s %s %s %s 0\n'%(i, ra[i], dec[i], red[i], type[i]))
+
 fcat.close()
