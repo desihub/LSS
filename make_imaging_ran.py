79804d9749881d8e6b5932c1fdb75fe756d5f451
ladosamushia <colkhis@gmail.com>
2017-05-10 07:52:06 -0700

    randoms will have the same size as real catalogue (approximately)

--- make_imaging_ran.py
+++ make_imaging_ran.py
@@ -15,6 +15,7 @@ from astropy.table import Table
 import numpy as np
 import argparse
 from itertools import izip
+import os
 
 parser = argparse.ArgumentParser(description='Construct angular mask for targeting')
 parser.add_argument('in_file',help='Input file with bricks information')
@@ -22,12 +23,19 @@ parser.add_argument('out_file',help='Output file with ra and dec of random point
 parser.add_argument('Nran',help='How many random files?')
 args = parser.parse_args()
 
+if os.path.isfile(args.out_file) == True:
+    print("The random files with this name already exist. Can not overwrite.")
+    print("Choose an alternative name for the output random catalogue files.")
+    quit()
+
+
 # The columns are (current format):
 # Ngal, depth_r, gdepth_r, ebv, area, ramin, ramax, decmin, decmax
 bricks = np.load(args.in_file)
+area = bricks[4,:]
 
 # Compute angular number density 
-bricks[0,:] /= bricks[4,:]
+bricks[0,:] /= area
 # Convert radians to degrees
 bricks[5:9,:] = np.degrees(bricks[5:9,:])
 
@@ -43,6 +51,8 @@ print(MM)
 a_coeff = np.dot(CC[0,1:],np.linalg.inv(CC[1:,1:]))
 mean_par = np.outer(np.mean(bricks[1:4,:],axis=1),np.ones(67701))
 nave_th = MM[0] + np.dot(a_coeff, bricks[1:4,:] - mean_par)
+Nave_th = nave_th*area
+
 print("a_coeff",a_coeff)
 print("bricks",bricks[1:4,:])
 print("mean_par",mean_par)
@@ -52,12 +62,12 @@ for i in range(int(args.Nran)):
 # Generate random ra, dec inside the brick based on nave_th
     print("generating ra")
     ra = []
-    for (min,max,num) in izip(bricks[5,:],bricks[6,:],nave_th):
+    for (min,max,num) in izip(bricks[5,:],bricks[6,:],Nave_th):
         ra.extend(np.random.uniform(min,max,num).tolist())
 
     print("generating dec")
     dec = []
-    for (min,max,num) in izip(bricks[7,:],bricks[8,:],nave_th):
+    for (min,max,num) in izip(bricks[7,:],bricks[8,:],Nave_th):
         dec.extend(np.random.uniform(min,max,num).tolist())
 
     np.asarray(ra, dtype=np.float32)
