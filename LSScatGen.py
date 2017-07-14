98228d8445940791f973ceb92be519180497706a
ladosamushia <colkhis@gmail.com>
2017-07-14 06:24:15 -0700

    use targets.fits instead of mtl.fits

--- LSScatGen.py
+++ LSScatGen.py
@@ -3,7 +3,7 @@
 # file. 
 #
 # Current version has few columns and basically rewrites a zcat.fits file and a
-# mtl.fits file but this will change as the code evolves.
+# targ.fits file but this will change as the code evolves.
 #
 # Lado Samushia March, 2017 (colkhis@gmail.com)
 
@@ -28,15 +28,12 @@ from itertools import izip
 # Two input files, one output file
 # Input:
 # zcat -- file containing measured redshifts
-# mtl -- merged target list contains ra, dec
-# targets -- file containing original targeting information
-# (see http://desidatamodel.readthedocs.io/en/latest/DESI_TARGET/mtl.html for
-# the mtl file datamodel)
+# targ -- file containing original targeting information
 # Output:
 # lsscat -- the output LSS catalogue
 parser = argparse.ArgumentParser(description='Create a LSS catalogue.')
 parser.add_argument('zcat',help='input redshift catalogue')
-parser.add_argument('mtl',help='input merged target list file')
+parser.add_argument('targ',help='input merged target list file')
 parser.add_argument('lsscat',help='output LSS catalogue')
 args = parser.parse_args()
 
@@ -45,18 +42,18 @@ if os.path.isfile(args.lsscat) == True:
     print("Choose an alternative name for the output LSScat file.")
     quit()
 
-print("Extract information from mtl fits file")
-mtl_file = fits.open(args.mtl)
-mtl = mtl_file[1].data
+print("Extract information from targets fits file")
+targ_file = fits.open(args.targ)
+targ = targ_file[1].gata
 
-mtl_target_id = mtl.field('targetid')
-ra = mtl.field('ra')
-dec = mtl.field('dec')
+targ_target_id = targ.field('targetid')
+ra = targ.field('ra')
+dec = targ.field('dec')
 # desi_target 1 = LRG, 2 = ELG, 4 = QSO
-desi_target = mtl.field('desi_target')
-depth_r = mtl.field('depth_r')
-galdepth_r = mtl.field('galdepth_r')
-Ntarg = np.size(mtl_target_id)
+desi_target = targ.field('desi_target')
+depth_r = targ.field('depth_r')
+galdepth_r = targ.field('galdepth_r')
+Ntarg = np.size(targ_target_id)
 # The arrays below will be filled in from the zcat file
 z = np.zeros(Ntarg)
 z_err = np.zeros(Ntarg)
@@ -64,7 +61,7 @@ z_warn = np.zeros(Ntarg)
 stype = np.array(['000000' for item in range(Ntarg)])
 # Create a dictionary from target_id to position in array
 print("Create dictionary")
-tid_dict = dict(zip(mtl_target_id,range(Ntarg)))
+tid_dict = dict(zip(targ_target_id,range(Ntarg)))
 
 print("Extract information from zcat fits file")
 zcat_file = fits.open(args.zcat)
@@ -85,7 +82,7 @@ for ID, st, zz, zz_err, zz_warn in izip(zcat_target_id, spec_type, redshift, red
     z_warn[index] = zz_warn
 
 # Write information into LSS catalogue fits file
-lss = Table([mtl_target_id, ra, dec, desi_target, depth_r, galdepth_r, stype, z, z_err, z_warn], names=('targetid','ra','dec','desitarget','depthr','galdepthr','stype','z','z_err','z_warn'))
+lss = Table([targ_target_id, ra, dec, desi_target, depth_r, galdepth_r, stype, z, z_err, z_warn], names=('targetid','ra','dec','desitarget','depthr','galdepthr','stype','z','z_err','z_warn'))
 lss.meta['comment'] = 'DESI Large-Scale Structure Catalogue'
 lss.meta['date'] = date.today()
 lss.write(args.lsscat, format='fits')
