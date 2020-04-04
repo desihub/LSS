897b52114b86b4097bf3b4cae9e1d6656bc6f1dd
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-04-04 10:08:21 -0400

    Update fa4lsscat_run.py

--- Sandbox/e2ecat/fa4lsscat_run.py
+++ Sandbox/e2ecat/fa4lsscat_run.py
@@ -1,3 +1,16 @@
+'''
+will write out fiberassignment files for each tile with the FASSIGN, FTARGETS, FAVAIL HDUS
+these are what are required to determine the geometry of what fiberassign thinks could have been observed and also match to actual observations (though FASSIGN is not really necessary)
+targetf is file with all targets to be run through
+tile files and output directories are generated based on the information in the run directories
+'''                                
+
+e2ein = '/global/homes/m/mjwilson/desi/survey-validation/svdc-spring2020d-onepercent/' #Mike's most recent
+e2eout = '/project/projectdirs/desi/users/ajross/catalogs/e2eoneper/' #I will keep over-writing most recent here until there are outputs to really be tested
+
+
+#standard python
+import sys
 import os
 import shutil
 import unittest
@@ -5,9 +18,12 @@ from datetime import datetime
 import json
 import numpy as np
 import fitsio
-import desimodel
 import glob
 
+#import from catalog code
+from cattools import mke2etiles
+
+#import from fiberassign
 from fiberassign.targets import (TargetsAvailable)
 from fiberassign.utils import option_list, GlobalTimers
 from fiberassign.hardware import load_hardware
@@ -20,54 +36,49 @@ from fiberassign.targets import (TARGET_TYPE_SCIENCE, TARGET_TYPE_SKY,
 from fiberassign.assign import (Assignment, write_assignment_fits,
                                 write_assignment_ascii, merge_results,
                                 read_assignment_fits_tile)                                 
+
+#desimodel
+import desimodel
 import desimodel.io as dmio
 
-import sys
-import os
 
-run = int(sys.argv[1])
 
-e2ein = '/global/homes/m/mjwilson/desi/survey-validation/svdc-spring2020b-onepercent/'
-e2eout = '/project/projectdirs/desi/users/ajross/catalogs/e2eoneper/'
-randir = e2eout+'randoms/'+str(run)
-if os.path.isdir(randir):
-	pass
-else:	
-	os.mkdir(randir)
 
-fafls = glob.glob(e2ein+'run/quicksurvey/'+str(run)+'/fiberassign/*')
-hd = fitsio.read_header(fafls[0])
-dt = hd['FA_RUN']
-#runtime = datetime.strptime(dt,'%Y-%m-%dT%H:%M:%S')
+srun = int(sys.argv[1]) #starting run
+nrun = int(sys.argv[2]) #number of runs
 
 targetf = '/project/projectdirs/desi/users/ajross/catalogs/minisv2/random/random_mtl.fits' #2e8 randoms from dr8 randoms, with columns needs for fa
 
-#make the tile file for this run
-from cattools import mke2etiles
-mke2etiles(run)
-tilef = e2eout+'e2etiles_run'+str(run)+'.fits'
-
-# First get the starting focalplane from desimodel
-#fp, exclude, state, tmstr = dmio.load_focalplane(runtime) #not necessary because of date above
-
-
-'''
-will write out fiberassignment files for each tile with the FASSIGN, FTARGETS, FAVAIL HDUS
-these are what are required to determine the geometry of what fiberassign thinks could have been observed and also match to actual observations (though FASSIGN is not really necessary)
-targetf is file with all targets to be run through
-tilef lists the tiles to "assign"
-dirout is the directory where this all gets written out !make sure this is unique for every different target!
-'''                                
+#use fiberassign tools to read in randoms to be assigned
 tgs = Targets()
 load_target_file(tgs,targetf)
 print('loaded target file '+targetf)
 tree = TargetTree(tgs, 0.01)
-hw = load_hardware(rundate=dt)
-tiles = load_tiles(tiles_file=tilef)
-tgsavail = TargetsAvailable(hw, tgs, tiles, tree)
-favail = LocationsAvailable(tgsavail)
-del tree
-asgn = Assignment(tgs, tgsavail, favail)
-asgn.assign_unused(TARGET_TYPE_SCIENCE)
-write_assignment_fits(tiles, asgn, out_dir=randir, all_targets=True)
-print('wrote assignment files to '+randir)	
+
+
+for run in range(srun,srun+nrun):
+	#make the tile file for this run
+	mke2etiles(run)
+	tilef = e2eout+'e2etiles_run'+str(run)+'.fits'
+
+
+	randir = e2eout+'randoms/'+str(run)
+	if os.path.isdir(randir):
+		pass
+	else:	
+		os.mkdir(randir)
+
+	fafls = glob.glob(e2ein+'run/quicksurvey/'+str(run)+'/fiberassign/*')
+	hd = fitsio.read_header(fafls[0])
+	dt = hd['FA_RUN']
+
+	hw = load_hardware(rundate=dt)
+
+	tiles = load_tiles(tiles_file=tilef)
+	tgsavail = TargetsAvailable(hw, tgs, tiles, tree)
+	favail = LocationsAvailable(tgsavail)
+	del tree
+	asgn = Assignment(tgs, tgsavail, favail)
+	asgn.assign_unused(TARGET_TYPE_SCIENCE)
+	write_assignment_fits(tiles, asgn, out_dir=randir, all_targets=True)
+	print('wrote assignment files to '+randir)	
