74db8be6685d4ef6e8656921262a95d9b6c5875f
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-04-14 17:49:25 -0400

    Update mkminiSV0cat.py

--- Sandbox/miniSVcat/mkminiSV0cat.py
+++ Sandbox/miniSVcat/mkminiSV0cat.py
@@ -35,32 +35,19 @@ fadir = minisvdir+'targets/'+fatype
 
 #type = 'ELG'
 if type == 'LRG':
-	bit = 53
+	bit = 10
 	pr = 4000
 if type == 'QSO':
-	bit = 55
+	bit = 12
 	pr = 10000
 if type == 'ELG':
-	bit = 54
+	bit = 11
 	pr = 10000
 	
 #tile = 70005
 #night = '20200219'
 #night = '20200303'
-coaddir = '/global/cfs/cdirs/desi/spectro/redux/minisv2/tiles/'
-#elgandlrgbits = [1,5,6,7,8,9,11,12,13]
-
-if tile == 70004 and night == '20200219':
-	id4coord = '00051002' #this is the exposure ID for 70004 for the coordinates file for getting the actual hardware performance; hopefully not necessary in future
-if tile == 70003 and night == '20200219':
-	id4coord = '00051073' 
-if tile == 70005 and night == '20200219':
-	id4coord = '00051039' 
-
-#if tile == 70002 and night == '20200304':
-#	id4coord = '00053122'
-#if tile == 70005 and night == '20200303':
-#	id4coord = '00052978'
+coaddir = '/global/cfs/cdirs/desi/spectro/redux/daily/tiles/'
 
 
 #put data from different spectrographs together, one table for fibermap, other for z
@@ -85,20 +72,7 @@ for i in range(1,len(specs)):
 wloc = tf['FIBERSTATUS'] == 0
 print(str(len(tf[wloc])) + ' locations with FIBERSTATUS 0')
 
-#get hardware info, not needed for most nights anymore because of fibermap info, definitely needed for 20200219
-if night == '20200219':
-	cf = fitsio.read('/global/cfs/cdirs/desi/spectro/data/'+night+'/'+id4coord+'/coordinates-'+id4coord+'.fits')
-	cloc = cf['PETAL_LOC']*1000 + cf['DEVICE_LOC']
-	wpos = cf['FLAGS_EXP_2'] == 4
-	print('there were '+str(len(cloc[wpos]))+' positioners that could reach their targets on '+night )
-	wspec = np.isin(cf['PETAL_LOC'],specs)
-	wps = wpos & wspec
-	print(str(len(cloc[wps]))+' of these went to the working spectrographs ('+str(specs)+')' )
-	goodloc = cloc[wps]
-
-
-if night != '20200219':
-	goodloc = tf[wloc]['LOCATION']
+goodloc = tf[wloc]['LOCATION']
 
 
 pdict = dict(zip(tf['LOCATION'], tf['PRIORITY'])) #to be used later for randoms
