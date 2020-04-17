2acb436d47d73a2d7fb1c22800c3bd67096a029f
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-04-17 11:14:53 -0400

    Update e2etools.py

--- Sandbox/e2ecat/e2etools.py
+++ Sandbox/e2ecat/e2etools.py
@@ -100,9 +100,9 @@ def combran(srun=0,nrun=7,program='dark'):
 		#fguc = setdiff((fgun,fgu))
 		dids = np.isin(fgun['TARGETID'],fgo['TARGETID']) #get the rows with target IDs that were duplicates in the new file
 		didsc = np.isin(fgu['TARGETID'],fgun['TARGETID'][dids]) #get the row in the concatenated table that had dup IDs
-		aa = np.chararray(len(fgu['TILE']))#,unicode=True)
+		aa = np.chararray(len(fgu['TILE']),unicode=True)
 		aa[:] = '-'+str(tile)
-		fgu['TILE'][didsc] = np.char.add(fgu['TILE'][didsc],aa[didsc]) #add the tile info
+		fgu['TILE'][didsc] = np.core.defchararray.add(fgu['TILE'][didsc],aa[didsc]) #add the tile info
 		print(str(len(fgu))+' unique randoms')
 		#else:
 		#	print(str(tile)+' not observed in assigned epoch')	
