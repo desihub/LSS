3e3fc940a4b7e415f45ad49111cf333d6cbb08b7
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-04-24 11:13:14 -0400

    run everything from mkCat
    target_type controls sample hardcoded at begining
    logfile gets written
    new python for fiberassign tools

---
+++ Sandbox/e2ecat/fatools.py
@@ -0,0 +1,74 @@
+#standard python
+import sys
+import os
+import shutil
+import unittest
+from datetime import datetime
+import json
+import numpy as np
+import fitsio
+import glob
+
+
+#import from fiberassign
+from fiberassign.targets import (TargetsAvailable)
+from fiberassign.utils import option_list, GlobalTimers
+from fiberassign.hardware import load_hardware
+from fiberassign.tiles import load_tiles, Tiles
+from fiberassign.targets import (TARGET_TYPE_SCIENCE, TARGET_TYPE_SKY,
+                                 TARGET_TYPE_SUPPSKY,
+                                 TARGET_TYPE_STANDARD, TARGET_TYPE_SAFE,
+                                 Targets, TargetsAvailable, TargetTree,
+                                 LocationsAvailable, load_target_file)
+from fiberassign.assign import (Assignment, write_assignment_fits,
+                                write_assignment_ascii, merge_results,
+                                read_assignment_fits_tile)                                 
+
+#desimodel
+import desimodel
+import desimodel.io as dmio
+
+def mkfa(targetf,tiles,rd,fad,dt):
+	'''
+	make the fiberassign files needed
+	'''
+	#targetf = e2eout +program+'/randoms_mtl_cuttod.fits' #above file, cut to ~e2e area with significant padding
+	#random are only for DARK right now, need to work something out for BRIGHT and GRAY
+
+	#use fiberassign tools to read in randoms to be assigned
+	tgs = Targets()
+	load_target_file(tgs,targetf)
+	print('loaded target file '+targetf)
+	tree = TargetTree(tgs, 0.01)
+
+	for run in range(srun,srun+nrun):
+		#make the tile file for this run
+		#e2e.mke2etiles(run,program=program)
+		tilef = tiles+str(run)+'.fits'
+		#+str(run)+'.fits'
+
+
+		randir = rd +str(run)
+		#+str(run)
+		if os.path.isdir(randir):
+			ofls = glob.glob(randir)
+			for fl in ofls:
+				os.remove(fl) #remove the old files if we are rerunning
+		else:	
+			os.mkdir(randir)
+		
+		fafls = glob.glob(fad+str(run)+'/fiberassign/fiberassign*')
+		#+str(run)+'/fiberassign/fiberassign*')
+		hd = fitsio.read_header(fafls[0])
+		dt = hd['FA_RUN']
+
+		hw = load_hardware(rundate=dt)
+
+		tiles = load_tiles(tiles_file=tilef)
+		tgsavail = TargetsAvailable(hw, tgs, tiles, tree)
+		favail = LocationsAvailable(tgsavail)
+		#del tree
+		asgn = Assignment(tgs, tgsavail, favail)
+		asgn.assign_unused(TARGET_TYPE_SCIENCE)
+		write_assignment_fits(tiles, asgn, out_dir=randir, all_targets=True)
+		print('wrote assignment files to '+randir)	
---
+++ Sandbox/e2ecat/mkCat.py
@@ -0,0 +1,150 @@
+'''
+one executable to make any of the four kinds of tracer, just setting type
+target_type can be LRG, QSO, BGS, or ELG
+'''
+
+target_type = 'BGS'
+
+#standard python
+import sys
+import os
+import shutil
+import unittest
+from datetime import datetime
+import json
+import numpy as np
+import fitsio
+import glob
+
+
+
+#import from catalog code
+import e2etools as e2e
+import fatools as fa
+
+
+E2EDIR = '/global/homes/m/mjwilson/desi/survey-validation/svdc-spring2020f-onepercent/' # Most recent: f.
+#E2EDIR = os.environ['E2EDIR']
+print('end to end directory is')
+print(E2EDIR)
+
+#directories for inputs and output
+e2ein  = E2EDIR                      
+e2eout = E2EDIR + 'run/catalogs/'
+
+if os.path.isdir(E2EDIR + 'run/catalogs/logfiles'):
+	pass
+else:
+	os.mkdir(E2EDIR + 'run/catalogs/logfiles')	
+
+	
+
+logf = open(diro+'/logfiles/mkCat'+str(datetime.datetime.today())+'.log','w')
+logf.write('mkCat.py run for type '+str(type)+'\n')
+logf.write('inputs root '+e2ein+'\n')
+logf.write('output root '+e2ein+'\n')
+
+targroot  = '/project/projectdirs/desi/target/catalogs/dr8/0.31.1/targets/main/resolve/targets-dr8'
+ranf      = '/project/projectdirs/desi/target/catalogs/dr8/0.31.0/randomsall/randoms-inside-dr8-0.31.0-all.fits' #DR8 imaging randoms file
+
+#now the routines in e2etools that need these will have them as globals
+e2e.setglobals(e2ein,e2eout,targroot,ranf)
+
+elgandlrgbits = [1,5,6,7,8,9,11,12,13] #the combination of mask bits proposed for LRGs and ELGs, for simplicity using them again for quasars
+#run through steps to make LRG catalogs
+
+if target_type == 'BGS':
+	print('Need to account for "legacy" LRG redshifts')
+	type = 60 #target bit for BGS
+	program = 'bright'
+	#epochs
+	srun = 0
+	# number of epochs
+	nrun = 5
+	imbits =  elgandlrgbits #mask bits for imaging
+	logf.write('used following settings:\n')
+	logf.write("\
+	type = "+str(type)+"\n\
+	program = "+program+"\n\
+	srun = "+str(srun)+"\n\
+	nrun = "+str(nrun)+"\n\
+	imbits =  "+str(imbits)+"\n\
+	\n\
+	")
+
+
+#list of independent tasks to perform
+mkrandoms = False #make randoms specific for type/observing program
+farandoms = True #run randoms through fiberassign; doesn't need to be done if already done for LRGs
+combran = True #concatenate random files and match randoms from FAVAIL back to full info using targetID; doesn't need to be done if already done for LRGs
+matchran = True
+combtar = True #concatenate target files; doesn't need to be done if already done for LRGs 
+matchtar = True #match targets to mtl info and to zcat info; doesn't need to be done if already done for LRGs
+plotntile = True
+mkfullran = True #make "full" catalog for randoms
+mkfulldat = True #make "full" catalog for data
+mkclusdat = True #make "clustering" catalog for data
+mkclusran = True #make clustering catalog for randoms
+
+
+
+
+if mkrandoms:
+	e2e.mkran_type(type,program)
+	logf.write('ran mkrandoms\n')
+
+if farandoms:
+	for run in range(srun,srun+nrun):
+		#make the tile file for this run
+		e2e.mke2etiles(run,program=program)
+
+	targetf = e2eout +program+'/randoms_mtl_cuttod.fits' #above file, cut to ~e2e area with significant padding
+	tiles = e2eout+program+'/e2etiles_run'
+	rd = e2eout+program+'/randoms/'
+	fad = e2ein+'run/quicksurvey/'+program+'/'
+	fa.mkfa(targetf,tiles,rd,fad,dt)
+	logf.write('ran farandoms\n')
+		
+if combran: #concatenate random files, match to mtl
+	e2e.combran(srun,nrun,program)
+	logf.write('ran combran\n')
+
+if matchran:
+	e2e.matchran(program)
+	logf.write('ran matchran\n')
+ 
+if combtar: #concatenate target files, match to mtl and zcat info
+	e2e.combtargets(srun,nrun,program)
+	logf.write('ran combtargets\n')
+
+if matchtar:	
+	rmax=nrun-1
+	e2e.matchtar(program,rmax)
+	e2e.matchzcattar(program,rmax)
+	logf.write('ran matchtar\n')
+	
+if plotntile:
+	e2e.plotrntile(program)
+	e2e.plottntile(program)
+	e2e.plotzprobvsntile(program,type)
+	logf.write('ran plotntile\n')
+	
+if mkfullran:
+    e2e.mkfullran(target_type,program,imbits)
+    logf.write('ran mkfullran\n')
+
+if mkfulldat:
+    e2e.mkfulldat(target_type,program,imbits)
+    logf.write('ran mkfulldat\n')
+
+#needs to happen before randoms so randoms can get z and weights
+if mkclusdat:
+    e2e.mkclusdat(target_type,program)
+    logf.write('ran mkclusdat\n')
+
+if mkclusran:
+    e2e.mkclusran(target_type,program)
+    logf.write('ran mkclusran\n')
+
+   	
+		
