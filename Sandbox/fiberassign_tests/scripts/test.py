085027015656dc87080c0c74c6288fca34c8f43b
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-07-14 17:01:17 -0400

    Update test.py

--- Sandbox/fiberassign_tests/scripts/test.py
+++ Sandbox/fiberassign_tests/scripts/test.py
@@ -10,6 +10,9 @@ ra_max=190
 dec_min=-5
 dec_max=15
 batch_cadence=28
+if batch_cadence == 28:
+   foot ='footprint_month'
+   darkout = "test_dark_global_month"
 
 make_glob = False
 if make_glob:
@@ -28,7 +31,7 @@ else:
     global_DR8_truth_file_bright = os.path.join(output_path, 'global_DR8_truth_{}.fits'.format(program))
     global_DR8_sky_file = os.path.join(output_path, "global_DR8_sky.fits")
      
-make_targets = True
+make_targets = False
 if make_targets:
 
     patch_DR8_mtl_file_dark = mb.make_patch_file(global_DR8_mtl_file_dark,ra_min, ra_max, dec_min, dec_max)
@@ -38,13 +41,13 @@ if make_targets:
     patch_DR8_truth_file_bright = mb.make_patch_file(global_DR8_truth_file_bright,ra_min, ra_max, dec_min, dec_max)
 
 
-make_tiles = True
+make_tiles = False
 if make_tiles:
     
 
     # batches for the first year of the survey (all the footprint is available) with different cadences
-    n = mb.prepare_tile_batches(surveysim_file, output_path='footprint_month', program='dark', start_day=0, end_day=365, batch_cadence=batch_cadence) 
-    n = mb.prepare_tile_batches(surveysim_file, output_path='footprint_month', program='bright', start_day=0, end_day=365, batch_cadence=batch_cadence) 
+    n = mb.prepare_tile_batches(surveysim_file, output_path=foot, program='dark', start_day=0, end_day=365, batch_cadence=batch_cadence) 
+    n = mb.prepare_tile_batches(surveysim_file, output_path=foot, program='bright', start_day=0, end_day=365, batch_cadence=batch_cadence) 
 
     # batches for the whole duration of the survey, restricted to a small region on the sky.
     n = mb.prepare_tile_batches(surveysim_file, output_path='footprint_patch_month', program='dark', 
@@ -53,3 +56,8 @@ if make_tiles:
     n = mb.prepare_tile_batches(surveysim_file, output_path='footprint_patch_month', program='bright', 
                                  start_day=0, end_day=2000, batch_cadence=batch_cadence, select_subset_sky=True,
                                 ra_min=ra_min, ra_max=ra_max, dec_min=dec_min, dec_max=dec_max) 
+
+run_fa = True
+if run_fa:
+    mb.run_strategy(global_DR8_mtl_file_dark,  global_DR8_truth_file_dark , global_DR8_sky_file,
+             output_path=darkout, batch_path=foot, program="dark")
