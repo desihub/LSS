a269fe821c37de8d254a8aaa81491ab129f0a222
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-09-24 12:31:48 -0400

    Update densvar.py

--- Sandbox/imaging/densvar.py
+++ Sandbox/imaging/densvar.py
@@ -120,6 +120,45 @@ def plot_brickdens(type,reg=False,ff='targetDR9m42.fits',sz=.2,vx=2):
     plt.scatter(rap,np.sin(decp*np.pi/180),c=od,s=sz,vmax=vx)#,vmin=1.,vmax=2)
     plt.show()
 
+def plot_brickprop(type,prop,reg=False,ff='targetDR9m42.fits',sz=.2,vx=2):
+    brickf = fitsio.read('/global/cfs/cdirs/cosmo/work/legacysurvey/dr9m/survey-bricks.fits.gz') 
+    brickdictrd = {}
+    for i in range(0,len(brickf)):
+        brickdictrd[brickf[i]['BRICKID']] = (brickf[i]['RA'],brickf[i]['DEC'])
+    ft = fitsio.read(sdir+type+ff)
+    print(len(ft))
+    if reg:
+        wd = ft['PHOTSYS'] == reg
+        ft = ft[wd]
+    nbd = np.max(ft['BRICKID'])
+    nbx = nbd
+    print('maximum brickid is '+str(nbx))
+    pixlr = np.zeros(nbx)
+    pixlg = np.zeros(nbx)
+
+    for i in range(0,len(ft)):
+        id = ft[i]['BRICKID']
+        pixlr[id] += 1.
+        pixlg[id] += ft[prop]
+    wp = pixlr > 0
+    pixls = []
+    for i in range(0,len(pixlr)):
+        if pixlr[i] > 0:
+            pixls.append(i)
+    pixls = np.array(pixls).astype(int) 
+    rap = []
+    decp = []
+    for id in pixls:
+        rai,deci = brickdictrd[id]
+        rap.append(rai)
+        decp.append(deci)   
+    od = pixlg[wp]/pixlr[wp]
+    #od = od/np.mean(od)
+    decp = np.array(decp)
+    plt.scatter(rap,np.sin(decp*np.pi/180),c=od,s=sz,vmax=vx)#,vmin=1.,vmax=2)
+    plt.title(prop +' averaged in bricks')
+    plt.show()
+
 
 def densvsimpar_ran(type,par,reg=None,ff='targetDR9m42.fits',vmin=None,vmax=None,nbin=10):
     ft = fitsio.read(sdir+type+ff)
