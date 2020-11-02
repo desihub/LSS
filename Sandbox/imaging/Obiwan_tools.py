48cd4e5f080407d3bea0d65aa92ac3244dea30df
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-11-02 16:41:24 -0500

    Update Obiwan_tools.py

--- Sandbox/imaging/Obiwan_tools.py
+++ Sandbox/imaging/Obiwan_tools.py
@@ -6,11 +6,14 @@ truthf = '/project/projectdirs/desi/users/ajross/MCdata/seed.fits'
 
 ranf = '/global/cscratch1/sd/adamyers/dr9m-sep26-2020/0.42.0/randoms/resolve/randoms-1-0.fits'
 #file has 63283969 entries
+
+#obiout = os.environ['obiwan_out'] #get this setup at some point
+
 outdir = '/global/cscratch1/sd/ajross/Obiwan/dr9m/obiwan_out/test/divided_randoms/'
 
 def getran_brick(brick,maxg=25):
     #columns to consider from imaging randoms
-    kr = ['RA','DEC','TARGETID'] #minimum number needed for now
+    kr = ['RA','DEC','TARGETID','BRICKNAME'] #minimum number needed for now
     #kr = ['RELEASE','BRICKID','BRICKNAME','RA','DEC','NOBS_G','NOBS_R','NOBS_Z','PSFDEPTH_G','PSFDEPTH_R','PSFDEPTH_Z','GALDEPTH_G','GALDEPTH_R',\
  #'GALDEPTH_Z','PSFDEPTH_W1','PSFDEPTH_W2','PSFSIZE_G','PSFSIZE_R','PSFSIZE_Z','APFLUX_G','APFLUX_R','APFLUX_Z','APFLUX_IVAR_G','APFLUX_IVAR_R',\
  #'APFLUX_IVAR_Z','MASKBITS','WISEMASK_W1','WISEMASK_W2','EBV','PHOTSYS','TARGETID','HPXPIXEL']
