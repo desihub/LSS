d95098a8dc942d8495e0313ddfa8f525926bbb7c
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2020-11-23 11:35:17 -0500

    Update select_samples.py

--- Sandbox/imaging/select_samples.py
+++ Sandbox/imaging/select_samples.py
@@ -19,7 +19,8 @@ def gather_targets(type,fo='targetDR9m44.fits',prog='dark'):
 	#just concatenate all of the targets for a given type, keeping only the columns quoted below
 	print(targroot+prog)
 	fns = glob.glob(targroot+prog+'/*.fits')
-	print('data is split into '+str(len(fns))+' healpix files')
+	ncat     = len(fns)
+	print('data is split into '+str(ncat)+' healpix files')
 	keys = ['RA', 'DEC', 'BRICKID', 'BRICKNAME','MORPHTYPE','DCHISQ','FLUX_G', 'FLUX_R', 'FLUX_Z','MW_TRANSMISSION_G', 'MW_TRANSMISSION_R', 'MW_TRANSMISSION_Z','FLUX_IVAR_G', 'FLUX_IVAR_R', 'FLUX_IVAR_Z','NOBS_G', 'NOBS_R', 'NOBS_Z','PSFDEPTH_G', 'PSFDEPTH_R', 'PSFDEPTH_Z', 'GALDEPTH_G', 'GALDEPTH_R',\
 	   'GALDEPTH_Z','FIBERFLUX_G', 'FIBERFLUX_R', 'FIBERFLUX_Z', 'FIBERTOTFLUX_G', 'FIBERTOTFLUX_R', 'FIBERTOTFLUX_Z',\
 	   'MASKBITS', 'EBV', 'PHOTSYS','TARGETID','DESI_TARGET']
@@ -45,7 +46,7 @@ def gather_targets(type,fo='targetDR9m44.fits',prog='dark'):
 	print('wrote to '+outf)
 	del data
 # 	put information together, takes a couple of minutes
-# 	ncat     = len(fns)
+# 	
 # 	mydict   = {}
 # 	for key in keys:
 # 		mydict[key] = []
