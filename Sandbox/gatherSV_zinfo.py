e6cf6b01cb294ae907283c5fe004faca1a93d1ab
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-01-11 15:04:50 -0500

    Update gatherSV_zinfo.py

--- Sandbox/gatherSV_zinfo.py
+++ Sandbox/gatherSV_zinfo.py
@@ -23,6 +23,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--type", help="tracer type to be selected")
 parser.add_argument("--tile", help="observed tile to use") #eventually remove this and just gather everything
 
+type = args.type
+tile = args.tile
+
+
 if type == 'LRG':
     tarbit = 0 #targeting bit
 if type == 'QSO':
