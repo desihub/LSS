760bfb2a94fee4e41606fa4a29699a9d4cc61c06
ashleyjross <ashley.jacob.ross@gmail.com>
2021-01-25 09:45:17 -0500

    Update evalSR.py

--- Sandbox/evalSR.py
+++ Sandbox/evalSR.py
@@ -128,7 +128,7 @@ def effvsdepth(tf,type,depth='R_DEPTH',nbin=10,lplace=(.15,.15)):
     print('spectroscopic contamination is defined as the fraction of redshifts, within the target range and with no zwarn flag, that are further than 0.0033(1+z_truth) from z_truth (cutting to the target redshift range and to zwarn == 0 in both numerator and denominator)')
     
 
-def repeatvsdchi2(tf,type,nbin=100,range=(9,2000),mind=1000.):    
+def repeatvsdchi2(tf,type,nbin=100,rng=(9,2000),mind=1000.):    
     '''
     input table tf should be created in add_truth
     type should be one of the ones in the above dictionaries
@@ -149,7 +149,7 @@ def repeatvsdchi2(tf,type,nbin=100,range=(9,2000),mind=1000.):
     tzrsel = (tcomp['Z_TRUTH'] > zs[0]) & (tcomp['Z_TRUTH'] < zs[1])
     bzsel = zrsel & (abs(dz) > catthresh*(1+tcomp['Z_TRUTH']))
     ggzsel = zrsel & (abs(dz) < catthresh*(1+tcomp['Z_TRUTH']))
-    a = plt.hist(tcomp[zrsel]['DELTACHI2'],bins=nbin,cumulative=-1,range=range)
+    a = plt.hist(tcomp[zrsel]['DELTACHI2'],bins=nbin,cumulative=-1,range=rng)
     b = plt.hist(tcomp[ggzsel]['DELTACHI2'],bins=a[1],cumulative=-1)
     plt.clf()
     hv = []
