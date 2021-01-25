9de3e78bf98d0c4abddce4313658d164abf1b372
ashleyjross <ashley.jacob.ross@gmail.com>
2021-01-25 16:22:38 -0500

    Update combdeep.py

--- bin/combdeep.py
+++ bin/combdeep.py
@@ -59,14 +59,14 @@ dt.write(dirout+type+'alltiles_deep_clustering.dat.fits',overwrite=True,format='
 rm = 0
 rx = 10
 for i in range(rm,rx):
-    ffrs = glob.glob(dirout+type+'*_deep_'+str(i)+'_full.ran.fits'
+    ffrs = glob.glob(dirout+type+'*_deep_'+str(i)+'_full.ran.fits')
     dt = Table.read(fffs[0])
     for ii in range(1,len(ffrs)):
         dtn = Table.read(ffrs[ii])
         dt = vstack([dt,dtn])
     dt.write(dirout+type+'alltiles_deep_full.ran.fits',overwrite=True,format='fits')    
 
-    ffrs = glob.glob(dirout+type+'*_deep_'+str(i)+'_clustering.ran.fits'
+    ffrs = glob.glob(dirout+type+'*_deep_'+str(i)+'_clustering.ran.fits')
     dt = Table.read(fffs[0])
     for ii in range(1,len(ffrs)):
         dtn = Table.read(ffrs[ii])
