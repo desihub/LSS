22dd860806d85514e63fefe5695718c062b0707e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-02-04 15:08:34 -0500

    Update zinfo.py

--- py/LSS/zcomp/zinfo.py
+++ py/LSS/zcomp/zinfo.py
@@ -55,7 +55,7 @@ def get_tsnrinfo(exps,spec):
         lsv = 0
         qsv = 0
         for band in bands:
-            cinfo = fitsio.read('/global/cscratch1/sd/mjwilson/desi/tsnr/summary_'+band+str(spec)+'.fits')
+            cinfo = fitsio.read('/global/cscratch1/sd/mjwilson/desi/tsnr/blanc/summary_'+band+str(spec)+'.fits')
             info = cinfo[cinfo['EXPID'] == '000'+str(exp)]    
             if len(info) == 0:
                 print('did not find infob for expid '+str(exp))
