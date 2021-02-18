1b93a4be204dd8c07bf8c602094f39731b653115
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-02-18 17:25:45 -0500

    Update densvar.py

--- py/LSS/imaging/densvar.py
+++ py/LSS/imaging/densvar.py
@@ -117,7 +117,7 @@ def obiELGvspar(reg,par,vmin=None,vmax=None,nbin=10,obidir='/global/cscratch1/sd
 
 
 class densvar:
-    def __init__(self,type,sdir='',tv='0.49.0',rel='DR9',ti=None,elgandlrgbits = [1,5,6,7,8,9,11,12,13],columns=['RA','DEC','PHOTSYS','NOBS_G','NOBS_R','NOBS_Z','MASKBITS','EBV','GALDEPTH_G','GALDEPTH_R','GALDEPTH_Z','BRICKID','MORPTHTYPE']):
+    def __init__(self,type,sdir='',tv='0.49.0',rel='DR9',ti=None,elgandlrgbits = [1,5,6,7,8,9,11,12,13],columns=['RA','DEC','PHOTSYS','NOBS_G','NOBS_R','NOBS_Z','MASKBITS','EBV','GALDEPTH_G','GALDEPTH_R','GALDEPTH_Z','BRICKID','MORPHTYPE']):
         df = sdir+type+'targets'+rel+'v'+tv+'.fits'
         ft = fitsio.read(df,columns=columns)
         print(len(ft))
