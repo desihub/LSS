e2d4c69dfcc59ee849adf36d560ef4579654d73b
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-02-19 16:03:07 -0500

    Update mkCat_tar4ang.py

--- bin/mkCat_tar4ang.py
+++ bin/mkCat_tar4ang.py
@@ -71,7 +71,7 @@ sfn = glob.glob(dirsweepn+'sweep*')
 
 elgandlrgbits = [1,5,6,7,8,9,11,12,13] #these get used to veto imaging area; combination of bits applied to ELGs and LRGs in DR8 targeting
 
-mkbsamp = False #make the base sample
+mkbsamp = True #make the base sample
 domaskd = True #mask data based on mask bits above
 domaskr = False #mask randoms
 
