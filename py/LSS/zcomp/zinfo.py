c55cf7b766a30f9fd6baaef2b6ee54609d5c518a
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-02-26 16:44:36 -0500

    Update zinfo.py

--- py/LSS/zcomp/zinfo.py
+++ py/LSS/zcomp/zinfo.py
@@ -171,7 +171,7 @@ def get_subset(tarbit,tp,night,tile,coaddir,exposures,mfn='temp.txt'):
                     for col in tcols:
                         cf.rename_column(col, col[:-2])
                     if ce ==0 and cam == 'b':
-                        tids = fistio.read(coaddir+'/'+night+'/cframe-'+cam+str(specs[0])+'-'+str(exp).zfill(8)+'.fits',ext='FIBERMAP',columns=['TARGETID'])
+                        tids = fitsio.read(coaddir+'/'+night+'/cframe-'+cam+str(specs[0])+'-'+str(exp).zfill(8)+'.fits',ext='FIBERMAP',columns=['TARGETID'])
                         tnsrt = cf.copy()
                         tnsrt['TARGETID'] = tids
                     else:
