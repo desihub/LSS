a6fbe6f0ae7de854180ac9b7beb14d537f64fe1f
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-03-03 15:00:47 -0500

    Update gatherSV_zinfo_alltiles_fromAll.py

--- bin/gatherSV_zinfo_alltiles_fromAll.py
+++ bin/gatherSV_zinfo_alltiles_fromAll.py
@@ -112,9 +112,11 @@ for tp in types:
             wz = ft['TILEID'] == tile
             
             tz = ft[wz]
+            print(len(tz))
             tt=Table.read(dirvi+tp[:3]+'/'+'desi-vi_'+tp[:3]+'_tile'+tile+'_nightdeep_merged_all_'+date+'.csv',format='pandas.csv')
             tt.keep_columns(['TARGETID','best_z','best_quality','best_spectype','all_VI_issues','all_VI_comments','merger_comment','N_VI'])
             tj = join(tz,tt,join_type='left',keys='TARGETID')
+            print(len(tj))
             tj['N_VI'].fill_value = 0
             tj['N_VI'] = tj['N_VI'].filled() #should easily be able to select rows with N_VI > 0 to get desired info
             #tj['TILEID'] = tile
