8f14e91ffd03ac88737ef07f899ab41109dee100
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-03-03 14:45:12 -0500

    Update gatherSV_zinfo_alltiles_fromAll.py

--- bin/gatherSV_zinfo_alltiles_fromAll.py
+++ bin/gatherSV_zinfo_alltiles_fromAll.py
@@ -115,7 +115,7 @@ for tp in types:
             tj = join(tz,tt,join_type='left',keys='TARGETID')
             tj['N_VI'].fill_value = 0
             tj['N_VI'] = tj['N_VI'].filled() #should easily be able to select rows with N_VI > 0 to get desired info
-            tj['TILEID'] = tile
+            #tj['TILEID'] = tile
             tj.write(dirz+'/'+tp+'/'+tile+'_'+tp+'zinfo_wVI.fits',format='fits',overwrite=True)
             print('wrote file with VI info to '+dirz+'/'+tp+'/'+tile+'_'+tp+'zinfo_wVI.fits')
             gt.append(tile)
