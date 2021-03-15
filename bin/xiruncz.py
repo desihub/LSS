00d4749c22a326d23c8c359c93d54a0469d33b60
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-03-15 17:52:48 -0400

    Update xiruncz.py

--- bin/xiruncz.py
+++ bin/xiruncz.py
@@ -58,6 +58,14 @@ if type == 'ELGhz497':
     zmax = 1.497
     type = 'ELG'
 
+if type == 'QSO':
+    zmin = 1.
+    zmax = 2.1
+
+if type == 'BGS_ANY':
+    zmin = 0.1
+    zmax = 0.5    
+
 ranwt1=False
 xt.prep4czxi(type,zmin,zmax,nran=10,indir=lssdir,ver=version,outdir=os.environ['CSCRATCH']+'/cz/',ranwt1=ranwt1)
 subprocess.run(['chmod','+x','czpc.sh'])
