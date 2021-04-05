027c77039e02b352844a5e8122c3551fc4d20bf6
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-05 16:24:06 -0400

    Update xiruncz.py

--- bin/xiruncz.py
+++ bin/xiruncz.py
@@ -104,7 +104,7 @@ if type == 'BGS_hiz':
     type = 'BGS_ANY'        
 
 ranwt1=False
-xt.prep4czxi(type,zmin,zmax,nran=18,indir=lssdir,ver=version,outdir=os.environ['CSCRATCH']+'/cz/',ranwt1=ranwt1,subt=subt)
+xt.prep4czxi(type,zmin,zmax,nran=1,indir=lssdir,ver=version,outdir=os.environ['CSCRATCH']+'/cz/',ranwt1=ranwt1,subt=subt)
 subprocess.run(['chmod','+x','czpc.sh'])
 subprocess.run('./czpc.sh')
 fa = ''
