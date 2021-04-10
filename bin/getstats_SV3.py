ed7f3dce06ab34de0cf5740ecb8c7eecc52452ae
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-10 17:46:06 -0400

    Update getstats_SV3.py

--- bin/getstats_SV3.py
+++ bin/getstats_SV3.py
@@ -38,15 +38,15 @@ print('splitting by type, numbers are after all veto masks:')
 tpl = ['ELG','LRG','QSO','BGS_ANY']
 for tp in tpl:
     dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/'+tp+'Alltiles_full.dat.fits')
-	wz = dat['ZWARN']*0 == 0
-	wz &= dat['ZWARN'] != 999999
-	wzg = dat['ZWARN'] == 0
-	print('number of '+tp+' observations (good hardware):',len(dat[wz]))
-	print('number of '+tp+' time good observations (ZWARN==0):',len(dat[wzg]))
-	ran = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/'+tp+'Alltiles_clustering.dat.fits')
-	print('effective '+tp+' area, after vetoing higher-priority positions, positions with redshift failures, and imaging: ',str(len(ran)/2500))
-	print('\n')
-	
+    wz = dat['ZWARN']*0 == 0
+    wz &= dat['ZWARN'] != 999999
+    wzg = dat['ZWARN'] == 0
+    print('number of '+tp+' observations (good hardware):',len(dat[wz]))
+    print('number of '+tp+' time good observations (ZWARN==0):',len(dat[wzg]))
+    ran = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/'+tp+'Alltiles_clustering.dat.fits')
+    print('effective '+tp+' area, after vetoing higher-priority positions, positions with redshift failures, and imaging: ',str(len(ran)/2500))
+    print('\n')
+    
 
     
     
