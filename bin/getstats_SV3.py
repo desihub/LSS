328c4b585b6cc3a13bad318dfaf95bc2913a9eb6
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-10 20:38:51 -0400

    Update getstats_SV3.py

--- bin/getstats_SV3.py
+++ bin/getstats_SV3.py
@@ -1,6 +1,24 @@
 import numpy as np
 import fitsio
 
+mtld = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/mtl-done-tiles.ecsv') #log of tiles completed for mtl
+tiles = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-sv3.ecsv')
+
+wp = tiles['PROGRAM'] == 'DARK'
+tiles = tiles[wp]
+
+wp = np.isin(mtld['TILEID'],tiles['TILEID']) #we want to consider MTL done tiles that correspond to the SV3 tile file
+mtld = mtld[wp]
+print('number of completed dark tiles:',len(mtld))
+
+wp = tiles['PROGRAM'] == 'BRIGHT'
+tiles = tiles[wp]
+
+wp = np.isin(mtld['TILEID'],tiles['TILEID']) #we want to consider MTL done tiles that correspond to the SV3 tile file
+mtld = mtld[wp]
+print('number of completed bright tiles:',len(mtld))
+
+
 ran = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/random0/rancomb_dark_Alltiles.fits')
 
 print('#area covered on DARK tiles\n#>N_tiles area(deg2)')
