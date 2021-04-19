6c68de6af4d5b9a773537a0da2bddcf5b32baf8d
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-19 12:31:13 -0400

    Update gatherSV_zinfo_alltiles_denali_inpar.py

--- bin/gatherSV_zinfo_alltiles_denali_inpar.py
+++ bin/gatherSV_zinfo_alltiles_denali_inpar.py
@@ -27,6 +27,7 @@ parser.add_argument("--type", help="tracer type to be selected")
 parser.add_argument("--release", help="what spectro release to use, e.g. blanc or daily",default='cascades') #eventually remove this and just gather everything
 parser.add_argument("--basedir", help="base directory for output, default is CSCRATCH",default=os.environ['CSCRATCH'])
 parser.add_argument("--version", help="catalog version; use 'test' unless you know what you are doing!",default='test')
+parser.add_argument("--runmd", help="prod/test; test is for debugging result on first tile",default='prod')
 args = parser.parse_args()
 print(args)
 
@@ -177,7 +178,11 @@ if __name__ == '__main__':
     tiles = np.unique(exps['TILEID'])
     print('going through '+str(len(tiles))+' tiles')
 
-    for j in range(0,len(tiles),N):
+    if arg.runmd == 'prod':
+        ntile = len(tiles)
+    if arg.runmd == 'test':
+        ntile = 1    
+    for j in range(0,ntile,N):
         #get_tilezinfo(tiles[j])
         inds = []
         for i in range(j,j+N):
