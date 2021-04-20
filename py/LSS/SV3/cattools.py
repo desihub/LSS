4ce6d7f231079ff37e0ebd883c62a0af63cc04e1
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-20 14:40:50 -0400

    Update cattools.py

--- py/LSS/SV3/cattools.py
+++ py/LSS/SV3/cattools.py
@@ -467,7 +467,7 @@ def mkfulldat(zf,imbits,tdir,tp,bit,outf):
     #print(np.histogram(nloclz))
     print(len(locl),len(nloclz),sum(nlocl),sum(nloclz))
     atloc = np.isin(dz['TILELOCID'],loclz)
-    allloc = 
+    
 
 
     print('counting tiles and finding rosette')
