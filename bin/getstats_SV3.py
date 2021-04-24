71e5489d8048c46bd011b1f7da58718b0c87fff0
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-23 21:15:26 -0400

    Update getstats_SV3.py

--- bin/getstats_SV3.py
+++ bin/getstats_SV3.py
@@ -1,6 +1,16 @@
 import numpy as np
 import fitsio
 from astropy.table import Table
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", help="catalog version; use 'test' unless you know what you are doing!",default='test')
+
+args = parser.parse_args()
+print(args)
+
+version = args.version
+
 
 mtld = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/mtl-done-tiles.ecsv') #log of tiles completed for mtl
 tiles = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-sv3.ecsv')
@@ -39,7 +49,7 @@ for nt in np.unique(ran['NTILE']):
     print(nt,len(ran[wt])/2500)
     
 print('#')
-dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/ALLAlltiles_dark_full.dat.fits')
+dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/'+version+'/datcomb_dark_Alltiles.fits')
 wz = dat['ZWARN']*0 == 0
 wz &= dat['ZWARN'] != 999999
 wzg = dat['ZWARN'] == 0
@@ -47,7 +57,7 @@ print('number of unique dark time targets observed (good hardware):',len(dat[wz]
 print('number of unique dark time targets with a good observation (ZWARN==0):',len(dat[wzg]))
 
 print('#')
-dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/ALLAlltiles_bright_full.dat.fits')
+dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/'+version+'/datcomb_bright_Alltiles.fits')
 wz = dat['ZWARN']*0 == 0
 wz &= dat['ZWARN'] != 999999
 wzg = dat['ZWARN'] == 0
@@ -59,14 +69,14 @@ print('splitting by type, numbers are after all veto masks:')
 
 tpl = ['ELG','LRG','QSO','BGS_ANY','MWS_ANY']
 for tp in tpl:
-    dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/'+tp+'Alltiles_full.dat.fits')
+    dat = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/'+version+'/'+tp+'Alltiles_full.dat.fits')
     wz = dat['ZWARN']*0 == 0
     wz &= dat['ZWARN'] != 999999
     wzg = dat['ZWARN'] == 0
     print('number of unique '+tp+' targets observed (good hardware):',len(dat[wz]))
     print('number of unique '+tp+' targets with a good observation (ZWARN==0):',len(dat[wzg]))
-    ran = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/test/'+tp+'Alltiles_0_clustering.ran.fits')
-    print('effective '+tp+' area, after vetoing higher-priority positions, positions with redshift failures, and imaging: ',str(len(ran)/2500))
+    ran = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/LSScats/'+version+'/'+tp+'Alltiles_0_clustering.ran.fits')
+    print('effective '+tp+' area, after vetoing higher-priority positions, and imaging: ',str(len(ran)/2500))
     print('#')
     
 
