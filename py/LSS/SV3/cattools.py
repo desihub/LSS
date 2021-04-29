93a55a55147f2576429143cc03125f9e3259201e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-04-29 09:58:52 -0400

    Update cattools.py

--- py/LSS/SV3/cattools.py
+++ py/LSS/SV3/cattools.py
@@ -123,8 +123,8 @@ def combfibmap_and_scores(tile,zdate,coaddir='/global/cfs/cdirs/desi/spectro/red
     for i in range(1,len(specs)):
         tnf = Table.read(coaddir+str(tile)+'/'+zdate+'/zbest-'+str(specs[i])+'-'+str(tile)+'-thru'+zdate+'.fits',hdu='FIBERMAP')
         tf = vstack([tf,tnf],metadata_conflicts='silent')
-		tns = Table.read(coaddir+str(tile)+'/'+zdate+'/coadd-'+str(specs[i])+'-'+str(tile)+'-thru'+zdate+'.fits',hdu='SCORES')
-		ts = vstack([ts,tns],metadata_conflicts='silent')
+        tns = Table.read(coaddir+str(tile)+'/'+zdate+'/coadd-'+str(specs[i])+'-'+str(tile)+'-thru'+zdate+'.fits',hdu='SCORES')
+        ts = vstack([ts,tns],metadata_conflicts='silent')
         
     
     tf = unique(tf,keys=['TARGETID'])
