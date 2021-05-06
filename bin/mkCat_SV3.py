4d2f94245912c379abbf1ab3626ea292d4902a1d
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-05-06 16:07:23 -0400

    Update mkCat_SV3.py

--- bin/mkCat_SV3.py
+++ bin/mkCat_SV3.py
@@ -165,8 +165,8 @@ if not os.path.exists(dirout):
 
 
 randir = sv3dir+'random'
-rm = args.minr
-rx = args.maxr
+rm = args.minr.astype(int)
+rx = args.maxr.astype(int)
 #logf.write('using random files '+str(rm)+ ' through '+str(rx)+' (this is python, so max is not inclusive)\n')
 for i in range(rm,rx):
     if not os.path.exists(sv3dir+'random'+str(i)):
