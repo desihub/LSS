c2d21221b1fac6fe8ec8e4659e2aafdcb5b1a269
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-06-01 19:24:34 -0400

    Update mkCat_SV3.py

--- bin/mkCat_SV3.py
+++ bin/mkCat_SV3.py
@@ -134,7 +134,7 @@ mdir = '/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/sv3/'+pdir+'/' #lo
 tdir = '/global/cfs/cdirs/desi/target/catalogs/dr9/0.57.0/targets/sv3/resolve/'+pdir+'/' #location of targets
 #mtld = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/mtl/mtl-done-tiles.ecsv') #log of tiles completed for mtl
 mtld = Table.read('/global/cfs/cdirs/desi/spectro/redux/daily/tiles.csv')
-wdone = mtld['ZDONE'] == True
+wdone = mtld['ZDONE'] == 'true'
 mtld = mtld[wdone]
 tiles = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-sv3.ecsv')
 imbits = [1,5,6,7,8,9,11,12,13]
