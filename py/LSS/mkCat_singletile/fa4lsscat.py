bd74b3dd5a6d0d2c34553e4137809fc82e43939c
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-06-17 15:13:34 -0400

    Update fa4lsscat.py

--- py/LSS/mkCat_singletile/fa4lsscat.py
+++ py/LSS/mkCat_singletile/fa4lsscat.py
@@ -14,7 +14,7 @@ from fiberassign.tiles import load_tiles, Tiles
 from fiberassign.targets import (TARGET_TYPE_SCIENCE, TARGET_TYPE_SKY,
                                  TARGET_TYPE_SUPPSKY,
                                  TARGET_TYPE_STANDARD, TARGET_TYPE_SAFE,
-                                 Targets, TargetsAvailable, TargetTree,
+                                 Targets, TargetsAvailable,
                                  LocationsAvailable, load_target_file)
 from fiberassign.assign import (Assignment, write_assignment_fits,
                                 write_assignment_ascii, merge_results,
@@ -35,12 +35,16 @@ def getfatiles(targetf,tilef,dirout='',dt = '2020-03-10T00:00:00',faver='2.3.0')
     tgs = Targets()
     load_target_file(tgs,targetf)
     print('loaded target file '+targetf)
-    tree = TargetTree(tgs, 0.01)
+    
     hw = load_hardware(rundate=dt)
     tiles = load_tiles(tiles_file=tilef)
     #tgsavail = TargetsAvailable(hw, tgs, tiles, tree)
     #favail = LocationsAvailable(tgsavail)
     #del tree
+    if int(faver[:1]) < 3:
+        from fiberassign.targets import (TargetTree)
+        tree = TargetTree(tgs, 0.01)
+     
     if faver == '2.3.0':
         tgsavail = TargetsAvailable(hw, tgs, tiles, tree)
         favail = LocationsAvailable(tgsavail)
