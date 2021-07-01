95dee7f23a0c6b613814f346c1d1e947db1f3204
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-07-01 17:44:52 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -1040,7 +1040,7 @@ def mkfullran(randir,rann,imbits,outf,tp,pd,bit,desitarg='DESI_TARGET',tsnr= 'TS
     dz = Table.read(zf)
     #dz.remove_columns(['TILES','NTILE'])
 
-    zfpd = randir+str(rann)+'/rancomb_'+pd+'_Alltilelocinfo.fits'
+    zfpd = '/global/cfs/cdirs/desi/survey/catalogs/main/LSS/'+specver+'/rancomb_'+str(rann)+pd+'_Alltilelocinfo.fits'
     dzpd = Table.read(zfpd)
     #dzpd.keep_columns(['TARGETID','TILES','NTILE'])
     dz = join(dz,dzpd,keys=['TARGETID'])
