955caafb2ea0416033013d167fd517247009d34d
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-07-27 16:01:42 -0400

    Update mkCat_SV3_ran.py

--- bin/mkCat_SV3_ran.py
+++ bin/mkCat_SV3_ran.py
@@ -272,7 +272,8 @@ def doran(ii):
                 specf = Table.read('/global/cfs/cdirs/desi/spectro/redux/everest/zcatalog/ztile-sv3-cumulative.fits')
                 wt = np.isin(specf['TILEID'],ta['TILEID']) #cut spec file to dark or bright time tiles
                 specf = specf[wt]
-                kc = ['ZWARN','LOCATION','FIBER','COADD_FIBERSTATUS','TILEID','FIBERASSIGN_X','FIBERASSIGN_Y','COADD_NUMEXP','COADD_EXPTIME','COADD_NUMNIGHT'\
+                specf['TILELOCID'] = 10000*specf['TILEID'] +specf['LOCATION']
+                kc = ['ZWARN','LOCATION','FIBER','COADD_FIBERSTATUS','TILEID','TILELOCID','FIBERASSIGN_X','FIBERASSIGN_Y','COADD_NUMEXP','COADD_EXPTIME','COADD_NUMNIGHT'\
                 ,'MEAN_DELTA_X','MEAN_DELTA_Y','RMS_DELTA_X','RMS_DELTA_Y','MEAN_PSF_TO_FIBER_SPECFLUX','TSNR2_ELG_B','TSNR2_LYA_B'\
                 ,'TSNR2_BGS_B','TSNR2_QSO_B','TSNR2_LRG_B',\
                 'TSNR2_ELG_R','TSNR2_LYA_R','TSNR2_BGS_R','TSNR2_QSO_R','TSNR2_LRG_R','TSNR2_ELG_Z','TSNR2_LYA_Z','TSNR2_BGS_Z',\
