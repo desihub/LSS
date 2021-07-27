500daa8cb1954a9f239d207c6d8feec6ae9e312b
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-07-27 15:58:41 -0400

    Update mkCat_SV3_ran.py

--- bin/mkCat_SV3_ran.py
+++ bin/mkCat_SV3_ran.py
@@ -281,7 +281,7 @@ def doran(ii):
                 specf = Table.read(ldirspec+'datcomb_'+type+'_specwdup_Alltiles.fits')
                 kc = ['ZWARN','LOCATION','TILEID','TILELOCID','FIBERSTATUS','FIBERASSIGN_X','FIBERASSIGN_Y','PRIORITY','DELTA_X','DELTA_Y','EXPTIME','PSF_TO_FIBER_SPECFLUX','TSNR2_ELG_B','TSNR2_LYA_B','TSNR2_BGS_B','TSNR2_QSO_B','TSNR2_LRG_B','TSNR2_ELG_R','TSNR2_LYA_R','TSNR2_BGS_R','TSNR2_QSO_R','TSNR2_LRG_R','TSNR2_ELG_Z','TSNR2_LYA_Z','TSNR2_BGS_Z','TSNR2_QSO_Z','TSNR2_LRG_Z','TSNR2_ELG','TSNR2_LYA','TSNR2_BGS','TSNR2_QSO','TSNR2_LRG']
 
-            ct.combran_wdup(mtld,ii,randir,type,ldirspec,specf,keepcol=kc)
+            ct.combran_wdup(mtld,ii,randir,type,ldirspec,specf,keepcols=kc)
             tc = ct.count_tiles_better(specf,'ran',type,ii,specrel=specrel)
             tc.write(ldirspec+'/rancomb_'+str(ii)+type+'_Alltilelocinfo.fits',format='fits', overwrite=True)
 
