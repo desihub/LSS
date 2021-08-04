1073ab957a101d4363b03f8e03390a2c47f92d47
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-08-04 12:22:17 -0400

    Update mkCat_SV3_ran.py

--- bin/mkCat_SV3_ran.py
+++ bin/mkCat_SV3_ran.py
@@ -290,12 +290,12 @@ def doran(ii):
         
     if mkfullr:
         if specrel == 'everest':
-            specf = Table.read('/global/cfs/cdirs/desi/spectro/redux/everest/zcatalog/ztile-sv3-'+type+'-cumulative.fits')
+            specf = Table.read('/global/cfs/cdirs/desi/spectro/redux/everest/zcatalog/ztile-sv3-'+pdir+'-cumulative.fits')
             wt = np.isin(specf['TILEID'],ta['TILEID']) #cut spec file to dark or bright time tiles
             specf = specf[wt]
             fbcol = 'COADD_FIBERSTATUS'
         if specrel == 'daily':
-            specf = Table.read(ldirspec+'datcomb_'+type+'_specwdup_Alltiles.fits')
+            specf = Table.read(ldirspec+'datcomb_'+pdir+'_specwdup_Alltiles.fits')
             fbcol = 'FIBERSTATUS'
 
         outf = dirout+type+'Alltiles_'+str(ii)+'_full.ran.fits'
