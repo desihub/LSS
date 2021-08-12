965c45ee6a137dd15c9f46a7ea23c637a86605c5
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-08-11 21:43:05 -0400

    Update mkCat_SV3_simp.py

--- bin/mkCat_SV3_simp.py
+++ bin/mkCat_SV3_simp.py
@@ -155,7 +155,7 @@ indirspec = indir+specrel+'/'
         
 if mkfulld:
     if specrel == 'everest':
-        specf = Table.read('/global/cfs/cdirs/desi/spectro/redux/everest/zcatalog/ztile-sv3-'+pdir+'-cumulative.fits')
+        specf = Table.read('/global/cfs/cdirs/desi/spectro/redux/everest/zcatalog/ztile-sv3-'+progl+'-cumulative.fits')
         wt = np.isin(specf['TILEID'],ta['TILEID']) #cut spec file to dark or bright time tiles
         specf = specf[wt]
     if specrel == 'daily':
