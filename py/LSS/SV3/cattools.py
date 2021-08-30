90ee0c622267ea501ac672ab9f0bbf2adfee9466
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-08-30 12:07:19 -0400

    Update cattools.py

--- py/LSS/SV3/cattools.py
+++ py/LSS/SV3/cattools.py
@@ -1211,7 +1211,7 @@ def mkfulldat(fs,zf,imbits,tdir,tp,bit,outf,ftiles,azf='',desitarg='SV3_DESI_TAR
     if tp == 'ELG' or tp == 'ELG_HIP':
         print('number of masked oII row (hopefully matches number not assigned) '+ str(np.sum(dz['o2c'].mask)))
     if tp == 'QSO':
-        print('number of good z according to qso file '+str(len(dz)-np.sum(dz['Z'].mask))
+        print('number of good z according to qso file '+str(len(dz)-np.sum(dz['Z'].mask)))
     print('length after cutting to unique targetid '+str(len(dz)))
     print('LOCATION_ASSIGNED numbers')
     print(np.unique(dz['LOCATION_ASSIGNED'],return_counts=True))
