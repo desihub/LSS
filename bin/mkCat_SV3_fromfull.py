6fdbcb1b51026200baa8419d762c6ddff2a40c27
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-09-14 15:14:56 -0400

    Update mkCat_SV3_fromfull.py

--- bin/mkCat_SV3_fromfull.py
+++ bin/mkCat_SV3_fromfull.py
@@ -33,7 +33,7 @@ parser.add_argument("--clus", help="make the data clustering files; these are cu
 parser.add_argument("--clusran", help="make the random clustering files; these are cut to a small subset of columns",default='y')
 parser.add_argument("--minr", help="minimum number for random files",default=0)
 parser.add_argument("--maxr", help="maximum for random files, default is 1, but 18 are available (use parallel script for all)",default=18) 
-parser.add_argument("--nz", help="get n(z) for type and all subtypes",default='n')
+parser.add_argument("--nz", help="get n(z) for type and all subtypes",default='y')
 
 parser.add_argument("--ntile",help="add any constraint on the number of overlapping tiles",default=0,type=int)
 parser.add_argument("--rcut",help="add any cut on the rosette radius, use string like rmin,rmax",default=None)
@@ -184,11 +184,11 @@ if mknz:
 #     if zmask:
 #         wzm = 'zmask_'
     if rcut is not None:
-        wzm += 'rmin'+str(rcut[0])+'rmax'+str(rcut[1])+'_'
+        wzm += '_rmin'+str(rcut[0])+'rmax'+str(rcut[1])+'_'
     if ntile > 0:
-        wzm += 'ntileg'+str(ntilecut)+'_'    
+        wzm += '_ntileg'+str(ntilecut)+'_'    
     if ccut is not None:
-        wzm += ccut+'_' #you could change this to however you want the file names to turn out
+        wzm += '_'+ccut #you could change this to however you want the file names to turn out
 
     regl = ['','_N','_S']
     
