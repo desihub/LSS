8c7ed1818ae3fd585e7bbc950b5b37d11dd63486
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-09-16 17:00:10 -0400

    fix getting spec info in random files

--- py/LSS/SV3/cattools.py
+++ py/LSS/SV3/cattools.py
@@ -1604,7 +1604,7 @@ def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6):
             nl[ii] = nzd[zind]
     del fd
     ft = Table.read(fn)
-    ft['NBAR'] = nl
+    ft['NZ'] = nl
     ft.write(fn,format='fits',overwrite=True)        
     print('done with data')
     for rann in range(0,nran):
@@ -1619,7 +1619,7 @@ def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6):
                 nl[ii] = nzd[zind]
         del fd
         ft = Table.read(fn)
-        ft['NBAR'] = nl
+        ft['NZ'] = nl
         ft.write(fn,format='fits',overwrite=True)      
         print('done with random number '+str(rann))  
     return True        
