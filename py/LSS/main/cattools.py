3b2542a27d9a2ba51db8ae9b23fa7ce57dc04fab
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-09-17 15:22:44 -0400

    TILE -> TILEID in qso file

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -1167,7 +1167,7 @@ def mkfulldat(fs,zf,imbits,ftar,tp,bit,outf,ftiles,azf='',desitarg='DESI_TARGET'
 
     if tp[:3] == 'QSO':
         arz = Table.read(azf)
-        arz.keep_columns(['TARGETID','LOCATION','TILE','Z','ZERR','Z_QN'])
+        arz.keep_columns(['TARGETID','LOCATION','TILEID','Z','ZERR','Z_QN'])
         print(arz.dtype.names)
         #arz['TILE'].name = 'TILEID'
         dz = join(dz,arz,keys=['TARGETID','TILEID','LOCATION'],join_type='left',uniq_col_name='{col_name}{table_name}',table_names=['','_QF'])
