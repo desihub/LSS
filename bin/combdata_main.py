99da85f06986da9de14d90514ab144c383eb2dd0
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-11-02 17:12:04 -0400

    Update combdata_main.py

--- bin/combdata_main.py
+++ bin/combdata_main.py
@@ -153,6 +153,6 @@ if specrel == 'everest':
 
 
 tj.write(ldirspec+'datcomb_'+prog+'_tarspecwdup_zdone.fits',format='fits', overwrite=True)
-tc = ct.count_tiles_better(specf,'dat',prog,specrel=specrel)
+tc = ct.count_tiles_better('dat',prog,specrel=specrel)
 tc.write(ldirspec+'Alltiles_'+prog+'_tilelocs.dat.fits',format='fits', overwrite=True)
 
