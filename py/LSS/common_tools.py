81194e29e484536acc4703167ae8bb3d2981121a
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-12-10 16:25:53 -0500

    Update common_tools.py

--- py/LSS/common_tools.py
+++ py/LSS/common_tools.py
@@ -111,6 +111,9 @@ def comp_tile(dz):
 def comp_tileloc(dz):
     #get numbers again after the re-assignment
     locl,nlocl = np.unique(dz['TILELOCID'],return_counts=True)
+    wz = dz['LOCATION_ASSIGNED'] == 1
+    dzz = dz[wz]
+
     loclz,nloclz = np.unique(dzz['TILELOCID'],return_counts=True)
 
     print('getting fraction assigned for each tilelocid')
