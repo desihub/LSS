5b210af43d46fcc583375b93a88701835d32e16e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2021-12-17 20:19:38 -0500

    Update combdata_main.py

--- scripts/main/combdata_main.py
+++ scripts/main/combdata_main.py
@@ -54,7 +54,8 @@ print('found '+str(len(mtd))+' '+prog+' time main survey tiles with zdone true f
 
 tiles4comb = Table()
 tiles4comb['TILEID'] = mtd['TILEID']
-tiles4comb['ZDATE'] = mtd['LASTNIGHT']
+#tiles4comb['ZDATE'] = mtd['LASTNIGHT']
+tiles4comb['ZDATE'] = mtd['ARCHIVEDATE']
 
 if len(tiles4comb) > 0:
     ral = []
