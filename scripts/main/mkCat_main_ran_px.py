702ba5dff9c490c68f66aeb64b4874b60f993868
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-01-10 18:15:38 -0500

    Update mkCat_main_ran_px.py

--- scripts/main/mkCat_main_ran_px.py
+++ scripts/main/mkCat_main_ran_px.py
@@ -302,7 +302,7 @@ def doran(ii):
             for px in hpxs:
                 print('combining target data for pixel '+str(px)+' '+str(npx)+' out of '+str(len(hpxs)))
                 ct.combran_wdup_hp(px,ta,ii,randir,type,ldirspec,specf,keepcols=kc)
-                tc = ct.count_tiles_better('ran',type,gtl,ii,specrel=specrel,px=px)
+                tc = ct.count_tiles_better_px('ran',type,gtl,ii,specrel=specrel,px=px)
                 tc.write(ldirspec+'/healpix/rancomb_'+str(ii)+type+'_'+str(px)+'__Alltilelocinfo.fits',format='fits', overwrite=True)
                 npx += 1
            
