a51ebb1413e1c83fefa43753963ab38c1b18ce7a
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-01-10 22:50:20 -0500

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -1324,7 +1324,7 @@ def mkfullran_px(indir,rann,imbits,outf,tp,pd,gtl,lznp,px,dirrt):
     tcol = ['TARGETID','MASKBITS','PHOTSYS','NOBS_G','NOBS_R','NOBS_Z'] #only including what are necessary for mask cuts for now
     #tcol = ['TARGETID','EBV','WISEMASK_W1','WISEMASK_W2','BRICKID','PSFDEPTH_G','PSFDEPTH_R','PSFDEPTH_Z','GALDEPTH_G',\
     #'GALDEPTH_R','GALDEPTH_Z','PSFDEPTH_W1','PSFDEPTH_W2','PSFSIZE_G','PSFSIZE_R','PSFSIZE_Z','MASKBITS','PHOTSYS','NOBS_G','NOBS_R','NOBS_Z']
-    tarf = fitsio.read(dirrt+'/randoms-1-hp-'+str(pix)+'.fits',columns=tcol)
+    tarf = fitsio.read(dirrt+'/randoms-1-hp-'+str(px)+'.fits',columns=tcol)
     dz = join(dz,tarf,keys=['TARGETID'])
     del tarf
 
