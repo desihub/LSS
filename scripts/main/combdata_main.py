9ce72eba119f154db61d54f1a66f43fb44a5e03b
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-01-13 15:19:12 -0500

    Update combdata_main.py

--- scripts/main/combdata_main.py
+++ scripts/main/combdata_main.py
@@ -193,10 +193,11 @@ if specrel == 'daily':
     newspec = ct.combtile_spec(tiles4comb,specfo)
     specf = Table.read(specfo)
     if newspec:
-        print('new tiles were found for spec data, will update '+specfo)
+        print('new tiles were found for spec dataso there were updates to '+specfo)
         specf.keep_columns(['CHI2','COEFF','Z','ZERR','ZWARN','ZWARN_MTL','NPIXELS','SPECTYPE','SUBTYPE','NCOEFF','DELTACHI2'\
         ,'FIBERASSIGN_X','FIBERASSIGN_Y','TARGETID','LOCATION','FIBER','COADD_FIBERSTATUS','PRIORITY'\
-        ,'DELTA_X','DELTA_Y','PSF_TO_FIBER_SPECFLUX','EXPTIME','OBJTYPE','NIGHT','EXPID','MJD','TILEID','INTEG_COADD_FLUX_B',\
+        ,'OBJTYPE','TILEID','INTEG_COADD_FLUX_B','MEAN_DELTA_X', 'RMS_DELTA_X', 'MEAN_DELTA_Y',\
+        'RMS_DELTA_Y', 'MEAN_FIBER_RA', 'STD_FIBER_RA',\
         'MEDIAN_COADD_FLUX_B','MEDIAN_COADD_SNR_B','INTEG_COADD_FLUX_R','MEDIAN_COADD_FLUX_R','MEDIAN_COADD_SNR_R','INTEG_COADD_FLUX_Z',\
         'MEDIAN_COADD_FLUX_Z','MEDIAN_COADD_SNR_Z','TSNR2_ELG_B','TSNR2_LYA_B','TSNR2_BGS_B','TSNR2_QSO_B','TSNR2_LRG_B',\
         'TSNR2_ELG_R','TSNR2_LYA_R','TSNR2_BGS_R','TSNR2_QSO_R','TSNR2_LRG_R','TSNR2_ELG_Z','TSNR2_LYA_Z','TSNR2_BGS_Z',\
