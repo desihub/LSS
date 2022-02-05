a6d6a3dfe0790d29664d890d30b0dc79dccd3e75
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-02-05 16:03:11 -0500

    Update daily_main_randoms_type.sh

--- scripts/main/daily_main_randoms_type.sh
+++ scripts/main/daily_main_randoms_type.sh
@@ -4,6 +4,7 @@
 #1st argument should be tracer type and 2nd should be whether or not to reject qso targets
 
 python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull n --fullr y --notqso $2
-python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull y --fullr n --notqso $2
+python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull y --fullr n --notqso $2 --minr 9
+python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull y --fullr n --notqso $2 --maxr 9
 python mkCat_main_ran.py --type $1 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec daily --notqso $2 --maxr 9
 python mkCat_main_ran.py --type $1 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec daily --notqso $2 --minr 9
