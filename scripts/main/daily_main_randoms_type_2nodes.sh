9e153a8308917736cd41726208d80dc3e2b8edf5
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-02-11 13:32:09 -0500

    Create daily_main_randoms_type_2nodes.sh

---
+++ scripts/main/daily_main_randoms_type_2nodes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#script to run main/daily randoms through for a given tracer type
+#to be run after getting interactive job with at least two node (e.g., via salloc -N 2 -C haswell -t 04:00:00 --qos interactive --account desi)
+#1st argument should be tracer type and 2nd should be whether or not to reject qso targets
+
+srun -N 1 python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull n --fullr y --notqso $2 &
+wait
+srun -N 1 python  mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull y --fullr n --notqso $2 --minr 9 &
+srun -N 1 python mkCat_main_ran_px.py  --type $1  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --rfa n --combhp n --combfull y --fullr n --notqso $2 --maxr 9 &
+wait
+srun -N 1 python mkCat_main_ran.py --type $1 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec daily --notqso $2 --maxr 9 &
+srun -N 1 python mkCat_main_ran.py --type $1 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec daily --notqso $2 --minr 9 &
