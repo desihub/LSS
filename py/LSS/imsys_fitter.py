23621d45f8469b48959bc0b29e5cc159bfb4201f
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-02-18 17:08:46 -0500

    Update imsys_fitter.py

--- py/LSS/imsys_fitter.py
+++ py/LSS/imsys_fitter.py
@@ -274,7 +274,7 @@ class Syst:
         print(self.par_names)
 
         #mig = Minuit(self.get_chi2, throw_nan=False, \
-         mig = Minuit(self.get_chi2,  \
+        mig = Minuit(self.get_chi2,  \
                              forced_parameters=par_names, \
                              print_level=1, errordef=1, \
                              **init_pars)
