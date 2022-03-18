a3df6bcb0a6c029848a9e9596e557ee515c05963
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-03-18 10:52:23 -0400

    Update cattools.py

--- py/LSS/SV3/cattools.py
+++ py/LSS/SV3/cattools.py
@@ -1671,62 +1671,62 @@ def mkclusran(fl,rann,rcols=['Z','WEIGHT'],zmask=False,tsnrcut=80,tsnrcol='TSNR2
         ffcs[col] = dshuf[col]     
     ffcs.write(outfs,format='fits', overwrite=True)
 
-def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6):
-    nzd = np.loadtxt(fb+'_nz.dat').transpose()[3] #column with nbar values
-    fn = fb+'_clustering.dat.fits'
-    fd = fitsio.read(fn) #reading in data with fitsio because it is much faster to loop through than table
-    zl = fd['Z']
-    nl = np.zeros(len(zl))
-    for ii in range(0,len(zl)):
-        z = zl[ii]
-        zind = int((z-zmin)/bs)
-        if z > zmin and z < zmax:
-            nl[ii] = nzd[zind]
-    del fd
-    ft = Table.read(fn)
-    ft['NZ'] = nl
-    ft.write(fn,format='fits',overwrite=True)        
-    print('done with data')
-    for rann in range(0,nran):
-        fn = fb+'_'+str(rann)+'_clustering.ran.fits'
-        fd = fitsio.read(fn) #reading in data with fitsio because it is much faster to loop through than table
-        zl = fd['Z']
-        nl = np.zeros(len(zl))
-        for ii in range(0,len(zl)):
-            z = zl[ii]
-            zind = int((z-zmin)/bs)
-            if z > zmin and z < zmax:
-                nl[ii] = nzd[zind]
-        del fd
-        ft = Table.read(fn)
-        ft['NZ'] = nl
-        ft.write(fn,format='fits',overwrite=True)      
-        print('done with random number '+str(rann))  
-    return True        
-    
-
-def mknz(fcd,fcr,fout,bs=0.01,zmin=0.01,zmax=1.6,om=0.31519):
-    
-    cd = distance(om,1-om)
-    ranf = fitsio.read(fcr) #should have originally had 2500/deg2 density, so can convert to area
-    area = len(ranf)/2500.
-    print('area is '+str(area))
-    
-    df = fitsio.read(fcd)
-    
-    nbin = int((zmax-zmin)/bs)
-    zhist = np.histogram(df['Z'],bins=nbin,range=(zmin,zmax),weights=df['WEIGHT'])
-    outf = open(fout,'w')
-    outf.write('#area is '+str(area)+'square degrees\n')
-    outf.write('#zmid zlow zhigh n(z) Nbin Vol_bin\n')
-    for i in range(0,nbin):
-        zl = zhist[1][i]
-        zh = zhist[1][i+1]
-        zm = (zh+zl)/2.
-        voli = area/(360.*360./np.pi)*4.*np.pi/3.*(cd.dc(zh)**3.-cd.dc(zl)**3.)
-        nbarz =  zhist[0][i]/voli
-        outf.write(str(zm)+' '+str(zl)+' '+str(zh)+' '+str(nbarz)+' '+str(zhist[0][i])+' '+str(voli)+'\n')
-    outf.close()
+# def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6):
+#     nzd = np.loadtxt(fb+'_nz.dat').transpose()[3] #column with nbar values
+#     fn = fb+'_clustering.dat.fits'
+#     fd = fitsio.read(fn) #reading in data with fitsio because it is much faster to loop through than table
+#     zl = fd['Z']
+#     nl = np.zeros(len(zl))
+#     for ii in range(0,len(zl)):
+#         z = zl[ii]
+#         zind = int((z-zmin)/bs)
+#         if z > zmin and z < zmax:
+#             nl[ii] = nzd[zind]
+#     del fd
+#     ft = Table.read(fn)
+#     ft['NZ'] = nl
+#     ft.write(fn,format='fits',overwrite=True)        
+#     print('done with data')
+#     for rann in range(0,nran):
+#         fn = fb+'_'+str(rann)+'_clustering.ran.fits'
+#         fd = fitsio.read(fn) #reading in data with fitsio because it is much faster to loop through than table
+#         zl = fd['Z']
+#         nl = np.zeros(len(zl))
+#         for ii in range(0,len(zl)):
+#             z = zl[ii]
+#             zind = int((z-zmin)/bs)
+#             if z > zmin and z < zmax:
+#                 nl[ii] = nzd[zind]
+#         del fd
+#         ft = Table.read(fn)
+#         ft['NZ'] = nl
+#         ft.write(fn,format='fits',overwrite=True)      
+#         print('done with random number '+str(rann))  
+#     return True        
+#     
+# 
+# def mknz(fcd,fcr,fout,bs=0.01,zmin=0.01,zmax=1.6,om=0.31519):
+#     
+#     cd = distance(om,1-om)
+#     ranf = fitsio.read(fcr) #should have originally had 2500/deg2 density, so can convert to area
+#     area = len(ranf)/2500.
+#     print('area is '+str(area))
+#     
+#     df = fitsio.read(fcd)
+#     
+#     nbin = int((zmax-zmin)/bs)
+#     zhist = np.histogram(df['Z'],bins=nbin,range=(zmin,zmax),weights=df['WEIGHT'])
+#     outf = open(fout,'w')
+#     outf.write('#area is '+str(area)+'square degrees\n')
+#     outf.write('#zmid zlow zhigh n(z) Nbin Vol_bin\n')
+#     for i in range(0,nbin):
+#         zl = zhist[1][i]
+#         zh = zhist[1][i+1]
+#         zm = (zh+zl)/2.
+#         voli = area/(360.*360./np.pi)*4.*np.pi/3.*(cd.dc(zh)**3.-cd.dc(zl)**3.)
+#         nbarz =  zhist[0][i]/voli
+#         outf.write(str(zm)+' '+str(zl)+' '+str(zh)+' '+str(nbarz)+' '+str(zhist[0][i])+' '+str(voli)+'\n')
+#     outf.close()
 
     
 
