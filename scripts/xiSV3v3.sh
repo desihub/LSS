cff06bcd32ada3e1589df0d805076b4a4b84af8f
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-03-29 14:20:47 -0400

    Update xiSV3v3.sh

--- scripts/xiSV3v3.sh
+++ scripts/xiSV3v3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#script to run all SV3 xismu
-#first get 16 nodes via salloc -N 16 -C haswell -t 04:00:00 --qos interactive --account desi
+:'script to run all SV3 xismu
+first get 16 nodes via salloc -N 16 -C haswell -t 04:00:00 --qos interactive --account desi
+'
 
-
-VERSPEC = 'fuji'
-VER = '3'
-WT = 'default_angular_bitwise'
-OUTDIR = '/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
+VERSPEC='fuji'
+VER='3'
+WT='default_angular_bitwise'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
 
 
 srun -N 1 -c 64 python xirunpc.py --type ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
