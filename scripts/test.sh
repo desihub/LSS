3a1d8bdb36707b3397ab5e3259b2af52a8200a7c
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-03-29 14:19:04 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-VERSPEC = 'fuji'
-VER = '3'
-WT = 'default_angular_bitwise'
-OUTDIR = '/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
+VERSPEC='fuji'
+VER='3'
+WT='default_angular_bitwise'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
 
 echo $VERSPEC
