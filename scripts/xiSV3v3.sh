8c81e6e692a1867ed89cca4978b0e31e65ee3e27
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-03-29 14:09:42 -0400

    Update xiSV3v3.sh

--- scripts/xiSV3v3.sh
+++ scripts/xiSV3v3.sh
@@ -1,30 +1,27 @@
 #script to run all SV3 xismu
+#first get 16 nodes via salloc -N 16 -C haswell -t 04:00:00 --qos interactive --account desi
 #!/bin/bash
-#SBATCH --qos=interactive
-#SBATCH --nodes=16
-#SBATCH --time=4:00
-#SBATCH --constraint=haswell
 
-verspec = 'fuji'
-ver = '3'
-wt = 'default_angular_bitwise'
-outdir = '/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
+VERSPEC = 'fuji'
+VER = '3'
+WT = 'default_angular_bitwise'
+OUTDIR = '/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3/xi/'
 
 
-srun -N 1 -c 64 python xirunpc.py --type ELG --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 &
-srun -N 1 -c 64 python xirunpc.py --type ELG --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type ELG_HIP --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 &
-srun -N 1 -c 64 python xirunpc.py --type ELG_HIP --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type ELG_HIPnotqso --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 &
-srun -N 1 -c 64 python xirunpc.py --type ELG_HIPnotqso --outdir $outdir --verspec $verspec --version $ver --weight_type $wt --nran 8 --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type LRG --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  &
-srun -N 1 -c 64 python xirunpc.py --type LRG --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type LRG_main --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  &
-srun -N 1 -c 64 python xirunpc.py --type LRG_main --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type QSO --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  &
-srun -N 1 -c 64 python xirunpc.py --type QSO --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type BGS_ANY --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  &
-srun -N 1 -c 64 python xirunpc.py --type BGS_ANY --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  --bin_type log &
-srun -N 1 -c 64 python xirunpc.py --type BGS_BRIGHT --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  &
-srun -N 1 -c 64 python xirunpc.py --type BGS_BRIGHT --outdir $outdir --verspec $verspec --version $ver --weight_type $wt  --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1 -c 64 python xirunpc.py --type ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type ELG_HIP --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1 -c 64 python xirunpc.py --type ELG_HIP --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type ELG_HIPnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1 -c 64 python xirunpc.py --type ELG_HIPnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1 -c 64 python xirunpc.py --type LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1 -c 64 python xirunpc.py --type LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1 -c 64 python xirunpc.py --type QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1 -c 64 python xirunpc.py --type BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+srun -N 1 -c 64 python xirunpc.py --type BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1 -c 64 python xirunpc.py --type BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
 wait
