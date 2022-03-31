d464ef5cca467f6cb462ed312f7608b22a21af72
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-03-31 16:47:57 -0400

    Update xirunpc.py

--- scripts/xirunpc.py
+++ scripts/xirunpc.py
@@ -384,7 +384,7 @@ if __name__ == '__main__':
         zlims = [float(zlim) for zlim in args.zlim]
     zlims = list(zip(zlims[:-1], zlims[1:])) + [(zlims[0], zlims[-1])]
 
-    rebinning_factors = [1, 4, 5, 10] if 'lin' in args.bin_type else [1,2]
+    rebinning_factors = [1, 4, 5, 10] if 'lin' in args.bin_type else [1,2,4]
     pi_rebinning_factors = [1, 4, 5, 10] if 'log' in args.bin_type else [1]
     if mpicomm is None or mpicomm.rank == mpiroot:
         logger.info('Computing correlation functions {} in regions {} in redshift ranges {}.'.format(args.corr_type, regions, zlims))
