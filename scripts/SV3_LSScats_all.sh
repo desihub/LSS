94ee1c6aa10663fd08cf643eb8bc5664f12c6705
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-01 17:06:09 -0400

    scripts to do all SV3 LSS

--- scripts/SV3_LSScats_all.sh
+++ scripts/SV3_LSScats_all.sh
@@ -26,13 +26,13 @@ python SV3/mkCat_SV3.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catal
 python SV3/mkCat_SV3.py --type BGS_ANY --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec fuji --maxr 0
 python SV3/mkCat_SV3.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec fuji --maxr 0
 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type ELG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type ELG_HIP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type ELG_HIP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji --notqso y
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type BGS_ANY --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
-srun -N 1 -C haswell -c 64 -t 01:00:00 python SV3/mkCat_SV3_ran.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type ELG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type ELG_HIP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type ELG_HIP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji --notqso y
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type BGS_ANY --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
+srun -N 1 -C haswell -c 64 -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --apply_veto y --verspec fuji 
 
 python SV3/mkCat_SV3.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clus y --clusran y  --nz y --maxr 18
 python SV3/mkCat_SV3.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clus y --clusran y  --nz y --maxr 18 --ccut main
