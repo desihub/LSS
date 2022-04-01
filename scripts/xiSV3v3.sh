cb62e2c4944b4685e390defd8a258aeb580193b9
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-01 12:20:25 -0400

    do all xi at once, including cross corr

--- scripts/xiSV3v3.sh
+++ scripts/xiSV3v3.sh
@@ -22,4 +22,25 @@ srun -N 1  python xirunpc.py --tracer BGS_ANY --outdir $OUTDIR --verspec $VERSPE
 srun -N 1  python xirunpc.py --tracer BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
 srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
 srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+
+srun -N 1  python xirunpc.py --tracer ELG LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIP LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG_HIP LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIP LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG_HIP LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer QSO LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1  python xirunpc.py --tracer QSO LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+srun -N 1  python xirunpc.py --tracer QSO LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1  python xirunpc.py --tracer QSO LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log &
+
+
 wait
