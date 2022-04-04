995de8981d205de6bc9f516d6e03de9ec2ce7778
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-04 12:09:16 -0400

    Update xiDA02v2_ELG.sh

--- scripts/xiDA02v2_ELG.sh
+++ scripts/xiDA02v2_ELG.sh
@@ -7,14 +7,14 @@ WT='RF_FKP'
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --verspec $VERSPEC --version $VER --weight_type $WT &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT &
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --verspec $VERSPEC --version $VER --weight_type $WT --zlim extended_elgzmask &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --zlim extended_elgzmask &
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --verspec $VERSPEC --version $VER --weight_type $WT --zlim extended &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --zlim extended &
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --verspec $VERSPEC --version $VER --weight_type $WT --zlim smallshells_elgzmask &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --zlim smallshells_elgzmask &
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --verspec $VERSPEC --version $VER --weight_type $WT --zlim smallshells &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --zlim smallshells &
 
 wait
