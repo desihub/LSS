475c30015975453752d982f345ca4fa044531d36
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-05 14:15:45 -0400

    Update xiDA02v2.sh

--- scripts/xiDA02v2.sh
+++ scripts/xiDA02v2.sh
@@ -9,15 +9,15 @@ WT='default_FKP'
 OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/2/xi/'
 
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP &
-srun -N 1  python xirunpc.py --tracer LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
-srun -N 1  python xirunpc.py --tracer QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
-srun -N 1  python xirunpc.py --tracer BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --nran 2 &
-srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 2 &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP &
+srun -N 1  python xirunpc.py --tracer LRG --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1  python xirunpc.py --tracer QSO --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1  python xirunpc.py --tracer BGS_ANY --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --nran 2 &
+srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 2 &
 
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP  &
-srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP  &
-srun -N 1  python xirunpc.py --tracer QSO LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso LRG --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP  &
+srun -N 1  python xirunpc.py --tracer ELG_LOPnotqso QSO --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type RF_FKP  &
+srun -N 1  python xirunpc.py --tracer QSO LRG --survey DA02 --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  &
 
 
 wait
