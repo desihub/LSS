2d4d9d8256fbb10e883033e93e597f5de73ee5c2
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-11 14:20:26 -0400

    Create xiDA02v2_32node.sh

---
+++ scripts/main/xiDA02v2_32node.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSPEC='guadalupe'
+VER='2'
+WT = 'default_FKP'
+WTe='RF_FKP'
+
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_BRIGHT --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_ANY --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+
+srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+
