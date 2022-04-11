e2f0afe44cf52b34028bb84fac84b2c05f0b8c02
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-11 14:24:17 -0400

    Update xiDA02v2_32node.sh

--- scripts/main/xiDA02v2_32node.sh
+++ scripts/main/xiDA02v2_32node.sh
@@ -8,19 +8,19 @@ WTe='RF_FKP'
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_BRIGHT --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_BRIGHT --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_ANY --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_ANY --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG QSO --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
 
-srun -N 32 -C haswell -n 1 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG ELG_LOPnotqso  --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WTe 
 
