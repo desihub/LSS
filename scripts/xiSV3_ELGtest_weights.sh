2bae2bc49f60e2e6dee9ce301071099507d2ac14
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-14 13:46:35 -0400

    Create xiSV3_ELGtest_weights.sh

---
+++ scripts/xiSV3_ELGtest_weights.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+
+VERSPEC='fuji'
+VER='test'
+WT='default_angular_bitwise'
+WT2='default_angular_bitwise_FKP'
+WT3='default_angular'
+WT4='default'
+WT5='default_bitwise'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/test/xi/'
+
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 --bin_type log &
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT2 --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT2 --nran 8 --bin_type log &
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT3 --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT3 --nran 8 --bin_type log &
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT4 --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT4 --nran 8 --bin_type log &
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT5 --nran 8 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT5 --nran 8 --bin_type log &
+
+
+wait
