1d216bfc74c5ee2cdce4d7670a0bdab42fe6f5cf
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-14 16:26:15 -0400

    Update xiSV3_ELGmocktest_weights.sh

--- scripts/xiSV3_ELGmocktest_weights.sh
+++ scripts/xiSV3_ELGmocktest_weights.sh
@@ -14,7 +14,7 @@ OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/mock/test/xi/'
 
 
 srun -N 1  python xirunpc.py --tracer ELG --basedir $BDIR --outdir $OUTDIR  --weight_type $WT --nran 8 &
-srun -N 1  python xirunpc.py --tracer ELG  --basedir $BDIR--outdir $OUTDIR  --weight_type $WT --nran 8 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG --basedir $BDIR --outdir $OUTDIR  --weight_type $WT --nran 8 --bin_type log &
 
 srun -N 1  python xirunpc.py --tracer ELG --basedir $BDIR --outdir $OUTDIR  --weight_type $WT2 --nran 8 &
 srun -N 1  python xirunpc.py --tracer ELG --basedir $BDIR --outdir $OUTDIR  --weight_type $WT2 --nran 8 --bin_type log &
