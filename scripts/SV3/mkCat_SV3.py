3c5a7db984dd63b2ff4ddfa45b3a0070a1835581
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-22 14:24:30 -0400

    Update mkCat_SV3.py

--- scripts/SV3/mkCat_SV3.py
+++ scripts/SV3/mkCat_SV3.py
@@ -502,6 +502,7 @@ if mkfulld:
         bitweightfile = SV3p.brightbitweightfile
 
 
+    #ct.mkfulldat(specf,dz,imbits,tdir,type,bit,dirout+type+notqso+'_full_noveto.dat.fits',ldirspec+'Alltiles_'+pdir+'_tilelocs.dat.fits',azf=azf,desitarg=desitarg,specver=specrel,notqso=notqso,bitweightfile=bitweightfile)
     ct.mkfulldat(specf,dz,imbits,tdir,type,bit,dirout+type+notqso+'_full_noveto.dat.fits',ldirspec+'Alltiles_'+pdir+'_tilelocs.dat.fits',azf=azf,desitarg=desitarg,specver=specrel,notqso=notqso,bitweightfile=bitweightfile)
     #get_tilelocweight()
     #logf.write('ran get_tilelocweight\n')
