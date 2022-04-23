9a641bebc48d3347da7412cbfab98a9ed7cb437b
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-22 21:42:27 -0400

    Update mkCat_SV3.py

--- scripts/SV3/mkCat_SV3.py
+++ scripts/SV3/mkCat_SV3.py
@@ -626,7 +626,7 @@ if mknz:
         fb = dirout+type+notqso+wzm+reg
         fcr = fb+'_0_clustering.ran.fits'
         fcd = fb+'_clustering.dat.fits'
-        fout = fb+'_nz.dat'
+        fout = fb+'_nz.txt'
         common.mknz(fcd,fcr,fout,bs=dz,zmin=zmin,zmax=zmax)
         common.addnbar(fb,bs=dz,zmin=zmin,zmax=zmax,P0=P0)
 
