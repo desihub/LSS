0ab3848505a2f26c0cc18c8612636524b4f9d55e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-29 17:06:18 -0400

    Update xiDA02vtest_32node.sh

--- scripts/main/xiDA02vtest_32node.sh
+++ scripts/main/xiDA02vtest_32node.sh
@@ -8,6 +8,8 @@ OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/test/x
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
+srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
+
 srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
 
 srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
@@ -15,8 +17,6 @@ srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tra
 srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --zlim lowz
 
 srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
-
-srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
 
 srun -N 32 -C haswell -n 32 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
 
