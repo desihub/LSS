af44e032ed0784c2eecaebbfd62f9fb9545b7a88
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-04-30 13:24:55 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -2161,7 +2161,8 @@ def mkclusdat(fl,weighttileloc=True,zmask=False,tp='',dchi2=9,tsnrcut=80,rcut=No
     if tp == 'BGS_BRIGHT':
         lrg = ssr_tools.LRG_ssr()
         ff = lrg.add_modpre(ff)
-        ff['WEIGHT_ZFAIL'] = 1./ff['mod_success_rate']
+        ff['WEIGHT_ZFAIL'] = np.clip(1./ff['mod_success_rate'],0,1)
+        print(np.min(ff['WEIGHT_ZFAIL']),np.max(ff['WEIGHT_ZFAIL']))
         print('checking sum of zfail weights compared to length of good z')
         print(len(ff),np.sum(ff['WEIGHT_ZFAIL']))
         ff['WEIGHT'] *= ff['WEIGHT_ZFAIL']
