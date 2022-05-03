937563252987896964f8f26123736416998b7410
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-05-03 11:47:11 -0400

    Update mkCat_main.py

--- scripts/main/mkCat_main.py
+++ scripts/main/mkCat_main.py
@@ -216,7 +216,7 @@ if mkfulld:
 
 if args.add_veto == 'y':
     fin = dirout+type+notqso+'zdone_full_noveto.dat.fits'
-    common.add_veto_col(fin,ran=False,tracer_mask=type[:3].lower())#,rann=0
+    common.add_veto_col(fin,ran=False,tracer_mask=type[:3].lower(),redo=True)#,rann=0
     for rn in range(rm,rx):
         fin = dirout+type+notqso+'zdone_'+str(rn)+'_full_noveto.ran.fits'
         common.add_veto_col(fin,ran=True,tracer_mask=type[:3].lower(),rann=rn)
