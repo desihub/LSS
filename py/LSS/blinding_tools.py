a0570baf8b5132b5ad28c588814274c28774fd3c
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-05-09 16:44:37 -0400

    Update blinding_tools.py

--- py/LSS/blinding_tools.py
+++ py/LSS/blinding_tools.py
@@ -22,7 +22,7 @@ def apply_zshift_DE(data,out_file,w0=-1,wa=0,zcol='Z'):
     #zcol is the column name
     #data = Table(fitsio.read(in_file))
     cosmo_d = Cosmology(h=cosmo_fid['h'],Omega_m=cosmo_fid['Omega_m'],w0_fld=w0,wa_fld=wa)
-    dis_val = dis_fid(data_in[zcol])
+    dis_val = dis_fid(data[zcol])
     d2z = DistanceToRedshift(cosmo_d.comoving_radial_distance)
     z_shift = d2z(dis_val)
     data['Z'] = z_shift
