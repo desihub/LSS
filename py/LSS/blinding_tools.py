eb1377cee1c7833340dc8f3a37eaa5288449ccb5
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-05-10 17:05:23 -0400

    Update blinding_tools.py

--- py/LSS/blinding_tools.py
+++ py/LSS/blinding_tools.py
@@ -20,13 +20,17 @@ def apply_zshift_DE(data,out_file,w0=-1,wa=0,zcol='Z'):
     from cosmoprimo.utils import DistanceToRedshift
     from cosmoprimo import Cosmology
 
+    #fiducial cosmo
     cosmo_fid = DESI()
     dis_fid = cosmo_fid.comoving_radial_distance
 
-
-    cosmo_d = Cosmology(h=cosmo_fid['h'],Omega_m=cosmo_fid['Omega_m'],w0_fld=w0,wa_fld=wa,engine='class')
-    dis_val = dis_fid(data[zcol])
-    d2z = DistanceToRedshift(cosmo_d.comoving_radial_distance)
+    #give distances assuming different cosmo
+    cosmo_d = cosmo_fid.clone(w0_fld=w0,wa_fld=wa)#Cosmology(h=cosmo_fid['h'],Omega_m=cosmo_fid['Omega_m'],w0_fld=w0,wa_fld=wa,engine='class')
+    dis_d = cosmo_d.comoving_radial_distance
+    dis_val = dis_d(data[zcol]) 
+    
+    #put back to z assuming fiducial cosmo
+    d2z = DistanceToRedshift(dis_fid) 
     z_shift = d2z(dis_val)
     data['Z'] = z_shift
     write_LSS(data,out_file,comments=None)
@@ -39,7 +43,10 @@ def swap_z(data,out_file,frac=0.01,zcols=['Z']):
     #zcols is the list of columns associated with the redshift column that should be swapped around
     sel1 = np.random.binomial(1, frac,len(data))
     sel2 = np.random.binomial(1, frac,len(data))
+    print(len(data[sel1]))
+    print(len(data[sel2]))
     for col in zcols:
+        print(col)
         d1 = data[col][sel1]
         d2 = data[col][sel2]
         data[col][sel1] = d2
