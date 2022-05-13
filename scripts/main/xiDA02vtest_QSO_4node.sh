c2a3dd3cc27ba1b6dc036b8da0a77ad5d92feb51
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-05-13 15:21:48 -0400

    Create xiDA02vtest_QSO_4node.sh

---
+++ scripts/main/xiDA02vtest_QSO_4node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSPEC='guadalupe'
+VER='test'
+WT='default_FKP'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/test/xi/'
+
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+
+srun -N 4 -C haswell -n 4 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu
+
+srun -N 4 -C haswell -n 4 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --zlim lowz
+
+
