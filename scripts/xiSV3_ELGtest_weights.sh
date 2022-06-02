72a76c44119d273122f09fbf28650ec6f6aafba5
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-02 15:19:44 -0400

    Update xiSV3_ELGtest_weights.sh

--- scripts/xiSV3_ELGtest_weights.sh
+++ scripts/xiSV3_ELGtest_weights.sh
@@ -5,13 +5,13 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 
 VERSPEC='fuji'
-VER='test'
+VER='3.1'
 WT='default_angular_bitwise'
 WT2='default_angular_bitwise_FKP'
 WT3='default_angular'
 WT4='default'
 WT5='default_bitwise'
-OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/test/xi/'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3.1/xi/'
 
 
 srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 8 &
