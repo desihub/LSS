8428cb5769baae7d589544c6a56b1eb38892619e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-07 11:36:06 -0400

    Update globals.py

--- py/LSS/globals.py
+++ py/LSS/globals.py
@@ -59,7 +59,7 @@ class main:
         self.mtld = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-specstatus.ecsv')
         self.tiles = Table.read('/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-main.ecsv')
         self.ebits = None
-        
+        self.badfib = None
         self.tsnrcol = 'TSNR2_ELG'
         self.tsnrcut = 0
         self.dchi2 = 0
@@ -101,4 +101,5 @@ class main:
             self.elgzf = '/global/cfs/cdirs/desi/users/raichoor/spectro/guadalupe/main-elg-guadalupe-tiles.fits'
             #self.qsozf = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/guadalupe/QSO_cat_guadalupe_cumulative.fits'
             self.qsozf = '/global/cfs/cdirs/desi/users/edmondc/QSO_catalog/guadalupe/QSO_cat_guadalupe_healpix.fits'
+            self.badfib = '/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/test/lrg+bgs_3sig_bad.txt'
         
