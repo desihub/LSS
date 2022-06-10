c8c0df455d39d0ac538ddd3b8981d29e63946eee
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-10 15:05:44 -0400

    Create xiSV3vEDA_auto.sh

---
+++ scripts/xiSV3vEDA_auto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSPEC='fuji'
+VER='3.1'
+WT='default_angular_bitwise_FKP'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/SV3/LSS/fuji/LSScats/3.1/xi/'
+
+
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer ELG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELGnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer ELGnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIP --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer ELG_HIP --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 --bin_type log &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer ELG_HIPnotqso --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 --bin_type log &
+srun -N 1  python xirunpc.py --tracer LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --nran 18 &
+srun -N 1  python xirunpc.py --tracer LRG --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log --nran 18 &
+srun -N 1  python xirunpc.py --tracer LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer LRG_main --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log --nran 18 &
+srun -N 1  python xirunpc.py --tracer QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer QSO --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log --nran 18 &
+srun -N 1  python xirunpc.py --tracer BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer BGS_ANY --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log --nran 18 &
+srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT --nran 18 &
+srun -N 1  python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --verspec $VERSPEC --version $VER --weight_type $WT  --bin_type log --nran 18 &
+wait
