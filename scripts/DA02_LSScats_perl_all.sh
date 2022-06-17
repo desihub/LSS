e852b8b7acd3a68bec702159182dae32225dfbfb
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-17 14:07:31 -0400

    Update DA02_LSScats_perl_all.sh

--- scripts/DA02_LSScats_perl_all.sh
+++ scripts/DA02_LSScats_perl_all.sh
@@ -10,9 +10,9 @@ python main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/cat
 python main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec guadalupe --survey DA02 --version $1
 
 srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type LRG  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --survey DA02 --version $1
-srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type ELG_LOP  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --notqso y --version $1
-srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type QSO  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --version $1
-srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type BGS_BRIGHT  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --version $1
+srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type ELG_LOP  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --survey DA02 --notqso y --version $1
+srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type QSO  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --fullr y --survey DA02 --version $1
+srun -N 1 -C cpu -t 01:00:00 -q interactive python main/mkCat_main_ran.py  --type BGS_BRIGHT  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec guadalupe --survey DA02 --fullr y --version $1
 
 python main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --add_veto y --apply_veto y --verspec guadalupe --survey DA02 --version $1 --maxr 0
 python main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --add_veto y --apply_veto y --verspec guadalupe --survey DA02 --version $1 --maxr 0 --notqso y
