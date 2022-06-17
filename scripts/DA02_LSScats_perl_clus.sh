ec79ac30e73ce66140e70438d3d7e2ea11764c98
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-17 15:12:50 -0400

    Create DA02_LSScats_perl_clus.sh

---
+++ scripts/DA02_LSScats_perl_clus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /global/common/software/desi/desi_environment.sh master
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+PYTHONPATH=$PYTHONPATH:/global/homes/a/ajross/.local/lib/python3.8/site-packages/
+
+
+python main/mkCat_main.py --type LRG --verspec guadalupe --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clusd y --clusran y --imsys n --nz y --regressis y --add_regressis y --survey DA02 --version $1 
+python main/mkCat_main.py --type ELG_LOP --verspec guadalupe --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clusd y --clusran y --imsys n --nz y --regressis y --add_regressis y --survey DA02 --notqso y --version $1 
+python main/mkCat_main.py --type QSO --verspec guadalupe --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clusd y --clusran y --imsys n --nz y --regressis y --add_regressis y --survey DA02 --version $1 
+python main/mkCat_main.py --type BGS_BRIGHT --verspec guadalupe --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --clusd y --clusran y --imsys n --nz y --regressis y --add_regressis y --survey DA02 --version $1 
+
+
