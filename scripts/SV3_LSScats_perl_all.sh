05048ec103c8a3a3fdb41b5182e79a7843a70643
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-17 11:08:47 -0400

    Update SV3_LSScats_perl_all.sh

--- scripts/SV3_LSScats_perl_all.sh
+++ scripts/SV3_LSScats_perl_all.sh
@@ -18,7 +18,11 @@ srun -N 1 -C cpu -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py  --type 
 srun -N 1 -C cpu -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py  --type BGS_ANY  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec fuji --fullr y --version $1
 srun -N 1 -C cpu -t 01:00:00 -q interactive python SV3/mkCat_SV3_ran.py  --type BGS_BRIGHT  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec fuji --fullr y --version $1
 
-srun -N 1 -C cpu -t 04:00:00 -q interactive python getLRGmask.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --survey SV3 --verspec fuji --maxr 18 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python getmask_type.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --tracer LRG --survey SV3 --verspec fuji --maxr 18 --version $1 --mver 1.1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python getmask_type.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --tracer ELG --survey SV3 --verspec fuji --maxr 18 --version $1 --mver 1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python getmask_type.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --tracer ELG_HIP --survey SV3 --verspec fuji --maxr 18 --version $1 --mver 1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python getmask_type.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --tracer ELG_HIPnotqso --survey SV3 --verspec fuji --maxr 18 --version $1 --mver 1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python getmask_type.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --tracer ELGnotqso --survey SV3 --verspec fuji --maxr 18 --version $1 --mver 1
 
 python SV3/mkCat_SV3.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec fuji --maxr 0 --version $1
 python SV3/mkCat_SV3.py --type ELG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec fuji --maxr 0 --version $1
