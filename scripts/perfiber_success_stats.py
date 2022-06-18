67c78f4283a3cd38b2c7f8b8d5c7c2c05fcc6890
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-17 21:48:01 -0400

    Update perfiber_success_stats.py

--- scripts/perfiber_success_stats.py
+++ scripts/perfiber_success_stats.py
@@ -133,83 +133,15 @@ fiblg,n_g = np.unique(dz[z_suc&z_tot]['FIBER'],return_counts=True)
 fib_test = np.isin(fibl,fiblg)
 z_tot &= np.isin(dz['FIBER'],fibl[fib_test])
 fibl,n_tot = np.unique(dz[z_tot]['FIBER'],return_counts=True)
+
 if np.array_equal(fibl,fiblg):
     gfrac = n_g/n_tot
 else:
     sys.exit('need to put something in for mismatch fiber lists')
 
-sys.exit()
-cat1 = Table(ff[z_tot])
+fn = basedir+'/'+survey+'/LSS/'+specver+"/"+tp+'_zsuccess.txt'
+fo = open(fn,'w')
+for ii in range(len(fibl)):
+    fo.write(fibl[ii]+' '+str(n_g[i]/n_tot[i])+' '+str(n_g[i])+' '+str(n_tot[i])+'\n')
+fo.close()
 
-full=Table()
-full['FIBER'] = np.arange(5000)
-
-fiberstats = Table()
-fiberstats['FIBER'], fiberstats['n_tot'] = np.unique(ff['FIBER'][z_tot], return_counts=True)
-#fiberstats.sort('n_tot')
-
-tt = Table()
-tt['FIBER'], tt['n_suc'] = np.unique(ff['FIBER'][z_suc], return_counts=True)
-
-fiberstats1 = join(fiberstats, tt, keys='FIBER', join_type='outer').filled(0)
-fiberstats1 = join(fiberstats1,full, keys='FIBER',join_type='outer').filled(0)
-#fiberstats1['frac_suc'] = fiberstats1['n_suc']/fiberstats1['n_tot']
-
-
-z_tot = ff2['ZWARN'] != 999999
-z_tot &= ff2['ZWARN']*0 == 0
-#print(len(ff2[z_suc]),len(ff2[z_tot]))
-print("zsuccess rate for BGS=",len(ff2[z_suc])/len(ff2[z_tot]))
-cat2 = Table(ff2[z_tot])
-
-fiberstats2 = Table()
-fiberstats2['FIBER'], fiberstats2['n_tot'] = np.unique(ff2['FIBER'][z_tot], return_counts=True)
-#fiberstats.sort('n_tot')
-
-tt2 = Table()
-tt2['FIBER'], tt2['n_suc'] = np.unique(ff2['FIBER'][z_suc], return_counts=True)
-fiberstats2 = join(fiberstats2, tt2, keys='FIBER', join_type='outer').filled(0)
-fiberstats2 = join(fiberstats2,full, keys='FIBER',join_type='outer').filled(0)
-#fiberstats2['frac_suc'] = fiberstats2['n_suc']/fiberstats2['n_tot']
-
-
-fstats_comb = Table()
-fstats_comb['Fiber']=np.arange(5000)
-fstats_comb['n_tot']=np.arange(5000)
-fstats_comb['n_suc']=np.arange(5000)
-for fiber in fstats_comb['Fiber']:
-    m1=fiberstats1['FIBER']==fiber
-    m2=fiberstats2['FIBER']==fiber
-    fstats_comb['n_tot'][fiber] = fiberstats1['n_tot'][m1]+fiberstats2['n_tot'][m2]
-    fstats_comb['n_suc'][fiber] = fiberstats1['n_suc'][m1]+fiberstats2['n_suc'][m2]
-
-mask0= fstats_comb['n_tot']>1
-fstats_comb=fstats_comb[mask0]
-fstats_comb['frac_suc']=fstats_comb['n_suc']/fstats_comb['n_tot']
-#fstats_comb 
-
-error_floor = True
-
-n, p = fstats_comb['n_tot'].copy(), fstats_comb['frac_suc'].copy()
-if error_floor:
-    p1 = np.maximum(1-p, 1/n)  # error floor
-else:
-    p1 = p
-fstats_comb['frac_suc_err'] = np.clip(np.sqrt(n * p * (1-p))/n, np.sqrt(n * p1 * (1-p1))/n, 1)
-
-#print("Removed fibers for having only 1 obs:\n",fstats_comb['FIBER'][ntotmask])
-mean = np.sum(fstats_comb['n_suc'])/np.sum(fstats_comb['n_tot'])
-fstats_comb['check'] =(mean - fstats_comb['frac_suc'])/fstats_comb['frac_suc_err']
-fstats_comb.sort('frac_suc')
-#fstats_comb
-
-
-
-#mean = np.sum(fstats_comb['n_suc'])/np.sum(fstats_comb['n_tot'])
-n = 3
-maskcheck = fstats_comb['check']>n
-print(fstats_comb)
-#np.savetxt(basedir+'/'+survey+'/LSS/'+specver+'/LSScats/'+version+"/lrg+bgs_"+str(n)+"sig_bad_fibers.txt",fstats_comb[maskcheck]['Fiber'],fmt='%i')
-fn = basedir+'/'+survey+'/LSS/'+specver+"/lrg+bgs_"+str(n)+"sig_bad_fibers.txt"
-np.savetxt(fn,fstats_comb[maskcheck]['Fiber'],fmt='%i')
-print('saved results to '+fn)
