83a6dd90d7cab507f425b763710f7edc0b333c32
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-06-19 16:16:54 -0400

    Create xiDA02vEDA_8node.sh

---
+++ scripts/main/xiDA02vEDA_8node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSPEC='guadalupe'
+VER='EDAbeta'
+WT='default_FKP'
+OUTDIR='/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/LSScats/EDAbeta/xi/'
+
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer ELG_LOPnotqso  --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --zlim lowz --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG QSO --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer QSO ELG_LOPnotqso  --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer LRG ELG_LOPnotqso --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+srun -N 8 -C haswell -n 8 -t 04:00:00 -q interactive   python xirunpc.py --tracer BGS_BRIGHT --outdir $OUTDIR --survey DA02 --verspec $VERSPEC --version $VER --weight_type $WT --corr_type smu --nran 10 &
+
+
