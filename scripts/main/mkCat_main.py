0fead15eaf5799e5e21d63bbdae8ba570342beb4
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-07-14 10:48:24 -0400

    Update mkCat_main.py

--- scripts/main/mkCat_main.py
+++ scripts/main/mkCat_main.py
@@ -410,8 +410,10 @@ if args.regressis == 'y':
 
     cut_fracarea = False
     seed = 42
-
-    rt._compute_weight('main', tracer_clus, dr9_footprint, suffix_tracer, suffix_regressor, cut_fracarea, seed, param, max_plot_cart,pixweight_path=pwf,sgr_stream_path=sgf)
+    fit_maps = None
+    if tracer_clus[:3] == 'BGS':# or tracer_clus[:3] == 'ELG':
+        fit_maps = ['STARDENS','EBV','GALDEPTH_G', 'GALDEPTH_R','GALDEPTH_Z','PSFSIZE_G','PSFSIZE_R','PSFSIZE_Z']
+    rt._compute_weight('main', tracer_clus, dr9_footprint, suffix_tracer, suffix_regressor, cut_fracarea, seed, param, max_plot_cart,pixweight_path=pwf,sgr_stream_path=sgf,feature_names=fit_maps)
 
 if args.add_regressis == 'y':
     from LSS.imaging import densvar
