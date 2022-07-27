eb4c370dc17253e882a0b1eee734d56b1cd9784e
Adam Myers <geordiemyers@gmail.com>
2022-07-26 17:21:58 -0700

    add functionality to write sky maps. Start with pixmap files from desitarget

--- py/LSS/imaging/sky_maps.py
+++ py/LSS/imaging/sky_maps.py
@@ -11,7 +11,7 @@ import fitsio
 import numpy as np
 from time import time
 
-from desitarget.randoms import pixmap
+from desitarget.io import read_targets_header
 
 # ADM the DESI default logger.
 from desiutil.log import get_logger
@@ -55,22 +55,57 @@ def get_lss_map_dir(lssmapdir=None):
     return lssmapdir
 
 
+def write_atomically(filename, data, extname=None, header=None):
+    """Write a FITS file in an atomic fashion.
+
+    Parameters
+    ----------
+    filename : :class:`str`
+        The output file.
+    data : :class:`~numpy.ndarray`
+        The numpy structured array of data to write.
+    extname, header optional
+        Passed through to `fitsio.write()`. `header` can be either
+        a FITShdr object or a dictionary.
+
+    Returns
+    -------
+    Nothing, but writes the `data` to the `filename`.
+
+    Notes
+    -----
+        - Always OVERWRITES existing files!
+        - Always makes the `filename` directory if it doesn't exist.
+        - By "in an atomic fashion" it is meant that files that died
+          mid-write will be appended by ".tmp".
+    """
+    # ADM make the necessary directory if it doesn't exist.
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    # ADM write the file atomically by making a .tmp file and moving it.
+    fitsio.write(filename+'.tmp', data, extname=extname, header=header,
+                 clobber=True)
+    os.rename(filename+'.tmp', filename)
+
+    return
+
+
 def wrap_pixmap(randoms, targets, nside=512, gaialoc=None, test=False):
     """HEALPix map from randoms (wrapper on desitarget.randoms.pixmap)
 
     Parameters
     ----------
     randoms : :class:`str` or :class:`list`
+        Random catalog.
         Filename of random catalog or list of filenames. Files must have
         columns 'RA', 'DEC', 'EBV', 'PSFDEPTH_W1/W2/G/R/Z', 'NOBS_G/R/Z'
         'GALDEPTH_G/R/Z', 'PSFSIZE_G/R/Z', 'MASKBITS' and have been
         generated at the same density. If `randoms` is a list files will
         be concatenated in list-order.
     targets : :class:`~numpy.ndarray` or `str`
-        Corresponding (i.e. same Data Release) file of targets, or the
-        name of a directory containing HEALPixel-split targets that
+        Corresponding (same Data Release as `randoms`) file of targets,
+        or name of a directory containing HEALPixel-split targets that
         can be read by :func:`desitarget.io.read_targets_in_box()`.
-    nside : :class:`int`, optional, defaults to nside=256
+    nside : :class:`int`, optional, defaults to nside=512
         Resolution (HEALPix nside) at which to build the (NESTED) map.
     gaialoc : :class:`str`, optional, defaults to ``None``
         Name of a FITS file that already contains a column "STARDENS",
@@ -106,12 +141,16 @@ def wrap_pixmap(randoms, targets, nside=512, gaialoc=None, test=False):
             - One column for every bit that is returned by
               :func:`desitarget.QA._load_targdens()`. Each column
               contains the target density in the pixel.
+    :class:`str`
+        Survey to which `targets` corresponds, e.g., 'main', 'svX', etc.
 
     Notes
     -----
     - If `gaialoc` is ``None`` then $GAIA_DIR must be set.
     - Docstring mostly stolen from :func:`desitarget.randoms.pixmap()`.
     """
+    # ADM desitarget function to wrap.
+    from desitarget.randoms import pixmap
     # ADM if we're testing, only read in a subset of randoms.
     rows = None
     if test:
@@ -151,6 +190,74 @@ def wrap_pixmap(randoms, targets, nside=512, gaialoc=None, test=False):
 
     log.info("Read {} total randoms at density {}".format(len(randoms), dens))
 
-    skymap, survey = pixmap(randoms, targets, dens, nside=nside, gaialoc=gaialoc)
+    return pixmap(randoms, targets, dens, nside=nside, gaialoc=gaialoc)
 
-    return skymap
+
+def write_pixmap(randoms, targets, hdr=None, nside=512, gaialoc=None, test=False,
+                 outdir=None):
+    """Write pixmap made by :func:`wrap_pixmap()`
+
+    Parameters
+    ----------
+    randoms : :class:`~numpy.ndarray`
+        Random catalog.
+    targets : :class:`str`
+        Corresponding (same Data Release as `randoms`) file of targets,
+        or name of a directory containing HEALPixel-split targets that
+        can be read by :func:`desitarget.io.read_targets_in_box()`. The
+        file (or all files in the directory) must contain "OBSCON" in the
+        header so the code can determine if we're working with dark-time
+        or bright-time targets.
+    hdr : :class:`dict` or `FITSHDR`
+        Header to write to the pixweight file.
+    nside : :class:`int`, optional, defaults to nside=512
+        Resolution (HEALPix nside) at which to build the (NESTED) map.
+    gaialoc : :class:`str`, optional, defaults to ``None``
+        Name of a FITS file that already contains a column "STARDENS",
+        which is simply read in. If ``None``, the stellar density is
+        constructed from files in $GAIA_DIR.
+    test : :class:`bool`, optional, defaults to ``False``
+        If ``True`` then only process the first 100,000 entries in
+        `randoms`. Useful for testing the code.
+    outdir : :class:`str`, optional, defaults to ``None``
+        Name of output directory to which to write pixel map. If ``None``
+        then default to the $LSS_MAP_DIR environment variable.
+
+    Returns
+    -------
+    Nothing, but writes a pixel map to:
+        `outdir`/pixweight_maps_all/pixweight-<obscon>.fits
+    if the keyword SEED isn't provided in `hdr`, or, if it is:
+        `outdir`/pixweight_maps_all/pixweight-<seed>-<obscon>.fits
+    """
+    # ADM read in the observing program from the target file header.
+    obscon = read_targets_header(targets)["OBSCON"].lower()
+
+    # ADM construct the output file.
+    outfile = "pixweight-{}.fits".format(obscon)
+    if hdr is not None:
+        if "SEED" in hdr:
+            outfile = outfile.replace("eight-", "eight-{}-".format(hdr["SEED"]))
+    else:
+        # ADM if no header was passed, we need to construct one.
+        hdr = fitsio.FITSHDR()
+
+    lss_map_dir = get_lss_map_dir(outdir)
+    outfile = os.path.join(lss_map_dir, "pixweight_maps_all", outfile)
+
+    # ADM augment the output header.
+    hdr['GAIALOC'] = gaialoc
+    hdr['HPXNSIDE'] = nside
+    hdr['HPXNEST'] = True
+
+    pixmap, survey = wrap_pixmap(randoms, targets, nside=nside, gaialoc=gaialoc, test=test)
+
+    hdr["SURVEY"] = survey
+
+    #ADM write out the map.
+    write_atomically(outfile, pixmap, extname='PIXWEIGHTS', header=hdr)
+    log.info('wrote map of HEALPixel weights to {}...t={:.1f}s'.format(
+        outfile, time()-start))
+
+    
+    
