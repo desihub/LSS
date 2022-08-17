384b7288661bc942910175953ced7a5c814d94f4
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-08-17 17:02:41 -0400

    Update mkCat_main.py

--- scripts/main/mkCat_main.py
+++ scripts/main/mkCat_main.py
@@ -208,7 +208,7 @@ if mktar: #concatenate target files for given type, with column selection hardco
 mketar = True
 etardir = '/global/cfs/cdirs/desi/survey/catalogs/extra_target_data/'+tarver+'/'
 etarf = maindir+type +'targets_pixelDR9v'+tarver.strip('.')+'.fits'        
-if os.path.isfile(etarf) and redotar == False:
+if os.path.isfile(etarf) and redotar == False and args.survey == 'main':
     mketar = False
 
 if mketar: #concatenate target files for given type, with column selection hardcoded
