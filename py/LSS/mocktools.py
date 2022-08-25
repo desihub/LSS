73098f6c510f37b2a8e3a0015c2b5410e5ed7b9d
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-08-25 01:00:56 -0400

    Update mocktools.py

--- py/LSS/mocktools.py
+++ py/LSS/mocktools.py
@@ -62,7 +62,8 @@ def get_fba_mock(mockdir,mocknum,survey='DA02',prog='dark'):
     fo.write(" --sky_per_slitblock 1")
     fo.write(" --ha "+str(fht['FA_HA']))
     fo.write(" --margin-gfa 0.4 --margin-petal 0.4 --margin-pos 0.05")
-    fo.close()    
+    fo.close()
+    print('wrote scripts for fiberassign '+outdir+'/fa-'+ts+'.sh')    
 
 def get_fba_mock_ran(mockdir,rannum,survey='DA02',prog='dark'):
     #produces script to run to get mock fiberassign files
