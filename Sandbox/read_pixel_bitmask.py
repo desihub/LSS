7a90ce6b5388565d7da52bb922b3a61f019ef967
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-10-12 21:44:58 -0400

    Update read_pixel_bitmask.py

--- Sandbox/read_pixel_bitmask.py
+++ Sandbox/read_pixel_bitmask.py
@@ -75,7 +75,8 @@ def bitmask_radec(brickid, ra, dec):
     coadd_x, coadd_y = w.wcs_world2pix(ra, dec, 0)
     coadd_x, coadd_y = np.round(coadd_x).astype(int), np.round(coadd_y).astype(int)
     sel = (coadd_x < 0) | (coadd_x >= 3600)
-    print(coadd_x[sel],brickid[sel],ra[sel],dec[sel])
+    if len(coadd_x[sel]) > 0:
+        print(coadd_x[sel],brickid,ra[sel],dec[sel])
     #if coadd_y < 0 or coadd_y >= 3600:
     #    print(coadd_y,brick_id,ra,dec)
 
