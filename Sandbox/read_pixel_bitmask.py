d0a9a8313b00e5130b69def292dcce6b5b9abf66
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-10-12 22:07:26 -0400

    Update read_pixel_bitmask.py

--- Sandbox/read_pixel_bitmask.py
+++ Sandbox/read_pixel_bitmask.py
@@ -124,6 +124,10 @@ if 'BRICKID' not in cat.colnames:
     tmp = brick.Bricks(bricksize=0.25)
     cat['BRICKID'] = tmp.brickid(cat['RA'], cat['DEC'])
 
+sel0 = cat['BRICKID'] == 0
+print(len(cat[sel0]))
+print(np.min(cat[sel0]['RA']),np.min(cat[sel0]['DEC']))
+
 cat.keep_columns(['RA','DEC','BRICKID','TARGETID'])
 sel = np.isin(cat['BRICKID'],bricks['BRICKID'])
 print(np.max(cat['BRICKID']),np.min(cat['BRICKID']))
