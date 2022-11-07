9d337f1427fe78a222cb036b6553b4b0224694c8
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-11-07 17:32:05 -0500

    Update ssr_tools.py

--- py/LSS/ssr_tools.py
+++ py/LSS/ssr_tools.py
@@ -544,7 +544,7 @@ class BGS_ssr:
         plt.ylabel('BGS_BRIGHT Z failure rate')
         plt.xlabel('BGS_BRIGHT EFFECTIVE exp time')
         plt.legend()
-        #plt.savefig(fn_root+'overall_failratefit.png')        
+        plt.savefig(fn_root+'overall_failratefit.png')        
         plt.show()
         plt.clf()
 
@@ -703,7 +703,7 @@ class ELG_ssr:
         plt.ylabel('ELG_LOPnotqso Z failure rate')
         plt.xlabel('ELG EFFECTIVE exp time')
         plt.legend()
-        plt.savefig(fn_root+'overall_failratefit.png')        
+        #plt.savefig(fn_root+'overall_failratefit.png')        
         plt.show()
         plt.clf()
 
