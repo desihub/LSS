302dfe30b0fde38c9227f24a59cc07d3222becd7
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-12-14 22:37:06 -0500

    Update mocksteps_data_cori.sh

--- scripts/mock_tools/mocksteps_data_cori.sh
+++ scripts/mock_tools/mocksteps_data_cori.sh
@@ -7,8 +7,8 @@ srun -N 1 -C haswell -t 04:00:00 -q interactive python run_mocks_multipass.py --
 
 python mkCat_mock.py --mockmin $1 --mockmax $2 --survey Y1 --combd y --combr y --combdr y --countran y --tracer dark --add_gtl y
 
-python mkCat_mock.py --tracer LRG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y -mkclusran_allpot y --mkclusdat_allpot y --nz y
+python mkCat_mock.py --tracer LRG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
 
-python mkCat_mock.py --tracer ELG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y -mkclusran_allpot y --mkclusdat_allpot y --nz y
+python mkCat_mock.py --tracer ELG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
 
-python mkCat_mock.py --tracer QSO --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y -mkclusran_allpot y --mkclusdat_allpot y --nz y
+python mkCat_mock.py --tracer QSO --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
