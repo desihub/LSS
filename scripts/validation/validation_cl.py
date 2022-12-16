424bb20c5bdf3041b93463eb4732ae200500ab84
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-12-16 15:07:41 -0500

    Create validation_cl.py

---
+++ scripts/validation/validation_cl.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import argparse
+
+import fitsio
+from astropy.table import join,Table
+import healpy as hp
+
+from LSS.imaging import densvar
+import LSS.common_tools as common
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", help="catalog version",default='test')
+parser.add_argument("--survey", help="e.g., main (for all), DA02, any future DA",default='SV3')
+parser.add_argument("--tracers", help="all runs all for given survey",default='all')
+parser.add_argument("--verspec",help="version for redshifts",default='fuji')
+parser.add_argument("--data",help="LSS or mock directory",default='LSS')
+parser.add_argument("--ps",help="point size for density map",default=1,type=float)
+parser.add_argument("--dpi",help="resolution in saved density map in dots per inch",default=90,type=int)
+args = parser.parse_args()
+
+
+indir = '/global/cfs/cdirs/desi/survey/catalogs/'+args.survey+'/'+args.data+'/'+args.verspec+'/LSScats/'+args.version+'/'
+outdir = indir+'plots/sky/'
+
+randir = '/global/cfs/cdirs/desi/target/catalogs/dr9/0.49.0/randoms/resolve/'
+ranall = fitsio.read(randir+'randoms-allsky-1-0.fits',columns=['RA','DEC'])
+th,phi = radec2thphi(ranall['RA'],ranall['DEC'])
+ranpix = hp.ang2pix(256,th,phi)
+ranpall = np.zeros(12*256*256)
+for pix in ranpix:
+    ranpall[pix] += 1.
+
+tps = [args.tracers]
+if args.tracers == 'all':
+    tps = ['QSO','LRG','BGS_BRIGHT','ELG_LOPnotqso']
+
+
+def get_delta(dat,ran,racol='RA',decol='DEC',wts=None,wtspix=None,thresh=0,nest=False,appfrac=True):#,ranpall=None
+    th,phi = radec2thphi(dat[racol],dat[decol])
+    datpix = hp.ang2pix(256,th,phi,nest=nest)
+    datp = np.zeros(12*256*256)
+    for i in range(0,len(datpix)):
+        pix = datpix[i]
+        if wts is not None:
+            datp[pix] += wts[i]
+        else:
+            datp[pix] += 1.
+    if wtspix is not None:
+        datp *= wtspix
+    th,phi = radec2thphi(ran[racol],ran[decol])
+    ranpix = hp.ang2pix(256,th,phi,nest=nest)
+    ranp = np.zeros(12*256*256)
+    for pix in ranpix:
+        ranp[pix] += 1.
+    #ranp /= rannorm
+    
+    sel = ranp > thresh
+    mnr = np.mean(datp[sel]/ranp[sel])
+    print(mnr)
+    delta = (datp/ranp/mnr -1)
+    #if ranpall is not None:
+    if appfrac:
+        if nest:
+            frac = ranp/ranpall_nest
+        else:
+            frac = ranp/ranpall
+        delta *= frac
+    delta[~sel] = hp.UNSEEN
+    fsky = np.sum(ranp[sel])/np.sum(ranpall)
+    return delta,fsky
+
+for tp in tps:
+    
+    dtf = fitsio.read(indir+tp+zdw+'_full.dat.fits')
+    ran = fitsio.read(indir+tp+zdw+'0_full.ran.fits')
+    #seld = dtf['PHOTSYS'] == reg
+    #dtf = dtf[seld]
+    sel_gz = common.goodz_infull(tp[:3],dtf)
+    sel_obs = dtf['ZWARN'] != 999999
+    dtfoz = dtf[sel_obs&sel_gz]
+    wt = 1./dtfoz['FRACZ_TILELOCID']*dtfoz['WEIGHT_ZFAIL']*dtfoz['WEIGHT_SYS']
+    sel_nan = wt*0 != 0
+    wt[sel_nan] = 1.
+    if tp[:3] == 'LRG':
+        zmin = 0.4
+        zmax = 1.1
+     if tp[:3] == 'BGS':
+        zmin = 0.1
+        zmax = 0.4
+    if tp[:3] == 'ELG':
+        zmin = 0.8
+        zmax = 1.6
+    if tp[:3] == 'QSO':
+        zmin = 0.8
+        zmax = 2.1
+
+    sel_zr = dtfoz['Z_not4clus'] > zmin
+    sel_zr &= dtfoz['Z_not4clus'] < zmax
+    delta_raw,fsky = get_delta(dtf,ran)
+    cl_raw = hp.anafast(delta_raw)
+    ell = np.arange(len(cl_raw))
+    delta_allz,_ = get_delta(dtfoz,ran,wts=wt)
+    cl_allz = hp.anafast(delta_allz)
+    delta_zr,_ = get_delta(dtfoz[sel_zr],ran,wts=wt[sel_zr])
+    cl_zr = hp.anafast(delta_zr)
+    plt.loglog(ell[1:],cl_LRG_raw[1:]/fsky,label='targets in Y1 area')
+    plt.loglog(ell[1:],cl_LRG_allz[1:]/fsky,label='all z')
+    plt.loglog(ell[1:],cl_LRG_zr[1:]/fsky,label=str(zmin)+' < z < '+str(zmax))
+    plt.title(tp)
+    plt.legend()
+    plt.xlabel(r'$\ell$')
+    plt.ylabel(r'$C_{\ell}$')
+    plt.savefig(outdir+tp+'_cell.png')
+    plt.clf()
+    
