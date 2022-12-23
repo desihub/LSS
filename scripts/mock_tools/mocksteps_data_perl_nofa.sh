4f2daabd6a5c76f6c747a61d9a931c12b0abdc3c
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2022-12-23 17:21:28 -0500

    Create mocksteps_data_perl_nofa.sh

---
+++ scripts/mock_tools/mocksteps_data_perl_nofa.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /global/common/software/desi/desi_environment.sh master
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+python mkCat_mock.py --mockmin $1 --mockmax $2 --survey Y1 --combd y --combr n --combdr y --countran y --tracer dark --add_gtl y
+
+python mkCat_mock.py --tracer LRG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
+
+python mkCat_mock.py --tracer ELG --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
+
+python mkCat_mock.py --tracer QSO --mockmin $1 --mockmax $2 --survey Y1 --fulld y --fullr y --apply_veto y --mkclusran y --mkclusdat y --mkclusran_allpot y --mkclusdat_allpot y --nz y
