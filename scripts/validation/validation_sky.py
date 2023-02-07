95d526b4090f0b301f74a0ba809fdd14e7ae979e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-02-07 14:02:10 -0500

    Update validation_sky.py

--- scripts/validation/validation_sky.py
+++ scripts/validation/validation_sky.py
@@ -87,6 +87,8 @@ for tp in tps:
 
 
     if args.data == 'LSS':
+        ral = []
+        sdecl = []
         for reg in regl:
             #dtf = fitsio.read(indir+tp+zdw+reg+'_clustering.dat.fits')
             #rf = indir+tp+zdw+reg+'_0_clustering.ran.fits'
@@ -175,30 +177,36 @@ for tp in tps:
             if args.survey != 'DA02':
                 wr = ra > 300
                 ra[wr] -=360
-            vx = 1.25
-            vm = 0.75
-            print(np.min(ra),np.max(ra),np.min(od),np.max(od))
-            nside_fac = (256/nside)**2.
-            size_fac = 2
+            ral.append(ra)
             sin_dec = np.sin(dec*np.pi/180)
-            yr = (np.max(sin_dec)-np.min(sin_dec))*1.05
-            xr = (np.max(ra)-np.min(ra))*1.1/90
-            xfac = 2.*size_fac
-            yfac = 2.3*size_fac
-            fig = plt.figure(figsize=(xr*xfac, yr*yfac))
-            ax = fig.add_subplot(111)
-            mp = plt.scatter(ra,sin_dec,c=od,edgecolor='none',vmax=vx,vmin=vm,s=args.ps*nside_fac*size_fac,marker='o')
-            ax.set_aspect(90)
-            plt.colorbar(mp, pad=0.01)
-            
-            plt.xlabel('RA')
-            plt.ylabel('sin(DEC)')
-            plt.title(titl)
-            plt.grid()
-
-
-            plt.savefig(outdir+tp+reg+'_weighteddens'+str(nside)+'.png',dpi=args.dpi)
-            plt.clf()
+            sdecl.append(sin_dec)
             del dtf
             del rt
-            print(tp+' done')
+            
+		ra = np.concatenate(ral)
+		sin_dec = np.concatenate(sdecl)
+		vx = 1.25
+		vm = 0.75
+		print(np.min(ra),np.max(ra),np.min(od),np.max(od))
+		nside_fac = (256/nside)**2.
+		size_fac = 2
+		
+		yr = (np.max(sin_dec)-np.min(sin_dec))*1.05
+		xr = (np.max(ra)-np.min(ra))*1.1/90
+		xfac = 2.*size_fac
+		yfac = 2.3*size_fac
+		fig = plt.figure(figsize=(xr*xfac, yr*yfac))
+		ax = fig.add_subplot(111)
+		mp = plt.scatter(ra,sin_dec,c=od,edgecolor='none',vmax=vx,vmin=vm,s=args.ps*nside_fac*size_fac,marker='o')
+		ax.set_aspect(90)
+		plt.colorbar(mp, pad=0.01)
+		
+		plt.xlabel('RA')
+		plt.ylabel('sin(DEC)')
+		plt.title(titl)
+		plt.grid()
+
+
+        plt.savefig(outdir+tp+'_weighteddens'+str(nside)+'.png',dpi=args.dpi)
+        plt.clf()
+        print(tp+' done')
