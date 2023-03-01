4500162ff3929ff98edfbd4e541f2389a2ce01a8
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-01 12:58:07 -0500

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -2592,7 +2592,7 @@ def add_zfail_weight2full(indir,tp='',tsnrcut=80,readpars=False):
         #    gal = func(surveys=[survey],specrels=[specrel],versions=[version],efftime_min=minefftime,efftime_max=maxefftime)
             
         if tp[:3] == 'ELG':
-            mod = ssr_tools_new.model_ssr_zfac(reg=reg,outdir=indir)
+            mod = ssr_tools_new.model_ssr_zfac(ff[selobs],reg=reg,outdir=indir)
         else:
             mod = ssr_tools_new.model_ssr(ff[selobs],tsnr_min=mintsnr,tsnr_max=maxtsnr,tracer=tp[:3],reg=reg,outdir=indir,band=band,outfn_root=tp,readpars=readpars)
         modl.append(mod)
