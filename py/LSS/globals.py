3e9903f2f0157585ddf9c731211c667b6a62e942
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-15 17:30:55 -0400

    Update globals.py

--- py/LSS/globals.py
+++ py/LSS/globals.py
@@ -138,4 +138,4 @@ class main:
             self.qsozf = '/global/cfs/cdirs/desi/survey/catalogs/Y1/QSO/'+specver+'/QSO_cat_iron_cumulative_v0.fits'
             #self.badfib = np.loadtxt('/global/cfs/cdirs/desi/survey/catalogs/DA02/LSS/guadalupe/lrg+bgs_3sig_bad_fibers.txt')
         self.darkbitweightfile = '/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/dark_bitweights_v0.fits'
-        self.brighbitweightfile = '/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/bright_bitweights_v0.fits'
+        self.brightbitweightfile = '/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/bright_bitweights_v0.fits'
