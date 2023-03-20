7dd334b9995722ab7fa32f27d87c8f20c08cf237
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-20 12:50:29 -0400

    Update prepare_mocks_main.py

--- scripts/mock_tools/prepare_mocks_main.py
+++ scripts/mock_tools/prepare_mocks_main.py
@@ -8,6 +8,8 @@ import h5py
 import argparse
 import sys
 
+from desitarget.targetmask import obsconditions
+
 if os.environ['NERSC_HOST'] == 'cori':
     scratch = 'CSCRATCH'
 elif os.environ['NERSC_HOST'] == 'perlmutter':
@@ -107,7 +109,7 @@ for real in range(args.realmin,args.realmax):
         targets['MWS_TARGET'] = np.zeros(n, dtype='i8')
         targets['SUBPRIORITY'] = np.random.uniform(0, 1, n)
         targets['BRICKNAME'] = np.full(n, '000p0000')    #- required !?!
-        targets['OBSCONDITIONS'] = np.zeros(n, dtype='i8')+int(3) 
+        targets['OBSCONDITIONS'] = obsconditions.mask(program.upper()) #np.zeros(n, dtype='i8')+int(3) 
         targets['NUMOBS_MORE'] = np.zeros(n, dtype='i8')+int(1) 
         targets['NUMOBS_INIT'] = np.zeros(n, dtype='i8')+int(1)
         targets['SCND_TARGET'] = np.zeros(n, dtype='i8')+int(0)
