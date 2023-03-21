d6d5ccdd5c28a97233e0534641676039a59dce42
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-21 12:10:15 -0400

    pipeline for when target spec join needs to be redone

---
+++ scripts/main/redo_daily_LSS_perlm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
+
+python $LSSCODE/LSS/scripts/main/combdata_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --prog dark --redotarspec y
+python $LSSCODE/LSS/scripts/main/combdata_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily --prog bright --redotarspec y
+
+$LSSCODE/LSS/scripts/main/daily_main_data_full.sh
+
+$LSSCODE/LSS/scripts/main/daily_main_randoms_all_noveto.sh
+
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --add_veto y --verspec daily --maxr 1
+
+$LSSCODE/LSS/scripts/main/daily_main_data1ran_vetos.sh > compnum_afterveto.out
+
+python $LSSCODE/LSS/scripts/main/write_daily_stats.py
