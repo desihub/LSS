ff36f4efec2906414b6f1039e8e8efecd14b8c7e
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-22 11:56:07 -0400

    Update mkCat_main.py

--- scripts/main/mkCat_main.py
+++ scripts/main/mkCat_main.py
@@ -48,6 +48,7 @@ parser.add_argument("--fulld", help="make the 'full' catalog containing info on 
 parser.add_argument("--add_veto", help="add veto column for given type, matching to targets",default='n')
 parser.add_argument("--join_etar", help="whether or not to join to the target files with extra brick pixel info",default='n')
 parser.add_argument("--apply_veto", help="apply vetos for imaging, priorities, and hardware failures",default='n')
+parser.add_argument("--mkHPmaps", help="make healpix maps for imaging properties using sample randoms",default='n')
 parser.add_argument("--fillran", help="add imaging properties to randoms",default='n')
 parser.add_argument("--clusd", help="make the 'clustering' catalog intended for paircounts",default='n')
 parser.add_argument("--clusran", help="make the random clustering files; these are cut to a small subset of columns",default='n')
@@ -289,12 +290,15 @@ if args.join_etar == 'y':
     fin = dirout+type+notqso+'_full_noveto.dat.fits'
     common.join_etar(fin,type)
 
+new_cols=['BETA_ML','BETA_RMS','HI','HALPHA', 'HALPHA_ERROR', 'CALIB_G', 'CALIB_R', 'CALIB_Z', 'EBV_MPF_Mean_FW15', 'EBV_MPF_Mean_ZptCorr_FW15', 'EBV_MPF_Var_FW15', 'EBV_MPF_VarCorr_FW15', 'EBV_MPF_Mean_FW6P1', 'EBV_MPF_Mean_ZptCorr_FW6P1', 'EBV_MPF_Var_FW6P1', 'EBV_MPF_VarCorr_FW6P1', 'EBV_SGF14', 'BETA_ML', 'BETA_MEAN', 'BETA_RMS', 'HI', 'KAPPA_PLANCK']
+fid_cols=['EBV','PSFDEPTH_G','PSFDEPTH_R','PSFDEPTH_Z','GALDEPTH_G','GALDEPTH_R','GALDEPTH_Z','PSFDEPTH_W1','PSFDEPTH_W2','PSFSIZE_G','PSFSIZE_R','PSFSIZE_Z']
+allmapcol = new_cols+fid_cols
 if args.fillran == 'y':
     print('filling randoms with imaging properties')
     for ii in range(rm,rx):
         fn = dirout+type+notqso+'_'+str(ii)+'_full_noveto.ran.fits'
         #ct.addcol_ran(fn,ii)
-        common.add_map_cols(fn,ii)
+        common.add_map_cols(fn,ii,new_cols=new_cols,fid_cols=fid_cols)
         print('done with '+str(ii))
 
 
@@ -349,6 +353,24 @@ regl = ['_N','_S']
 if mkclusdat:
     ct.mkclusdat(dirout+type+notqso,tp=type,dchi2=dchi2,tsnrcut=tsnrcut,zmin=zmin,zmax=zmax,ccut=ccut)#,ntilecut=ntile)
 
+
+if args.mkHPmaps:
+    from LSS.imaging.sky_maps import create_pixweight_file, rancat_names_to_pixweight_name
+    lssmapdirout = dirout+'/hpmaps/'
+    if not os.path.exists(lssmapdir):
+        os.mkdir(lssmapdirout)
+        print('made '+lssmapdirout)
+    lssmapdir = '/global/cfs/cdirs/desi/survey/catalogs/external_input_maps/'
+    rancatname = dirout+tracer_clus+'_*_full.ran.fits'
+    rancatlist = sorted(glob(rancatname))
+    fieldslist = allmapcols
+    masklist = np.zeros(len(fieldslist))
+    nside = 256
+    outfn = lssmapdirout+tracer_clus+'_mapprops_healpix_nested_nside'+str(nside)+'.fits'
+    create_pixweight_file(rancatlist, fieldslist, masklist, nside_out=nside,
+                          lssmapdir=lssmapdir, outfn=None)    
+
+    
 
 rcols=['Z','WEIGHT','WEIGHT_SYS','WEIGHT_COMP','WEIGHT_ZFAIL']#,'WEIGHT_FKP']#,'WEIGHT_RF']
 if type[:3] == 'BGS':
