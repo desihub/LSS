42be81ec5f43f022261c88d50453ec6daa6e65f8
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-22 17:26:31 -0400

    Update sky_maps.py

--- py/LSS/imaging/sky_maps.py
+++ py/LSS/imaging/sky_maps.py
@@ -1523,10 +1523,11 @@ def create_pixweight_file(randomcatlist, fieldslist, masklist, nside_out=512,
     for field in fieldslist:
         ii = counts[field] > 0
         print(field,np.sum(wcounts[ii][field]),np.sum(counts[ii][field]),np.sum(wcounts[ii][field])/np.sum(counts[ii][field]),np.mean(wcounts[ii][field]/counts[ii][field]))
+        print(np.mean(wcounts[ii][field]))
         wcounts[ii][field] = wcounts[ii][field] / counts[ii][field]
         print(np.mean(wcounts[ii][field]))
-        #wcounts[counts[field] == 0][field] = hp.UNSEEN
-        #print(np.mean(wcounts[ii][field]))
+        wcounts[counts[field] == 0][field] = hp.UNSEEN
+        print(np.mean(wcounts[ii][field]))
 
     # MMM Write atomically (sanity check done before).
     if write:
