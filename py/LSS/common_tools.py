dbda72f21f4dac773996bd4c7c74564a4d6497f4
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-03-24 13:42:36 -0400

    Update common_tools.py

--- py/LSS/common_tools.py
+++ py/LSS/common_tools.py
@@ -399,7 +399,7 @@ def addFKPfull(fb,nz,tp,bs=0.01,zmin=0.01,zmax=1.6,P0=10000,add_data=True):
     zl = fd['Z_not4clus']
     zind = ((zl-zmin)/bs).astype(int)
     gz &= zl > zmin
-    gz &= zl < zmin
+    gz &= zl < zmax
     gz &= fd['ZWARN'] != 999999
     print(np.min(fd[gz]['FRACZ_TILELOCID']),np.max(fd[gz]['FRACZ_TILELOCID']))
     nl = np.zeros(len(fd))
