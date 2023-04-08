2dee1c6ef6eee42919e35ebad819aab8348248c5
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-04-07 20:38:36 -0400

    Create Y1all_full.sh

---
+++ scripts/main/Y1all_full.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#make sure to set $LSSCODE to wherever the LSS git repo is (e.g., $HOME)
+#provide the version, e.g. Y1all_full.sh 0.1
+source /global/common/software/desi/desi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+
+python $LSSCODE/LSS/scripts/main/combdata_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --prog dark --survey Y1 --counts_only y --dospec n
+python $LSSCODE/LSS/scripts/main/combdata_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --prog bright --survey Y1 -- counts_only y --dospec n
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_Y1ran_px.py  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --type dark 
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_Y1ran_px.py  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --type bright 
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_Y1ran_px.py  --type ELG_LOP  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --rfa n --combhp n --combfull y --fullr y --notqso y --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_Y1ran_px.py  --type LRG  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --rfa n --combhp n --combfull y --fullr y  --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_Y1ran_px.py  --type QSO  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --rfa n --combhp n --combfull y --fullr y  --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_Y1ran_px.py  --type BGS_BRIGHT  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --rfa n --combhp n --combfull y --fullr y  --version $1
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_ran.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --notqso n --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_ran.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --notqso y --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_ran.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/mkCat_main_ran.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --add_veto y --fillran y --apply_veto y --verspec daily --survey Y1 --version $1
+
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --add_veto y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --notqso y --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --verspec iron --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --add_ke y --survey Y1 --version $1
+
+python $LSSCODE/LSS/scripts/plotting/densvs_improb_tpixw.py --version $1
+python $LSSCODE/LSS/scripts/plotting/validation_tsnr_zbin.py --version $1
+python $LSSCODE/LSS/scripts/plotting/validation_sky.py --version $1
+python $LSSCODE/LSS/scripts/plotting/validation_fiber.py --version $1
+python $LSSCODE/LSS/scripts/plotting/validation_focal.py --version $1
+python $LSSCODE/LSS/scripts/plotting/validation_cl.py --version $1
