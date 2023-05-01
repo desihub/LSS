6e127aa80702b42daafccc742122bae238780588
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-05-01 15:54:41 -0400

    Create testmaskpota.py

---
+++ Sandbox/testmaskpota.py
@@ -0,0 +1,98 @@
+'''
+This is a little demo script for the Assignment.check_avail_collisions() function.
+'''
+
+import numpy as np
+
+from astropy.table import Table
+
+from fiberassign.hardware import load_hardware
+from fiberassign.tiles import load_tiles
+from fiberassign.targets import Targets, TargetsAvailable, LocationsAvailable, create_tagalong, load_target_file, targets_in_tiles
+from fiberassign.assign import Assignment
+
+from fiberassign.utils import Logger
+
+import fitsio
+
+t = Table.read('/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/tiles-DARK.fits')
+print('tiles:', t)
+
+margins = dict(pos=0.05,
+                   petal=0.4,
+                   gfa=0.4)
+
+
+#def main():
+
+    # from LSS.mkCat_singletile.fa4lsscat import getfatiles
+    # getfatiles()
+    # return
+log = Logger.get()
+
+n = 0
+for tile in t['TILEID']:    
+
+
+    #tile = 1230
+    ts = '%06i' % tile
+
+    fbah = fitsio.read_header('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz')
+    dt = fbah['RUNDATE'][:19]
+    hw = load_hardware(rundate=dt, add_margins=margins)
+    pr = 'DARK'
+    t['OBSCONDITIONS'] = 516
+    t['IN_DESI'] = 1
+    t['MTLTIME'] = fbah['MTLTIME']
+    t['FA_RUN'] = fbah['FA_RUN']
+    t['PROGRAM'] = pr
+
+    t.write('tiles.fits', overwrite=True)
+
+    tiles = load_tiles(
+        tiles_file='tiles.fits',
+        select=[tile])
+
+    tids = tiles.id
+    print('Tile ids:', tids)
+    I = np.flatnonzero(np.array(tids) == tile)
+    assert(len(I) == 1)
+    i = I[0]
+    tile_ra  = tiles.ra[i]
+    tile_dec = tiles.dec[i]
+
+    # Create empty target list
+    tgs = Targets()
+    # Create structure for carrying along auxiliary target data not needed by C++.
+    plate_radec=True
+    tagalong = create_tagalong(plate_radec=plate_radec)
+
+    # Load target files...
+    load_target_file(tgs, tagalong, '/global/cfs/cdirs/desi/survey/catalogs/main/LSS/random0/tilenofa-%i.fits' % tile)
+
+    # Find targets within tiles, and project their RA,Dec positions
+    # into focal-plane coordinates.
+    tile_targetids, tile_x, tile_y, tile_xy_cs5 = targets_in_tiles(hw, tgs, tiles, tagalong)
+    # Compute the targets available to each fiber for each tile.
+    tgsavail = TargetsAvailable(hw, tiles, tile_targetids, tile_x, tile_y)
+    # Compute the fibers on all tiles available for each target and sky
+    favail = LocationsAvailable(tgsavail)
+
+    # FAKE stucksky
+    stucksky = {}
+
+    # Create assignment object
+    asgn = Assignment(tgs, tgsavail, favail, stucksky)
+
+    coll = asgn.check_avail_collisions(tile)
+
+    #print('collisions:', coll)
+    print('N collisions:', len(coll))
+    # coll: dict (loc, targetid) -> bitmask
+    forig = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/main/LSS/random0/fba-%i.fits' % tile,ext='FAVAIL')
+    print(coll)
+    
+    n += 1
+    if n >= 1:
+        break
+
