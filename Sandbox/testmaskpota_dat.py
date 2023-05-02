957aca601c394e7d5b54a23615249dbf295a5b01
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-05-01 22:39:08 -0400

    Update testmaskpota_dat.py

--- Sandbox/testmaskpota_dat.py
+++ Sandbox/testmaskpota_dat.py
@@ -38,87 +38,87 @@ for tile in t['TILEID']:
     ts = '%06i' % tile
 
     fbah = fitsio.read_header('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz')
-    if fbah['FA_VER'][0] == '5':
-        dt = fbah['RUNDATE'][:19]
-        hw = load_hardware(rundate=dt, add_margins=margins)
-        t = Table(fitsio.read('/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-tiles.fits'))
-        t['MTLTIME'] = fbah['MTLTIME']
-        t['FA_RUN'] = fbah['FA_RUN']
-        obsha = fbah['FA_HA']
-        obstheta = fbah['FIELDROT']
+    #if fbah['FA_VER'][0] == '5':
+    dt = fbah['RUNDATE'][:19]
+    hw = load_hardware(rundate=dt, add_margins=margins)
+    t = Table(fitsio.read('/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-tiles.fits'))
+    t['MTLTIME'] = fbah['MTLTIME']
+    t['FA_RUN'] = fbah['FA_RUN']
+    obsha = fbah['FA_HA']
+    obstheta = fbah['FIELDROT']
 
 
-        t.write('tiles.fits', overwrite=True)
+    t.write('tiles.fits', overwrite=True)
 
-        tiles = load_tiles(
-            tiles_file='tiles.fits',obsha=obsha,obstheta=obstheta,
-            select=[tile])
+    tiles = load_tiles(
+        tiles_file='tiles.fits',obsha=obsha,obstheta=obstheta,
+        select=[tile])
 
-        tids = tiles.id
-        print('Tile ids:', tids)
-        I = np.flatnonzero(np.array(tids) == tile)
-        assert(len(I) == 1)
-        i = I[0]
-        tile_ra  = tiles.ra[i]
-        tile_dec = tiles.dec[i]
+    tids = tiles.id
+    print('Tile ids:', tids)
+    I = np.flatnonzero(np.array(tids) == tile)
+    assert(len(I) == 1)
+    i = I[0]
+    tile_ra  = tiles.ra[i]
+    tile_dec = tiles.dec[i]
 
-        # Create empty target list
-        tgs = Targets()
-        # Create structure for carrying along auxiliary target data not needed by C++.
-        plate_radec=True
-        tagalong = create_tagalong(plate_radec=plate_radec)
+    # Create empty target list
+    tgs = Targets()
+    # Create structure for carrying along auxiliary target data not needed by C++.
+    plate_radec=True
+    tagalong = create_tagalong(plate_radec=plate_radec)
 
-        # Load target files...
-        load_target_file(tgs, tagalong, '/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-targ.fits')
-        ttids = fitsio.read('/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-targ.fits')['TARGETID']
-    
+    # Load target files...
+    load_target_file(tgs, tagalong, '/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-targ.fits')
+    ttids = fitsio.read('/global/cfs/cdirs/desi/survey/fiberassign/main/'+ts[:3]+'/'+ts+'-targ.fits')['TARGETID']
 
-        # Find targets within tiles, and project their RA,Dec positions
-        # into focal-plane coordinates.
-        tile_targetids, tile_x, tile_y, tile_xy_cs5 = targets_in_tiles(hw, tgs, tiles, tagalong)
-        # Compute the targets available to each fiber for each tile.
-        tgsavail = TargetsAvailable(hw, tiles, tile_targetids, tile_x, tile_y)
-        # Compute the fibers on all tiles available for each target and sky
-        favail = LocationsAvailable(tgsavail)
 
-        # FAKE stucksky
-        stucksky = {}
+    # Find targets within tiles, and project their RA,Dec positions
+    # into focal-plane coordinates.
+    tile_targetids, tile_x, tile_y, tile_xy_cs5 = targets_in_tiles(hw, tgs, tiles, tagalong)
+    # Compute the targets available to each fiber for each tile.
+    tgsavail = TargetsAvailable(hw, tiles, tile_targetids, tile_x, tile_y)
+    # Compute the fibers on all tiles available for each target and sky
+    favail = LocationsAvailable(tgsavail)
 
-        # Create assignment object
-        asgn = Assignment(tgs, tgsavail, favail, stucksky)
+    # FAKE stucksky
+    stucksky = {}
 
-        coll = asgn.check_avail_collisions(tile)
-        kl = np.array(list(coll.keys())).transpose()
-        locs = kl[0]
-        ids = kl[1]
-        #sel = np.isin(ids,ttids)
-        #locs = locs[sel]
-        #ids = ids[sel]
-        #print('collisions:', coll)
-        print('N collisions:', len(coll))
-        # coll: dict (loc, targetid) -> bitmask
-        forig = fitsio.read('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz',ext='POTENTIAL_ASSIGNMENTS')
-        #print(coll)
-        selo = np.isin(forig['TARGETID'],ttids)
-        forig = forig[selo]
-        locsin = np.isin(forig['LOCATION'],locs)
-        idsin = np.isin(forig['TARGETID'],ids)
-        masked = locsin&idsin
-        print(np.sum(locsin),np.sum(idsin),np.sum(masked),len(forig))
-        print('checking actual assignments, should not find any are masked')
-        forig = fitsio.read('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz',ext='FIBERASSIGN')
-        #print(coll)
-        locsin = np.isin(forig['LOCATION'],locs)
-        idsin = np.isin(forig['TARGETID'],ids)
-        masked = locsin&idsin
-        print(np.sum(masked))
-        if np.sum(masked) != 0:
-            print('BAD, assigned id/location is in mask')
-            print(forig[masked])
+    # Create assignment object
+    asgn = Assignment(tgs, tgsavail, favail, stucksky)
 
-        n += 1
-    else:
-        print(ts,fbah['FA_VER'])
+    coll = asgn.check_avail_collisions(tile)
+    kl = np.array(list(coll.keys())).transpose()
+    locs = kl[0]
+    ids = kl[1]
+    #sel = np.isin(ids,ttids)
+    #locs = locs[sel]
+    #ids = ids[sel]
+    #print('collisions:', coll)
+    print('N collisions:', len(coll))
+    # coll: dict (loc, targetid) -> bitmask
+    forig = fitsio.read('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz',ext='POTENTIAL_ASSIGNMENTS')
+    #print(coll)
+    selo = np.isin(forig['TARGETID'],ttids)
+    forig = forig[selo]
+    locsin = np.isin(forig['LOCATION'],locs)
+    idsin = np.isin(forig['TARGETID'],ids)
+    masked = locsin&idsin
+    print(np.sum(locsin),np.sum(idsin),np.sum(masked),len(forig))
+    print('checking actual assignments, should not find any are masked')
+    forig = fitsio.read('/global/cfs/cdirs/desi/target/fiberassign/tiles/trunk/'+ts[:3]+'/fiberassign-'+ts+'.fits.gz',ext='FIBERASSIGN')
+    #print(coll)
+    locsin = np.isin(forig['LOCATION'],locs)
+    idsin = np.isin(forig['TARGETID'],ids)
+    masked = locsin&idsin
+    print(np.sum(masked))
+    if np.sum(masked) != 0:
+        print('BAD, assigned id/location is in mask')
+        print(forig[masked])
+
+    n += 1
+    #else:
+    #    print(ts,fbah['FA_VER'])
     if n >= 10:
         break
 
