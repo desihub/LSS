d57e12fce3f61e8843836421bb8c18f6f4568c67
Aurelio Carnero Rosell <aurelio.crosell@gmail.com>
2023-05-05 06:46:00 -0700

    More options to fix_SV3

--- scripts/SV3/fix_SV3_columns_toEDR.py
+++ scripts/SV3/fix_SV3_columns_toEDR.py
@@ -3,12 +3,12 @@ import pandas as pd
 from astropy.table import Table
 from astropy.io import fits
 import numpy as np
-
+import glob
 #This script modifies SV3 products to meet the datamodel standards. It substitutes the files indicated in the specified directory, 
 #Therefore, be careful. Suggestion is to work on a copy of the original files.
 
 #This is the directory where we make the modifications. 
-dir_ = '/global/cfs/cdirs/desi/survey/catalogs/edr_prepfor_public'
+dir_ = '/global/cfs/cdirs/desi/survey/catalogs/edr_prepfor_public/fuji/sv3/v1'
 
 #Here it reads all the directory tree structure
 directories = [x[0] for x in os.walk(dir_)]
@@ -32,6 +32,7 @@ names_col_official = list(columns_official.Name)
 #6) Add units to columns in fits files
 #7) Add header comment redirecting to readthedocs
 #8) Remove columns from specific files or directory
+#9) Remove specific header keys from files
 
 check_columns = True
 change_names = False
@@ -40,7 +41,10 @@ remove_columns = False
 define_extname = False
 add_units = False
 add_comment_header = False
+add_desidr_header = False
 remove_specific_columns = False
+remove_specific_headers = False
+check_extname = False
 
 ''' to check if there are columns not defined in description files '''
 if check_columns:
@@ -68,6 +72,15 @@ if check_columns:
     else:
         print('Some columns are not described in column_description. Check with the WG if those need to be removed')
         print('or if they need to be add to column_descriptions')
+
+'''check files extnames'''
+if check_extname:
+    for root, dirs, files in os.walk(dir_):
+        for file in files:
+            if file.endswith(".fits") and not file.startswith("fba"):
+                filename = os.path.join(root,file)
+                hdul = fits.open(filename)
+                print(filename, hdul.info())
 
 ''' Change column names '''
 if change_names:
@@ -129,7 +142,7 @@ if change_types:
 if remove_columns:
     print('Removing columns')
 
-    columns_to_remove = ['ZERR_QF', 'TSNR2_LYA_QF', 'TSNR2_QSO_QF', 'Z_QN_QF', 'QSO_MASKBITS', 'elg_mask', 'sort', 'LOCFULL', 'FRACFLUX_G', 'FRACFLUX_R', 'FRACFLUX_Z', 'FRACMASKED_G', 'FRACMASKED_R', 'FRACMASKED_Z', 'FRACIN_G', 'FRACIN_R', 'FRACIN_Z', 'ALLMASK_G', 'ALLMASK_R', 'ALLMASK_Z', 'Z_HP', 'DELTACHI2_HP', 'TSNR2_ELG_HP', 'TSNR2_BGS_HP', 'TSNR2_QSO_HP', 'TSNR2_LRG_HP']
+    columns_to_remove = ['ZERR_QF', 'TSNR2_LYA_QF', 'TSNR2_QSO_QF', 'Z_QN_QF', 'QSO_MASKBITS', 'elg_mask', 'sort', 'LOCFULL', 'FRACFLUX_G', 'FRACFLUX_R', 'FRACFLUX_Z', 'FRACMASKED_G', 'FRACMASKED_R', 'FRACMASKED_Z', 'FRACIN_G', 'FRACIN_R', 'FRACIN_Z', 'ALLMASK_G', 'ALLMASK_R', 'ALLMASK_Z', 'DELTACHI2_HP', 'TSNR2_ELG_HP', 'TSNR2_BGS_HP', 'TSNR2_QSO_HP', 'TSNR2_LRG_HP']
     count=0
     for root, dirs, files in os.walk(dir_):
         for file in files:
@@ -161,18 +174,6 @@ if remove_columns:
     else:
         print(count, ' files have been affected')
 
-''' Add extension name to given files '''
-if define_extname:
-    print('Setting extension name to files')
-
-    extname_files = {'rancomb_bright_Alltilelocinfo.fits':[1,'TILELOC'], 'rancomb_dark_Alltilelocinfo.fits':[1,'TILELOC']}
-    for root, dirs, files in os.walk(dir_):
-        for file in files:
-            if file in extname_files.keys(): 
-                filename = os.path.join(root,file)
-                fits.setval(filename, 'EXTNAME', value=extname_files[file][1], ext=extname_files[file][0])
-                print(filename, ' EXTNAME changed')
-
 ''' Add units to columns in fits files '''
 if add_units:
     print('Adding units to columns')
@@ -189,6 +190,7 @@ if add_units:
                     for colindata in data_to_read.columns.names:
                         index_column = np.argwhere(np.array(columns_official.Name)==colindata)[0][0]
                         if data_to_read.columns[colindata].unit != columns_official.Units[index_column] and len(columns_official.Units[index_column])!=0:
+                            print(file,colindata)
                             data_to_read.columns[colindata].unit = columns_official.Units[index_column]
                             count+=1
                 hdul.close() 
@@ -298,3 +300,100 @@ if remove_specific_columns:
         print(count, ' files have been affected')
 
 
+'''Remove specidif header keys from files'''
+if remove_specific_headers:
+    print('removing headers from files')
+    
+    headers_to_remove = {'datcomb_dark_tarwdup_Alltiles.fits': ['TILEID', 'TILERA', 'TILEDEC', 'FIELDROT','FA_PLAN','FA_HA','FA_RUN',
+                                                                'REQRA','REQDEC','FIELDNUM','FA_VER','FA_SURV','DEPNAM00','DEPVER00',
+                                                                'DEPNAM01','DEPVER01','DEPNAM02','DEPVER02','DEPNAM03','DEPVER03',
+                                                                'DEPNAM04','DEPVER04','DEPNAM05','DEPVER05','DEPNAM06','DEPVER06',
+                                                                'DEPNAM07','DEPVER07','DEPNAM08','DEPVER08'], 
+                         'rancomb_darkwdup_Alltiles.fits': ['TILEID', 'TILERA', 'TILEDEC', 'FIELDROT','FA_PLAN','FA_HA','FA_RUN',
+                                                                'REQRA','REQDEC','FIELDNUM','FA_VER','FA_SURV','DEPNAM00','DEPVER00',
+                                                                'DEPNAM01','DEPVER01','DEPNAM02','DEPVER02','DEPNAM03','DEPVER03',
+                                                                'DEPNAM04','DEPVER04','DEPNAM05','DEPVER05','DEPNAM06','DEPVER06',
+                                                                'DEPNAM07','DEPVER07','DEPNAM08','DEPVER08'],
+                         'rancomb_brightwdup_Alltiles.fits': ['TILEID', 'TILERA', 'TILEDEC', 'FIELDROT','FA_PLAN','FA_HA','FA_RUN',
+                                                                'REQRA','REQDEC','FIELDNUM','FA_VER','FA_SURV','DEPNAM00','DEPVER00',
+                                                                'DEPNAM01','DEPVER01','DEPNAM02','DEPVER02','DEPNAM03','DEPVER03',
+                                                                'DEPNAM04','DEPVER04','DEPNAM05','DEPVER05','DEPNAM06','DEPVER06',
+                                                                'DEPNAM07','DEPVER07','DEPNAM08','DEPVER08']
+                         }
+    input_wspec_list = ['TILEID', 'TILERA', 'TILEDEC', 'FIELDROT','FA_PLAN','FA_HA','FA_RUN',
+                                                                'REQRA','REQDEC','FIELDNUM','FA_VER','FA_SURV','DEPNAM00','DEPVER00',
+                                                                'DEPNAM01','DEPVER01','DEPNAM02','DEPVER02','DEPNAM03','DEPVER03',
+                                                                'DEPNAM04','DEPVER04','DEPNAM05','DEPVER05','DEPNAM06','DEPVER06',
+                                                                'DEPNAM07','DEPVER07','DEPNAM08','DEPVER08','LONGSTRN','RRVER','TEMNAM00',
+                                                                'TEMVER00','TEMNAM01','TEMVER01','TEMNAM02','TEMVER02','TEMNAM03','TEMVER03',
+                                                                'TEMNAM04','TEMVER04','TEMNAM05','TEMVER05','TEMNAM06','TEMVER06','TEMNAM07',
+                                                                'TEMVER07','TEMNAM08','TEMVER08','TEMNAM09','TEMVER09','DEPNAM09','DEPVER09',
+                                                                'DEPNAM10','DEPVER10','DEPNAM11','DEPVER11','DEPNAM12','DEPVER12','DEPNAM13',
+                                                                'DEPVER13','DEPNAM14','DEPVER14','DEPNAM15','DEPVER15','DEPNAM16','DEPVER16',
+                                                                'DEPNAM17','DEPVER17','DEPNAM18','DEPVER18','SPGRP','SURVEY','PROGRAM']
+    name_prep = 'rancomb_{NUMBER}{PROGRAM}wdupspec_Alltiles.fits'
+
+    for i in range(18):
+        for j in ['dark','bright']:
+            headers_to_remove[name_prep.format(NUMBER=i, PROGRAM=j)] = input_wspec_list
+
+    headers_to_remove['datcomb_bright_tarspecwdup_Alltiles.fits'] = input_wspec_list
+    headers_to_remove['datcomb_dark_tarspecwdup_Alltiles.fits'] = input_wspec_list
+
+    for root, dirs, files in os.walk(dir_):
+        for file in files:
+            if file in list(headers_to_remove.keys()):
+                filename = os.path.join(root,file)
+                hdul = fits.open(filename, mode='update')
+
+                for header in headers_to_remove[file]:
+                    if header in list(hdul[1].header.keys()):
+                        del hdul[1].header[header]
+
+                hdul.close()
+
+''' Add extension name to given files '''
+if define_extname:
+    print('Setting extension name to files')
+
+    extname_files = {'rancomb_bright_Alltilelocinfo.fits':[1,'TILELOC'], 'rancomb_dark_Alltilelocinfo.fits':[1,'TILELOC']}
+    d=glob.glob('/global/cfs/cdirs/desi/survey/catalogs/edr_prepfor_public/LSScats/full/*')
+    for dd in d:
+        extname_files[dd.split('/')[-1]] = [1,'LSS']
+
+    for root, dirs, files in os.walk(dir_):
+        for file in files:
+            if file in extname_files.keys(): 
+                filename = os.path.join(root,file)
+                fits.setval(filename, 'EXTNAME', value=extname_files[file][1], ext=extname_files[file][0])
+                print(filename, ' EXTNAME changed')
+
+
+''' Add DESIDR to header for EDR release '''
+if add_desidr_header:
+    print('Adding DESIDR to header')
+    count=0
+    for root, dirs, files in os.walk(dir_):
+        for file in files:
+            if file.endswith(".fits"):
+                filename = os.path.join(root,file)
+                hdul = fits.open(filename)
+                hh_new = []
+                for i in range(len(hdul)-1):
+                    hindex = i+1
+                    head_temp = hdul[hindex].header
+                    to_update = True
+                    if 'DESIDR' in list(head_temp.keys()):
+                        continue
+                    else:
+                        hh_new.append(hindex)
+                hdul.close()
+
+                if len(hh_new) == 0:
+                    continue
+                else:
+                    count += 1
+                    for i in hh_new:
+                        fits.setval(filename, 'DESIDR', value='edr', ext=i)
+
+    print(count, 'files affected')
