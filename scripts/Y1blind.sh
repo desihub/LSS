5e14eed1a2b46aed755aaed8f088d3119574a47d
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-05-08 15:03:52 -0400

    Update Y1blind.sh

--- scripts/Y1blind.sh
+++ scripts/Y1blind.sh
@@ -6,5 +6,5 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 module swap pyrecon/main pyrecon/mpi
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py 
 
-srun -N 1 -C cpu -t 00:30:00 -q interactive python scripts/main/apply_blinding_main_fromfile_fcomp.py --type LRG --baoblind y --mkclusdat y --mkclusran y --minr 0 --maxr 18 --version v0.1 --useMPI n
-srun -n 128 -N 1 -C cpu -t 01:00:00 -q interactive python scripts/main/apply_blinding_main_fromfile_fcomp.py --dorecon y --rsdblind y --fnlblind y --useMPI y --version v0.1 --minr 0 --maxr 18 --type LRG
+srun -N 1 -C cpu -t 00:30:00 -q interactive python scripts/main/apply_blinding_main_fromfile_fcomp.py --type QSO --baoblind y --mkclusdat y --mkclusran y --minr 0 --maxr 18 --version v0.1 --useMPI n
+srun -n 128 -N 1 -C cpu -t 01:00:00 -q interactive python scripts/main/apply_blinding_main_fromfile_fcomp.py --dorecon y --rsdblind y --fnlblind y --useMPI y --version v0.1 --minr 0 --maxr 18 --type QSO
