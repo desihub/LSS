81ccd33be729d95a81d231d8a4163b9471b498f4
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-05-08 19:00:19 -0400

    Update apply_blinding_main_fromfile_fcomp.py

--- scripts/main/apply_blinding_main_fromfile_fcomp.py
+++ scripts/main/apply_blinding_main_fromfile_fcomp.py
@@ -241,7 +241,7 @@ if root:
     fb_in = dirin + type + notqso
     fbr_in = fb_in
     if type == 'BGS_BRIGHT-21.5':
-        fbr_in = 'BGS_BRIGHT'
+        fbr_in = dirin +'BGS_BRIGHT'
     fcr_in = fbr_in + '_1_full.ran.fits'
     fcd_in = fb_in + '_full.dat.fits'
     nzf_in = dirin + type + notqso + '_full_nz.txt'
