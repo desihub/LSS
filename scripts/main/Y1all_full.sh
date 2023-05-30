d60a5a29acc0209e3e666eb3eec215bd943507d0
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-05-30 17:24:31 -0400

    Update Y1all_full.sh

--- scripts/main/Y1all_full.sh
+++ scripts/main/Y1all_full.sh
@@ -17,32 +17,35 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 #srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_Y1ran_px.py  --type QSO  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --combhp n --combfull y --fullr y  --version $1
 #srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_Y1ran_px.py  --type BGS_BRIGHT  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec iron --combhp n --combfull y --fullr y  --version $1
 
-#srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --notqso n --survey Y1 --version $1
-#srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --notqso y --survey Y1 --version $1
-#srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --fillran y --apply_veto y --verspec iron --survey Y1 --version $1
-#srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr n --add_veto y --fillran y --apply_veto y --verspec iron --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr y --fillran y --apply_veto y --verspec iron --notqso n --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr y --fillran y --apply_veto y --verspec iron --notqso y --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr y --fillran y --apply_veto y --verspec iron --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fullr y --add_veto y --fillran y --apply_veto y --verspec iron --survey Y1 --version $1
 
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --add_veto y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --notqso y --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --add_veto y   --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --apply_veto y --mkHPmaps y --verspec iron --survey Y1 --notqso y --maxr 0 --regressis y --add_regressis y --add_weight_zfail y --version $1
 
-#srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --verspec iron --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --add_ke y --survey Y1 --version $1
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --verspec iron --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n  --add_ke y --survey Y1 --version $1
 
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --FKPfull y  --verspec iron --survey Y1 --maxr 0  --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y  --verspec iron --survey Y1 --maxr 0 --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y  --verspec iron --survey Y1 --maxr 0  --version $1
-#python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y --verspec iron --survey Y1 --notqso y --maxr 0 --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --FKPfull y  --verspec iron --survey Y1 --maxr 0  --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT-21.5 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --FKPfull y  --verspec iron --survey Y1 --maxr 0  --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y  --verspec iron --survey Y1 --maxr 0 --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y  --verspec iron --survey Y1 --maxr 0  --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --FKPfull y --verspec iron --survey Y1 --notqso y --maxr 0 --version $1
 
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --nzfull y  --verspec iron --survey Y1 --maxr 0  --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT-21.5 --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n   --nzfull y  --verspec iron --survey Y1 --maxr 0  --version $1
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --nzfull y  --verspec iron --survey Y1 --maxr 0 --version $1
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --nzfull y  --verspec iron --survey Y1 --maxr 0  --version $1
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --nzfull y --verspec iron --survey Y1 --notqso y --maxr 0 --version $1
 
 
-#python $LSSCODE/LSS/scripts/plotting/densvs_improp_tpixw.py --version $1
-#python $LSSCODE/LSS/scripts/validation/validation_tsnr_zbin.py --version $1
-#python $LSSCODE/LSS/scripts/validation/validation_sky.py --version $1
-#python $LSSCODE/LSS/scripts/validation/validation_fiber.py --version $1
-#python $LSSCODE/LSS/scripts/validation/validation_focal.py --version $1
-#python $LSSCODE/LSS/scripts/validation/validation_cl.py --version $1
+python $LSSCODE/LSS/scripts/plotting/densvs_improp_tpixw.py --version $1
+python $LSSCODE/LSS/scripts/validation/validation_tsnr_zbin.py --version $1
+python $LSSCODE/LSS/scripts/validation/validation_sky.py --version $1
+python $LSSCODE/LSS/scripts/validation/validation_fiber.py --version $1
+python $LSSCODE/LSS/scripts/validation/validation_focal.py --version $1
+python $LSSCODE/LSS/scripts/validation/validation_cl.py --version $1
