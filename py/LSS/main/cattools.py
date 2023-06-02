780b83bbbaa3a4dc00b07e4ad7dbc8203f178e50
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-06-02 17:26:57 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -3307,7 +3307,7 @@ def clusran_resamp(flin,rann,rcols=['Z','WEIGHT'],write_cat='y'):
 
     outfn =  flin+'_'+str(rann)+'_clustering.ran.fits'
     
-    print(len(ffr))
+    len_o = len(ffr))
     if 'NGC' in flin:
         #need to split N/S when sampling
         selregr = ffr['DEC'] > 32.375
@@ -3325,7 +3325,7 @@ def clusran_resamp(flin,rann,rcols=['Z','WEIGHT'],write_cat='y'):
             for col in rcols:
                 tabsr[i][col] =  dshuf[col]
         ffr = vstack(tabsr)   
-        print(len(ffr))     
+        print(len(ffr),len_o)     
     else:
         inds = np.random.choice(len(fcdn),len(ffr))
         dshuf = fcdn[inds]
