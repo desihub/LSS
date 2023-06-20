ebd43bf10af8e14a6982d92284aad136e2823968
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-06-20 10:30:07 -0400

    Update apply_blinding_main_fromfile_fcomp.py

--- scripts/main/apply_blinding_main_fromfile_fcomp.py
+++ scripts/main/apply_blinding_main_fromfile_fcomp.py
@@ -521,3 +521,4 @@ if root:
     os.system('rm '+dirout+args.type+'*_S_*')
     os.system('rm '+dirout+args.type+'*_N_*')
     os.system('rm '+dirout+args.type+'*IFFT*')
+    os.system('rm '+dirout+args.type+'*full*')
