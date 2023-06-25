f44e52f874db214dd6e364c09955291f264fa714
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-06-25 14:46:22 -0400

    Update Y1_sysnetall.sh

--- scripts/Y1_sysnetall.sh
+++ scripts/Y1_sysnetall.sh
@@ -12,7 +12,7 @@ $LSSDIR/LSS/scripts/run_sysnet.sh N ELG_LOPnotqso true false 1024 0.003 dnnp pnl
 $LSSDIR/LSS/scripts/run_sysnet.sh S ELG_LOPnotqso true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 $LSSDIR/LSS/scripts/run_sysnet.sh N ELG_LOPnotqso false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh N ELG_LOPnotqso false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S ELG_LOPnotqso false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOPnotqso --add_sysnet y --fulld n --survey Y1 --verspec iron --version $1
 
@@ -24,7 +24,7 @@ $LSSDIR/LSS/scripts/run_sysnet.sh N LRG true false 1024 0.003 dnnp pnll $1 $LSSB
 $LSSDIR/LSS/scripts/run_sysnet.sh S LRG true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 $LSSDIR/LSS/scripts/run_sysnet.sh N LRG false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh N LRG false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S LRG false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type LRG --add_sysnet y --fulld n --survey Y1 --verspec iron --version $1
 
@@ -34,7 +34,7 @@ $LSSDIR/LSS/scripts/run_sysnet.sh N QSO true false 1024 0.003 dnnp pnll $1 $LSSB
 $LSSDIR/LSS/scripts/run_sysnet.sh S QSO true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 $LSSDIR/LSS/scripts/run_sysnet.sh N QSO false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh N QSO false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S QSO false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type BGS_BRIGHT-21.5 --prepsysnet y --fulld n --survey Y1 --verspec iron --version $1
@@ -43,4 +43,4 @@ $LSSDIR/LSS/scripts/run_sysnet.sh N BGS_BRIGHT-21.5 true false 1024 0.003 dnnp p
 $LSSDIR/LSS/scripts/run_sysnet.sh S BGS_BRIGHT-21.5 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
 $LSSDIR/LSS/scripts/run_sysnet.sh N BGS_BRIGHT-21.5 false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh N BGS_BRIGHT-21.5 false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S BGS_BRIGHT-21.5 false true 1024 0.01 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
