f55623d32043e326b32bff753df1e2b5c07f2912
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-06-26 15:58:24 -0400

    Create Y1_regressis_all.sh

---
+++ scripts/Y1_regressis_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --regressis y --add_regressis y --fulld n --survey Y1 --verspec iron --version $1
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type LRG --regressis y --add_regressis y --fulld n --survey Y1 --verspec iron --version $1
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type QSO --regressis y --add_regressis y --fulld n --survey Y1 --verspec iron --version $1
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type BGS_BRIGHT-21.5 --regressis y --add_regressis y --fulld n --survey Y1 --verspec iron --version $1
