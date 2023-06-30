dcde4263c8ce9fe5ae954c17b157898707de223a
Ashley J. Ross <ashley.jacob.ross@gmail.com>
2023-06-29 23:00:27 -0400

    Update Y1all_xi_rec.sh

--- scripts/Y1all_xi_rec.sh
+++ scripts/Y1all_xi_rec.sh
@@ -8,6 +8,6 @@ srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xir
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer ELG_LOPnotqso --rec_type $2 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --recon_dir $3 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/$3/xi/
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer BGS_BRIGHT-21.5 --rec_type $2--survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --recon_dir $3 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/$3/xi/
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer BGS_BRIGHT-21.5 --rec_type $2 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --recon_dir $3 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/$3/xi/
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer QSO --rec_type $2 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --recon_dir $3 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/$3/xi/
