0822cacf6085a2e00a965f6e87a59e52209deaf0
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-07 15:06:27 -0400

    Update test_logging.py

--- scripts/test_logging.py
+++ scripts/test_logging.py
@@ -1,5 +1,31 @@
 import logging
 
-logger = logging.getLogger('test')
-logger.setLevel(level=logging.INFO)
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# 'application' code
+logger.debug('debug message')
 logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
+#import logging
+
+#logger = logging.getLogger('test')
+#logger.setLevel(level=logging.INFO)
+#logger.info('info message')
