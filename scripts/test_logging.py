df5b22c303f7c308f20bcf8078a96cff970c7c59
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-07 15:10:16 -0400

    Update test_logging.py

--- scripts/test_logging.py
+++ scripts/test_logging.py
@@ -9,7 +9,7 @@ logger.setLevel(logging.DEBUG)
 #ch.setLevel(logging.DEBUG)
 
 # create formatter
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # add formatter to ch
 #ch.setFormatter(formatter)
