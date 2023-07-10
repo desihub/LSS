5db23d82f4e31c57b85b63ba245cbdef8a940b15
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-09 23:23:24 -0400

    Update Y1_sysnet_tracer_zbin.sh

--- scripts/Y1_sysnet_tracer_zbin.sh
+++ scripts/Y1_sysnet_tracer_zbin.sh
@@ -7,8 +7,8 @@ PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --prepsysnet y --fulld n --survey Y1 --verspec iron --version $1
 
-$LSSDIR/LSS/scripts/run_sysnet.sh N ELG_LOPnotqso$2 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh S ELG_LOPnotqso$2 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh N $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 
-$LSSDIR/LSS/scripts/run_sysnet.sh N ELG_LOPnotqso$2 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
-$LSSDIR/LSS/scripts/run_sysnet.sh S ELG_LOPnotqso$2 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh N $2$3 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet.sh S $2$3 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
