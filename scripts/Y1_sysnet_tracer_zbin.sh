9eb7da71179084ee2b0e13173ba1eb24c1c08e90
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-09 23:24:16 -0400

    Update Y1_sysnet_tracer_zbin.sh

--- scripts/Y1_sysnet_tracer_zbin.sh
+++ scripts/Y1_sysnet_tracer_zbin.sh
@@ -3,9 +3,6 @@ source /global/common/software/desi/desi_environment.sh main
 export LSSDIR=$HOME
 export SYSNETDIR=$HOME/desicode
 export LSSBASE=/global/cfs/cdirs/desi/survey/catalogs/
-PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py
-
-python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --prepsysnet y --fulld n --survey Y1 --verspec iron --version $1
 
 $LSSDIR/LSS/scripts/run_sysnet.sh N $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 $LSSDIR/LSS/scripts/run_sysnet.sh S $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
