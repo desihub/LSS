bec514cdfe5c9b5fd3f202fb9b446a7a5a5ca604
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-11 16:55:04 -0400

    Update Y1all_wp_unblind.sh

--- scripts/Y1all_wp_unblind.sh
+++ scripts/Y1all_wp_unblind.sh
@@ -4,11 +4,10 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer LRG --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer ELG_LOPnotqso --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer BGS_BRIGHT-21.5 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer QSO --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y  --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
-
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer LRG --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
-
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer ELG_LOPnotqso --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
-
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer BGS_BRIGHT-21.5 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
