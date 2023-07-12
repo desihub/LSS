c6245a61f0bace084fb5f5d71a5e0aa6ef42dd46
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-11 20:09:43 -0400

    Update Y1all_wp_unblind.sh

--- scripts/Y1all_wp_unblind.sh
+++ scripts/Y1all_wp_unblind.sh
@@ -6,8 +6,16 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer LRG --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer LRG --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/ --bin_type log
+
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer ELG_LOPnotqso --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer ELG_LOPnotqso --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/ --bin_type log
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer BGS_BRIGHT-21.5 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer BGS_BRIGHT-21.5 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/ --bin_type log
+
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer QSO --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y  --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/wp_Y1_unblind_onfly.py --tracer QSO --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type rppi --use_arrays y  --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/ --bin_type log
