332960f67d9e4d8afdc454cf7104ce014625bd4a
ashleyjross <ashley.jacob.ross@gmail.com>
2023-07-18 12:51:40 +0100

    Update cosmodesi_io_tools.py

--- py/LSS/cosmodesi_io_tools.py
+++ py/LSS/cosmodesi_io_tools.py
@@ -156,9 +156,16 @@ def get_clustering_positions_weights(catalog, distance, zlim=(0., np.inf),maglim
             mask &= ~zmask
     if option:
        if 'ntile' in option:
-           opsp = option.split('=')
-           nt = int(opsp[1])
-           mask &= catalog['NTILE'] == nt 
+           if '=' in option:
+               opsp = option.split('=')
+               nt = int(opsp[1])
+               mask &= catalog['NTILE'] == nt
+           if '>' in option:
+               opsp = option.split('>')
+               nt = int(opsp[1])
+               mask &= catalog['NTILE'] >= nt
+               
+         
     logger.info('Using {:d} rows for {}.'.format(mask.sum(), name))
     positions = [catalog['RA'][mask], catalog['DEC'][mask], distance(catalog['Z'][mask])]
     weights = np.ones_like(positions[0])
