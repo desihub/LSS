860bc4d942bad5f7547f1d1825d47edc3ba22639
ashleyjross <ashley.jacob.ross@gmail.com>
2023-08-11 16:52:52 -0400

    Update Y1_zfail_all.sh

--- scripts/Y1_zfail_all.sh
+++ scripts/Y1_zfail_all.sh
@@ -7,6 +7,6 @@ python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalo
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type LRG --add_weight_zfail y --fulld n --survey Y1 --verspec iron --version $1
 
-python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type QSO ---add_weight_zfail y --fulld n --survey Y1 --verspec iron --version $1
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type QSO --add_weight_zfail y --fulld n --survey Y1 --verspec iron --version $1
 
 python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type BGS_BRIGHT --add_weight_zfail y --fulld n --survey Y1 --verspec iron --version $1
