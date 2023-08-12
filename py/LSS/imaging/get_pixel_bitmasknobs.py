18b47179cbe80ba2f3ed31128667966d460778c2
ashleyjross <ashley.jacob.ross@gmail.com>
2023-08-12 18:03:42 -0400

    masking tools and application to mocks

---
+++ py/LSS/imaging/get_pixel_bitmasknobs.py
@@ -0,0 +1,117 @@
+# Get bitmask values from pixel-level per-brick masks for a catalog
+# Examples:
+# srun -N 1 -C haswell -c 64 -t 04:00:00 -q interactive python read_pixel_bitmaskmasknobs.py 
+from __future__ import division, print_function
+import sys, os, glob, time, warnings, gc
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.table import Table, vstack, hstack, join
+import fitsio
+
+from astropy.io import fits
+from astropy import wcs
+
+from multiprocessing import Pool
+import argparse
+
+
+    
+
+bitmask_dir = '/global/cfs/cdirs/cosmo/data/legacysurvey/dr9/'
+bricks = Table(fitsio.read('/global/cfs/cdirs/cosmo/data/legacysurvey/dr9/randoms/survey-bricks-dr9-randoms-0.48.0.fits'))
+
+
+def bitmask_radec(brickid, ra, dec):
+
+    brick_index = np.where(bricks['BRICKID']==brickid)[0][0]
+
+    brickname = str(bricks['BRICKNAME'][brick_index])
+    if bricks['PHOTSYS'][brick_index]=='N':
+        field = 'north'
+    elif bricks['PHOTSYS'][brick_index]=='S':
+        field = 'south'
+    else:
+        # raise ValueError
+        # Outside DR9 footprint; assign mask bit 16
+        bitmask = np.full(len(ra), 2**16, dtype=np.uint8)
+        nobsg = 0
+        nobsr = 0
+        nobsz = 0
+        return bitmask,nobsg,nobsr,nobsz
+
+    bitmask_fn = bitmask_dir+'{}/coadd/{}/{}/legacysurvey-{}-maskbits.fits.fz'.format(field, brickname[:3], brickname, brickname)
+
+    bitmask_img = fitsio.read(bitmask_fn)
+
+    header = fits.open(bitmask_fn)[1].header
+    w = wcs.WCS(header)
+
+    coadd_x, coadd_y = w.wcs_world2pix(ra, dec, 0)
+    coadd_x, coadd_y = np.round(coadd_x).astype(int), np.round(coadd_y).astype(int)
+
+    bitmask = bitmask_img[coadd_y, coadd_x]
+    nobsl = []
+    bl = ['g','r','z']
+    for band in bl:
+        nobs_fn = bitmask_dir+'{}/coadd/{}/{}/legacysurvey-{}-nexp-{}.fits.fz'.format(field, brickname[:3], brickname, brickname,band)
+        if os.path.isfile(nobs_fn): 
+            nobs_img = fitsio.read(nobs_fn)
+            nobs = nobs_img[coadd_y,coadd_x]
+        else:
+            nobs = 0
+        nobsl.append(nobs)
+    return bitmask,nobsl[0],nobsl[1],nobsl[2]
+
+def get_nobsandmask(cat,nproc=128):
+	#cat should be an astropy table
+	#returns table with same ordering and NOBS_{G,R,Z} and MASKBITS columns
+    for col in cat.colnames:
+        cat.rename_column(col, col.upper())
+
+	if 'TARGETID' not in cat.colnames:
+		cat['TARGETID'] = np.arange(len(cat))
+
+	if 'TARGET_RA' in cat.colnames:
+        cat.rename_columns(['TARGET_RA', 'TARGET_DEC'], ['RA', 'DEC'])
+    
+    if 'INPUT_RA' in cat.colnames:
+        cat.rename_columns(['input_ra', 'input_dec'], ['RA', 'DEC'])
+    if 'BRICKID' not in cat.colnames:
+        from desiutil import brick
+        tmp = brick.Bricks(bricksize=0.25)
+        cat['BRICKID'] = tmp.brickid(cat['RA'], cat['DEC'])
+    # Just some tricks to speed up things up
+    bid_unique, bidcnts = np.unique(cat['BRICKID'], return_counts=True)
+    bidcnts = np.insert(bidcnts, 0, 0)
+    bidcnts = np.cumsum(bidcnts)
+    bidorder = np.argsort(cat['BRICKID'])
+
+	print('adding nobs and mask values to '+str(len(cat))+' rows')
+	def _wrapper(bid_index):
+
+		idx = bidorder[bidcnts[bid_index]:bidcnts[bid_index+1]]
+		brickid = bid_unique[bid_index]
+
+		ra, dec = cat['RA'][idx], cat['DEC'][idx]
+		tid = cat['TARGETID'][idx]
+		bitmask,nobsg,nobsr,nobsz = bitmask_radec(brickid, ra, dec)
+
+		data = Table()
+		data['idx'] = idx
+		data['MASKBITS'] = bitmask
+		data['NOBS_G'] = nobsg
+		data['NOBS_R'] = nobsr
+		data['NOBS_Z'] = nobsz
+		data['TARGETID'] = tid
+
+		return data
+
+    # start multiple worker processes
+    with Pool(processes=n_processes) as pool:
+        res = pool.map(wrapper, np.arange(len(bid_unique)))
+
+    res = vstack(res)
+    res.sort('idx')
+    res.remove_column('idx')
+    return res
+
---
+++ scripts/mock_tools/prepare_mocks_Y1.py
@@ -0,0 +1,148 @@
+from astropy.io import fits # Access to FITS (Flexible Image Transport System) files.
+from astropy.table import Table, hstack, vstack, Column # A class to represent tables of heterogeneous data.
+import fitsio
+import numpy as np
+import glob
+import os
+import h5py
+import argparse
+import sys
+
+from desitarget.targetmask import obsconditions
+from desimodel.footprint import is_point_in_desi
+
+import LSS.common_tools as common
+from LSS.imaging import get_nobsandmask
+from LSS.main.cattools import count_tiles_better
+from LSS.globals import main
+
+
+if os.environ['NERSC_HOST'] == 'cori':
+    scratch = 'CSCRATCH'
+elif os.environ['NERSC_HOST'] == 'perlmutter':
+    scratch = 'PSCRATCH'
+else:
+    print('NERSC_HOST is not cori or permutter but is '+os.environ['NERSC_HOST'])
+    sys.exit('NERSC_HOST not known (code only works on NERSC), not proceeding') 
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mockver", help="type of mock to use",default=None)
+parser.add_argument("--mockpath", help="Location of mock file(s)",default='/global/cfs/cdirs/desi/cosmosim/FirstGenMocks/AbacusSummit/CutSky/')
+parser.add_argument("--mockfile", help="formattable name of mock file(s). e.g. cutsky_{TYPE}_{Z}_AbacusSummit_base_c000_ph{PH}.fits. TYPE will be replaced with tracer type. PH will be replaced with realization number for simulation of mock.",default='cutsky_{TYPE}_{Z}_AbacusSummit_base_c000_ph{PH}.fits')
+#parser.add_argument("--realization", help="number for the realization",default=1,type=int)
+parser.add_argument("--realmin", help="number for the realization",default=1,type=int)
+parser.add_argument("--realmax", help="number for the realization",default=2,type=int)
+parser.add_argument("--survey", help="points to set of tiles",default='DA02')
+parser.add_argument("--prog", help="dark or bright",default='dark')
+parser.add_argument("--base_output", help="base directory for output",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/')
+parser.add_argument("--prep", help="prepare file for fiberassign?",default='y')
+parser.add_argument("--runfa", help="run fiberassign",default='n')
+parser.add_argument("--par", help="running in parallel?",default='n')
+
+args = parser.parse_args()
+
+tiletab = Table.read('/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/tiles-'+args.prog.upper()+'.fits')
+
+if args.prog == 'dark':
+    types = ['ELG', 'LRG', 'QSO']
+    desitar = {'ELG':34,'LRG':1,'QSO':4}
+    priority = {'ELG':3000,'LRG':3200,'QSO':3400}
+
+for real in range(args.realmin,args.realmax):
+    if not (args.mockver is None):
+        if args.mockver == 'ab_firstgen':
+            mockpath = '/global/cfs/cdirs/desi/cosmosim/FirstGenMocks/AbacusSummit/CutSky/'
+        
+            file_name = 'cutsky_{TYPE}_{Z}_AbacusSummit_base_c000_ph{PH}.fits'
+            out_file_name = args.base_output+'/FirstGenMocks/AbacusSummit/forFA'+str(real)+'.fits'
+            if not os.path.exists(args.base_output+'/FirstGenMocks'):
+                os.mkdir(args.base_output+'/FirstGenMocks')
+                print('made '+args.base_output+'/FirstGenMocks')
+            if not os.path.exists(args.base_output+'/FirstGenMocks/AbacusSummit'):
+                os.mkdir(args.base_output+'/FirstGenMocks/AbacusSummit')
+                print('made '+args.base_output+'/FirstGenMocks/AbacusSummit')
+            mockdir = args.base_output+'/FirstGenMocks/AbacusSummit/'
+        else:
+            raise ValueError(args.mockver+' not supported with legacy mockver argument. Use mockpath/mockfilename arguments instead.')
+    else:
+        mockpath = args.mockpath
+        file_name = args.mockfile
+        out_file_name = args.base_output + '/forFA_Real{0}.fits'.format(real)
+    
+    print('will write to '+out_file_name)
+    if not os.path.exists(args.base_output):
+        os.makedirs(args.base_output)
+        print('made '+args.base_output)
+    
+    mockdir = args.base_output
+    zs = {'ELG':'z1.100','LRG':'z0.800','QSO':'z1.400'}
+
+
+    def mask(main=0, nz=0, Y5=0, sv3=0):
+        return main * (2**3) + sv3 * (2**2) + Y5 * (2**1) + nz * (2**0)
+    if args.prep == 'y':
+        datat = []
+        for type_ in types:
+            thepath = os.path.join(mockpath, type_, zs[type_], file_name.format(TYPE = type_, Z = zs[type_], PH = "%03d" % real))
+            #f = fits.open(thepath)
+            print('thepath')
+            print(thepath)
+            data = fitsio.read(thepath,columns=['RA','DEC','Z','Z_COSMO','STATUS'])#f[1].data
+            print(data.dtype.names)
+            print(type_,len(data))
+            status = data['STATUS'][()]
+            idx = np.arange(len(status))
+            mask_main = mask(main=0, nz=1, Y5=1, sv3=0)
+            if type_ == 'LRG':
+                mask_main = mask(main=1, nz=1, Y5=1, sv3=0)
+            idx_main = idx[(status & (mask_main))==mask_main]
+            data = data[idx_main]
+            print(len(data))
+            data = Table(data)
+            data['DESI_TARGET'] = desitar[type_]
+            data['PRIORITY_INIT'] = priority[type_]
+            data['PRIORITY'] = priority[type_]
+            datat.append(data)
+        targets = vstack(datat)
+        print(len(targets)+' in Y5 area')
+        del datat
+        selY1 = is_point_in_desi(tiletab,targets['RA'],targets['DEC']
+        targets = targets[selY1]
+        print(len(targets)+' in Y1 area')
+
+    if args.apply_mask == 'y':
+        print('getting nobs and mask bits')
+        maskv = get_nobsandmask(targets)
+        maskcols = ['NOBS_G','NOBS_R','NOBS_Z','MASKBITS']
+        for col in maskcols:
+            targets[col] = maskv[col]
+        del maskv
+        targets = common.cutphotmask(targets,bits=main.imbits)
+        
+
+    if args.prep == 'y':
+        n=len(targets)
+        targets.rename_column('Z_COSMO', 'TRUEZ') 
+        targets.rename_column('Z', 'RSDZ') 
+        targets['BGS_TARGET'] = np.zeros(n, dtype='i8')
+        targets['MWS_TARGET'] = np.zeros(n, dtype='i8')
+        targets['SUBPRIORITY'] = np.random.uniform(0, 1, n)
+        targets['BRICKNAME'] = np.full(n, '000p0000')    #- required !?!
+        targets['OBSCONDITIONS'] = obsconditions.mask(program.upper()) #np.zeros(n, dtype='i8')+int(3) 
+        targets['NUMOBS_MORE'] = np.zeros(n, dtype='i8')+int(1) 
+        targets['NUMOBS_INIT'] = np.zeros(n, dtype='i8')+int(1)
+        targets['SCND_TARGET'] = np.zeros(n, dtype='i8')+int(0)
+        targets['ZWARN'] = np.zeros(n, dtype='i8')+int(0)
+        targets['TARGETID'] = np.arange(1,n+1)
+
+        targets.write(out_file_name, overwrite = True)
+
+        fits.setval(out_file_name, 'EXTNAME', value='TARGETS', ext=1)
+        fits.setval(out_file_name, 'OBSCON', value=args.prog.upper(), ext=1)
+
+
+
+
+sys.exit()
+
