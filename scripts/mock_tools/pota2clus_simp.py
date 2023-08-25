e681ce5468eb8c1ac7523d3f5cf27420a6ce7248
ashleyjross <ashley.jacob.ross@gmail.com>
2023-08-25 13:07:11 -0400

    Update pota2clus_simp.py

--- scripts/mock_tools/pota2clus_simp.py
+++ scripts/mock_tools/pota2clus_simp.py
@@ -34,7 +34,7 @@ else:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--tracer", help="tracer type to be selected")
+#parser.add_argument("--tracer", help="tracer type to be selected")
 parser.add_argument("--realization",type=int)
 parser.add_argument("--prog", default="DARK")
 parser.add_argument("--veto",default='_noveto')
@@ -55,22 +55,22 @@ notqso = ''
 #if args.notqso == 'y':
 #    notqso = 'notqso'
 
-tracer = args.tracer
+#tracer = args.tracer
 
-if tracer == 'LRG':
-    zmin = 0.4
-    zmax = 1.1
-
-elif tracer == 'ELG_LOP':
-    zmin = 0.8
-    zmax = 1.6
-
-elif tracer == 'QSO':
-    zmin = 0.8
-    zmax = 2.1
-
-else:
-    sys.exit('tracer type '+args.tracer+' not supported (yet)')
+# if tracer == 'LRG':
+#     zmin = 0.4
+#     zmax = 1.1
+# 
+# elif tracer == 'ELG_LOP':
+#     zmin = 0.8
+#     zmax = 1.6
+# 
+# elif tracer == 'QSO':
+#     zmin = 0.8
+#     zmax = 2.1
+# 
+# else:
+#     sys.exit('tracer type '+args.tracer+' not supported (yet)')
 
 
 mockdir = args.base_dir+'mock'+str(args.realization)+'/'
@@ -99,14 +99,10 @@ if args.prog == 'DARK':
     tracers = ['LRG','QSO','ELG_LOP']
 
 ndattot = len(mock_data)
-out_data_fn = mockdir+tracer+'_complete'+args.veto+'_clustering.dat.fits'
-out_data_froot = mockdir+tracer+'_complete'+args.veto+'_'
 
 lmockdat_noveto = len(mock_data)
 mainp = main('LRG','iron','Y1') #needed for bad fiber list
-'''
-PUT IN SOMETHING HERE TO MASK TO GOODHARDLOC AS AN OPTION
-'''
+
 if 'gtl' in args.veto:
     tilelocid = 10000*mock_data['TILEID']+mock_data['LOCATION']
     specfo = args.specdata_dir+'datcomb_'+args.prog.lower()+'_spec_zdone.fits'
@@ -121,63 +117,78 @@ if 'gtl' in args.veto:
     mock_data = mock_data[goodtl]
     print(lmockdat_noveto,len(mock_data))
     
-#for tracer in tracers:
-mainp = main(tracer,'iron','Y1')
-seltar = mock_data[desitarg] & bit > 0
-mock_data = mock_data[seltar]
-lmockdat_noveto = len(mock_data)
-print('length before/after cut to target type '+args.tracer)
-print(ndattot,lmockdat_noveto)
+for tracer in tracers:
+    out_data_fn = mockdir+tracer+'_complete'+args.veto+'_clustering.dat.fits'
+    out_data_froot = mockdir+tracer+'_complete'+args.veto+'_'
+
+    mainp = main(tracer,'iron','Y1')
+    bit = targetmask.desi_mask[tracer]
+    seltar = mock_data[desitarg] & bit > 0
+    mock_data = mock_data[seltar]
+    lmockdat_noveto = len(mock_data)
+    print('length before/after cut to target type '+args.tracer)
+    print(ndattot,lmockdat_noveto)
+    if tracer == 'LRG':
+        zmin = 0.4
+        zmax = 1.1
+
+    elif tracer == 'ELG_LOP':
+        zmin = 0.8
+        zmax = 1.6
+
+    elif tracer == 'QSO':
+        zmin = 0.8
+        zmax = 2.1
 
 
-selz = mock_data['RSDZ'] > zmin
-selz &= mock_data['RSDZ'] < zmax
-mock_data = mock_data[selz]
-mock_data = Table(mock_data)
-mock_data = unique(mock_data,keys=['TARGETID'])
-print('length after cutting to redshift and unique targetid',len(mock_data))
-mock_data.rename_column('RSDZ', 'Z')
-mock_data['WEIGHT'] = 1
-common.write_LSS(mock_data,out_data_fn)
+    selz = mock_data['RSDZ'] > zmin
+    selz &= mock_data['RSDZ'] < zmax
+    mock_data = mock_data[selz]
+    mock_data = Table(mock_data)
+    mock_data = unique(mock_data,keys=['TARGETID'])
+    print('length after cutting to redshift and unique targetid',len(mock_data))
+    mock_data.rename_column('RSDZ', 'Z')
+    mock_data['WEIGHT'] = 1
+    common.write_LSS(mock_data,out_data_fn)
 
-def splitGC(flroot,datran='.dat',rann=0):
-    import LSS.common_tools as common
-    from astropy.coordinates import SkyCoord
-    import astropy.units as u
-    app = 'clustering'+datran+'.fits'
-    if datran == '.ran':
-        app = str(rann)+'_clustering'+datran+'.fits'
+    def splitGC(flroot,datran='.dat',rann=0):
+        import LSS.common_tools as common
+        from astropy.coordinates import SkyCoord
+        import astropy.units as u
+        app = 'clustering'+datran+'.fits'
+        if datran == '.ran':
+            app = str(rann)+'_clustering'+datran+'.fits'
 
-    fn = Table(fitsio.read(flroot+app))
-    c = SkyCoord(fn['RA']* u.deg,fn['DEC']* u.deg,frame='icrs')
-    gc = c.transform_to('galactic')
-    sel_ngc = gc.b > 0
-    outf_ngc = flroot+'NGC_'+app
-    common.write_LSS(fn[sel_ngc],outf_ngc)
-    outf_sgc = flroot+'SGC_'+app
-    common.write_LSS(fn[~sel_ngc],outf_sgc)
+        fn = Table(fitsio.read(flroot+app))
+        c = SkyCoord(fn['RA']* u.deg,fn['DEC']* u.deg,frame='icrs')
+        gc = c.transform_to('galactic')
+        sel_ngc = gc.b > 0
+        outf_ngc = flroot+'NGC_'+app
+        common.write_LSS(fn[sel_ngc],outf_ngc)
+        outf_sgc = flroot+'SGC_'+app
+        common.write_LSS(fn[~sel_ngc],outf_sgc)
 
-splitGC(out_data_froot,'.dat')
+    splitGC(out_data_froot,'.dat')
 
-ran_samp_cols = ['Z','WEIGHT']
+    ran_samp_cols = ['Z','WEIGHT']
 
-def ran_col_assign(randoms,data,sample_columns):
-    inds = np.random.choice(len(data),len(randoms))
-    dshuf = data[inds]
-    for col in sample_columns:
-        randoms[col] =  dshuf[col]
-    return randoms
+    def ran_col_assign(randoms,data,sample_columns):
+        inds = np.random.choice(len(data),len(randoms))
+        dshuf = data[inds]
+        for col in sample_columns:
+            randoms[col] =  dshuf[col]
+        return randoms
 
-for rann in range(rm,rx):
-    in_ran_fn = args.random_dir+args.tracer+'_'+str(rann)+'_full_noveto.ran.fits' #all noveto have same ra,dec, tracer becomes important for LRG imaging veto
-    out_ran_fn = out_data_froot+str(rann)+'_clustering.ran.fits'
-    ran = Table(fitsio.read(in_ran_fn,columns=['RA','DEC','TILELOCID']))
-    if 'gtl' in args.veto:
-        goodtl = np.isin(ran['TILELOCID'],gtl)
-        ran = ran[goodtl]
-    ran = ran_col_assign(ran,mock_data,ran_samp_cols)
-    common.write_LSS(ran,out_ran_fn)
-    splitGC(out_data_froot,'.ran',rann)
+    for rann in range(rm,rx):
+        in_ran_fn = args.random_dir+tracer+'_'+str(rann)+'_full_noveto.ran.fits' #all noveto have same ra,dec, tracer becomes important for LRG imaging veto
+        out_ran_fn = out_data_froot+str(rann)+'_clustering.ran.fits'
+        ran = Table(fitsio.read(in_ran_fn,columns=['RA','DEC','TILELOCID']))
+        if 'gtl' in args.veto:
+            goodtl = np.isin(ran['TILELOCID'],gtl)
+            ran = ran[goodtl]
+        ran = ran_col_assign(ran,mock_data,ran_samp_cols)
+        common.write_LSS(ran,out_ran_fn)
+        splitGC(out_data_froot,'.ran',rann)
 
 
 
