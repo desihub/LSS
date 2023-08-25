9be21bced3f062d2ce4337a3eec0d8ed42c41b36
ashleyjross <ashley.jacob.ross@gmail.com>
2023-08-25 13:44:52 -0400

    Update pota2clus_simp.py

--- scripts/mock_tools/pota2clus_simp.py
+++ scripts/mock_tools/pota2clus_simp.py
@@ -186,7 +186,7 @@ for tracer in tracers:
         tracerr = tracer
         if tracer == 'ELG_LOP':
             tracerr += 'notqso'
-        in_ran_fn = args.random_dir+tracerf+'_'+str(rann)+'_full_noveto.ran.fits' #all noveto have same ra,dec, tracer becomes important for LRG imaging veto
+        in_ran_fn = args.random_dir+tracerr+'_'+str(rann)+'_full_noveto.ran.fits' #all noveto have same ra,dec, tracer becomes important for LRG imaging veto
         out_ran_fn = out_data_froot+str(rann)+'_clustering.ran.fits'
         ran = Table(fitsio.read(in_ran_fn,columns=['RA','DEC','TILELOCID']))
         if 'gtl' in args.veto:
