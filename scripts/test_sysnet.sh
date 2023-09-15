0dc8ba10c62877e0f4183f85cfb077bf354201a1
ashleyjross <ashley.jacob.ross@gmail.com>
2023-09-15 17:22:24 -0400

    Create test_sysnet.sh

---
+++ scripts/test_sysnet.sh
@@ -0,0 +1,7 @@
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py #make sure to set $LSSDIR to wherever the LSS repo is installed, e.g., export LSSCODE=$HOME
+
+run_sysnet=sysnet-app
+
+run_sysnet #should output something like configuration file missing
