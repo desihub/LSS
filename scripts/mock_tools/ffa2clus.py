3de40521287b54189cfbc7edb2c73e887a743c94
ashleyjross <ashley.jacob.ross@gmail.com>
2023-09-22 12:00:37 -0400

    Update ffa2clus.py

--- scripts/mock_tools/ffa2clus.py
+++ scripts/mock_tools/ffa2clus.py
@@ -211,8 +211,10 @@ for tracer in tracers:
             ran = Table(fitsio.read(in_ran_fn,columns=rcols))
             ran['FRAC_TLOBS_TILES'] = 1.
         
-            goodtl = np.isin(ran['TILELOCID'],gtl)
+            print(len(ran)+' random length before good loc cut')
+            goodtl = ran['GOODHARDLOC']#np.isin(ran['TILELOCID'],gtl)
             ran = ran[goodtl]
+            print(len(ran)+' random length after good loc cut')
             if 'imaging' in args.veto:
                 ebits = mainp.ebits
                 reccircmasks = mainp.reccircmasks
