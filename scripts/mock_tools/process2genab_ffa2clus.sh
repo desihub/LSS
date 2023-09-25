4485683198dd91aa7a85de3ec8dd5a8630b70b47
ashleyjross <ashley.jacob.ross@gmail.com>
2023-09-24 20:09:58 -0400

    Create process2genab_ffa2clus.sh

---
+++ scripts/mock_tools/process2genab_ffa2clus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+for (( i=$1;i<=$2;i++ ))
+do
+ srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/ffa2clus.py --realization $i --tracer LRG --mkdat y --mkran y --resamp y --nz y --par y  --apply_HPmapcut y --maxr 18
+done
