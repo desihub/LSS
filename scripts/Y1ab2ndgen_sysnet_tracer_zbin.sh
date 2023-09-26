5bae24007f17f1a5107f74229146eb1a96ce143d
ashleyjross <ashley.jacob.ross@gmail.com>
2023-09-26 12:56:12 -0400

    Update Y1ab2ndgen_sysnet_tracer_zbin.sh

--- scripts/Y1ab2ndgen_sysnet_tracer_zbin.sh
+++ scripts/Y1ab2ndgen_sysnet_tracer_zbin.sh
@@ -2,8 +2,8 @@ source /global/common/software/desi/desi_environment.sh main
 
 export LSSDIR=$HOME
 
-$LSSDIR/LSS/scripts/run_sysnet_ab2gen.sh N $2$3 true false 1024 0.003 dnnp pnll $1 
-$LSSDIR/LSS/scripts/run_sysnet_ab2gen.sh S $2$3 true false 1024 0.003 dnnp pnll $1 
+$LSSDIR/LSS/scripts/run_sysnet_ab2ndgen.sh N $2$3 true false 1024 0.003 dnnp pnll $1 
+$LSSDIR/LSS/scripts/run_sysnet_ab2ndgen.sh S $2$3 true false 1024 0.003 dnnp pnll $1 
 
-$LSSDIR/LSS/scripts/run_sysnet_ab2gen.sh N $2$3 false true 1024 0.004 dnnp pnll $1 
-$LSSDIR/LSS/scripts/run_sysnet_ab2gen.sh S $2$3 false true 1024 0.004 dnnp pnll $1 
+$LSSDIR/LSS/scripts/run_sysnet_ab2ndgen.sh N $2$3 false true 1024 0.004 dnnp pnll $1 
+$LSSDIR/LSS/scripts/run_sysnet_ab2ndgen.sh S $2$3 false true 1024 0.004 dnnp pnll $1 
