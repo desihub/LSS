631a2c59286fbcfb5a91a78039fc7781d0a5aa8a
ashleyjross <ashley.jacob.ross@gmail.com>
2023-09-27 12:11:03 -0400

    Update cosmodesi_io_tools.py

--- py/LSS/cosmodesi_io_tools.py
+++ py/LSS/cosmodesi_io_tools.py
@@ -191,10 +191,10 @@ def get_clustering_positions_weights(catalog, distance, zlim=(0., np.inf),maglim
         #assumes default already added the rest of the weights and that SN was used as default weight
         weights *=  catalog['WEIGHT_RF'][mask]/catalog['WEIGHT_SN'][mask]
     if 'addRF' in weight_type:
-        #assumes default already added the rest of the weights and that SN was used as default weight
+        #assumes no imaging systematic weights were in default
         weights *=  catalog['WEIGHT_RF'][mask]
     if 'addSN' in weight_type:
-        #assumes default already added the rest of the weights and that SN was used as default weight
+        #assumes no imaging systematic weights were in default
         weights *=  catalog['WEIGHT_SN'][mask]
 
     if 'completeness_only' in weight_type:
