ae6305c95c5b8ad1909abb8e5c7b0bb36e039ccd
ashleyjross <ashley.jacob.ross@gmail.com>
2023-10-02 11:46:49 -0400

    Create submit_ab2ndgen_LRG_ffa_sysnet_zbin.py

---
+++ scripts/mock_tools/submit_ab2ndgen_LRG_ffa_sysnet_zbin.py
@@ -0,0 +1,39 @@
+from desipipe import Queue, Environment, TaskManager, spawn, setup_logging
+
+import argparse
+
+iparser = argparse.ArgumentParser()
+parser.add_argument("--zbin", help="which zbin to use, 1,2,3",default=1,type=int)
+args = parser.parse_args()
+
+if args.zbin == 1:
+    zrange = (0.4,0.6)
+if args.zbin == 2:
+    zrange = (0.6,0.8)
+if args.zbin == 3:
+    zrange = (0.8,1.1)
+
+
+setup_logging()
+
+queue = Queue('LRGsysnet')
+queue.clear()
+
+environ = Environment('nersc-cosmodesi')
+tm = TaskManager(queue=queue, environ=environ)
+# 24 tasks per node, 72 workers total = 3 nodes, for 3 hours each
+tm_sysnet = tm.clone(scheduler=dict(max_workers=75), provider=dict(provider='nersc', time='03:00:00', mpiprocs_per_worker=1, nodes_per_worker=1/25, output='_sbatch/slurm-%j.out', error='_sbatch/slurm-%j.err'))
+
+
+@tm_sysnet.bash_app
+def sysnet(i, zrange):
+    return ['./scripts/Y1ab2ndgen_sysnet_tracer_zbin.sh', i, 'LRG_ffa', f'{zrange[0]:.1f}_{zrange[1]:.1f}']
+
+
+if __name__ == '__main__':
+
+    setup_logging()
+    #for zrange in [(0.4, 0.6), (0.6, 0.8), (0.8, 1.1)]:
+    for i in range(0, 25):
+        sysnet(i, zrange)
+    spawn(queue, spawn=True)  # that'll launch all jobs
