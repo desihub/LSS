b43781023ab48f56cbb9dccdff0892d5be77cd4d
Jiaxi-Yu <yujx_jessy@outlook.com>
2023-10-05 08:45:49 -0700

    add choices for --option ['elgzmask','elgzcatas']

--- scripts/xirunpc.py
+++ scripts/xirunpc.py
@@ -577,7 +577,7 @@ if __name__ == '__main__':
     parser.add_argument('--region', help='regions; by default, run on N, S; pass NS to run on concatenated N + S', type=str, nargs='*', choices=['N', 'S', 'NS','NGC','SGC'], default=None)
     parser.add_argument('--zlim', help='z-limits, or options for z-limits, e.g. "highz", "lowz", "fullonly"', type=str, nargs='*', default=None)
     parser.add_argument('--maglim', help='absolute r-band magnitude limits', type=str, nargs='*', default=None)
-    parser.add_argument('--option', help='place to put extra options for cutting catalogs', default=None)
+    parser.add_argument('--option', help='place to put extra options for cutting catalogs', choices=['elgzmask', 'elgzcatas'], default=None)
     parser.add_argument('--corr_type', help='correlation type', type=str, nargs='*', choices=['smu', 'rppi', 'theta'], default=['smu'])
     parser.add_argument('--weight_type', help='types of weights to use; use "default_angular_bitwise" for PIP with angular upweighting; "default" just uses WEIGHT column', type=str, default='default')
     # Need to add support for fkp weights for use_arrays option
