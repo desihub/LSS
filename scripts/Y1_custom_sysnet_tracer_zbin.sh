7921458dece41b94a079d3b5940bea34dbd35cd5
ashleyjross <ashley.jacob.ross@gmail.com>
2023-10-07 15:42:04 -0400

    Update Y1_custom_sysnet_tracer_zbin.sh

--- scripts/Y1_custom_sysnet_tracer_zbin.sh
+++ scripts/Y1_custom_sysnet_tracer_zbin.sh
@@ -1,4 +1,4 @@
-source /global/common/software/desi/desi_environment.sh main
+#!/bin/bash
 
 $LSSDIR/LSS/scripts/run_sysnet_cd.sh N $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
 $LSSDIR/LSS/scripts/run_sysnet_cd.sh S $2$3 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/iron/LSScats/
