5a1d87c4ac44fcc42bca7a2fcaae08741abdb2fd
ashleyjross <ashley.jacob.ross@gmail.com>
2023-10-30 21:01:45 -0400

    Update validation_improp_full.py

--- scripts/validation/validation_improp_full.py
+++ scripts/validation/validation_improp_full.py
@@ -510,7 +510,8 @@ for tp in tps:
                 nest = False
                 eclrs = ['gr','rz']
                 for ec in eclrs:
-                    ebvn = fitsio.read(dirmap+'v1_desi_ebv_'+ec+'_'+str(nside)+'.fits')
+                    #ebvn = fitsio.read(dirmap+'v1_desi_ebv_'+ec+'_'+str(nside)+'.fits')
+                    ebvn = fitsio.read(dirmap+'v1_desi_ebv_'+str(nside)+'.fits')
                     debv = ebvn['EBV_DESI_'+ec.upper()]-ebvn['EBV_SFD']
                     parv = debv
                     fig = plt.figure()
