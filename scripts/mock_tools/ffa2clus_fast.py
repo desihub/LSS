28bad02b513d172407a47f067a5079cda0d1bb98
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-03 22:56:41 -0400

    Update ffa2clus_fast.py

--- scripts/mock_tools/ffa2clus_fast.py
+++ scripts/mock_tools/ffa2clus_fast.py
@@ -37,22 +37,21 @@ parser = argparse.ArgumentParser()
 #parser.add_argument("--tracer", help="tracer type to be selected")
 parser.add_argument("--realization",type=int)
 parser.add_argument("--prog", default="DARK")
-parser.add_argument("--veto",default='_imaging')
+#parser.add_argument("--veto",default='_imaging')
 #parser.add_argument("--mockdir", help="directory when pota mock data is",default='/global/cfs/cdirs/desi/users/acarnero/y1mock/SecondGen/clustering/')
-parser.add_argument("--base_dir", help="base directory for input/output",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/')
-parser.add_argument("--random_dir",help="where to find the data randoms",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/v0.6/')
+parser.add_argument("--base_dir", help="base directory for input/output",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/')
+parser.add_argument("--data_dir",help="where to find the data randoms",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/v0.6/')
 parser.add_argument("--specdata_dir",help="where to find the spec data ",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/')
 parser.add_argument("--minr", help="minimum number for random files",default=0,type=int)
-parser.add_argument("--maxr", help="maximum for random files, default is 1, but 40 are available (use parallel script for all)",default=1,type=int) 
-parser.add_argument("--mockver", default='abacus2ffa', help = "which mocks to use. use abacus2ffa for Abacus 2nd gen fast fiber assignment")
-parser.add_argument("--tracer", default = 'LRG')
-parser.add_argument("--par", default = 'n',help='whether to run random steps in parallel or not')
-parser.add_argument("--mkdat", default = 'n')
-parser.add_argument("--mkran", default = 'n')
-parser.add_argument("--resamp", default = 'n')
-parser.add_argument("--nz", default = 'n')
-parser.add_argument("--apply_HPmapcut", default = 'n')
-parser.add_argument("--mapcutdata", help="only happens if apply_HPmapcut is 'y'",default = 'y')
+parser.add_argument("--maxr", help="maximum for random files, default is all 18)",default=18,type=int) 
+parser.add_argument("--mockver", default='AbacusSummit', help = "which mocks to use. use abacus2ffa for Abacus 2nd gen fast fiber assignment")
+parser.add_argument("--tracer", default = 'all')
+parser.add_argument("--outloc", default = None)
+parser.add_argument("--par", default = 'y',help='whether to run random steps in parallel or not')
+parser.add_argument("--mkdat", default = 'y')
+parser.add_argument("--mkran", default = 'y')
+parser.add_argument("--nz", default = 'y')
+parser.add_argument("--splitGC", default = 'y')
 #parser.add_argument("--remove_unassigned", default = 'y', help = 'set to n if dont want to include unassigned targets in catalog')
 
 
@@ -64,6 +63,11 @@ rx = int(args.maxr)
 
 notqso = ''
 
+lssmapdirout = args.random_dir+'/hpmaps/'
+mapn = fitsio.read(lssmapdirout+'QSO_mapprops_healpix_nested_nside'+str(nside)+'_N.fits')
+maps = fitsio.read(lssmapdirout+'QSO_mapprops_healpix_nested_nside'+str(nside)+'_S.fits')
+mainp = main('LRG','iron','Y1')
+mapcuts = mainp.mapcuts
 
 
 if args.prog == 'DARK':
@@ -98,11 +102,51 @@ def splitGC(flroot,datran='.dat',rann=0):
 
 
 
-def ran_col_assign(randoms,data,sample_columns):
-    inds = np.random.choice(len(data),len(randoms))
-    dshuf = data[inds]
-    for col in sample_columns:
-        randoms[col] =  dshuf[col]
+def ran_col_assign(randoms,data,sample_columns,tracer):
+
+    def _resamp(selregr,selregd):
+        for col in sample_columns:
+            randoms[col] =  np.zeros(len(randoms))
+        rand_sel = [selregr,~selregr]
+        dat_sel = [ selregd,~selregd]
+        for dsel,rsel in zip(dat_sel,rand_sel):
+            inds = np.random.choice(len(data[dsel]),len(randoms[rsel]))
+            print(len(data[dsel]),len(inds),np.max(inds))
+            dshuf = data[dsel][inds]
+            for col in rcols:
+                randoms[col][rsel] = dshuf[col]
+        randoms['WEIGHT'] *= randoms['FRAC_TLOBS_TILES'] 
+        rdl = []
+        for dsel,rsel in zip(dat_sel,rand_sel):
+            rd = np.sum(randoms[rsel]['WEIGHT'])/np.sum(data[dsel]['WEIGHT'])
+            rdl.append(rd)
+        rdr = rdl[0]/rdl[1]
+        print('norm factor is '+str(rdr))
+        randoms['WEIGHT'][rand_sel[1]] *= rdr
+        for dsel,rsel in zip(dat_sel,rand_sel):
+            rd = np.sum(randoms[rsel]['WEIGHT'])/np.sum(data[dsel]['WEIGHT'])
+            print('data/random weighted ratio after resampling:'+str(rd))
+
+	des_resamp = False
+	if 'QSO' in tracer:
+		des_resamp = True
+	selregr = randoms['PHOTSYS'] ==  'N'
+	selregd = data['PHOTSYS'] ==  'N'
+	randoms = _resamp(selregr,selregd)
+
+	if des_resamp:
+		print('resampling in DES region')
+		from regressis import footprint
+		foot = footprint.DR9Footprint(256, mask_lmc=False, clear_south=True, mask_around_des=False, cut_desi=False)
+		north, south, des = foot.get_imaging_surveys()
+		th_ran,phi_ran = (-randoms['DEC']+90.)*np.pi/180.,randoms['RA']*np.pi/180.
+		th_dat,phi_dat = (-data['DEC']+90.)*np.pi/180.,data['RA']*np.pi/180.
+		pixr = hp.ang2pix(256,th_ran,phi_ran,nest=True)
+		selregr = des[pixr]
+		pixd = hp.ang2pix(256,th_dat,phi_dat,nest=True)
+		selregd = des[pixd]
+		randoms = _resamp(selregr,selregd)
+
     return randoms
 
 def apply_imaging_veto(ff,reccircmasks,ebits):
@@ -122,21 +166,24 @@ def apply_imaging_veto(ff,reccircmasks,ebits):
     return ff
 
 nproc = 9
+
+mockdir = args.base_dir+mockver+'/mock'+str(args.realization)+'/'
+if args.outloc == None
+    outdir = os.getenv(scratch)+'/'+mockver+'/mock'+str(args.realization)+'/'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+if args.outloc == 'prod':
+    outdir = mockdir
     
 for tracer in tracers:
-    if args.mockver == 'abacus2ffa':
-        mockdir = args.base_dir+'mock'+str(args.realization)+'/'
-        in_data_fn = mockdir + 'ffa_full_'+args.tracer+'.fits'
-        #mock_data_tr = fitsio.read(in_data_fn)
-        #do this later after cutting to unique and getting completeness info
-        #if args.remove_unassigned == 'y':
-        #    mock_data = mock_data[mock_data["WEIGHT_IIP"] != 1e+20]
+   
+    
+    in_data_fn = mockdir + 'ffa_full_'+args.tracer+'.fits'
 
-    else:
-        sys.exit('mock versions other than ' +abacus2ff+' not supported')
-
-    out_data_fn = mockdir+tracer+'_ffa'+args.veto+'_clustering.dat.fits'
-    out_data_froot = mockdir+tracer+'_ffa'+args.veto+'_'
+    out_data_fn = outdir+tracer+'_ffa_clustering.dat.fits'
+    out_data_froot = outdir+tracer+'_ffa_'
+    
     subfrac = 1
     if tracer == 'LRG':
         zmin = 0.4
@@ -157,12 +204,6 @@ for tracer in tracers:
     if args.mkdat == 'y':
 
         
-        #bit = bittest[tracer]#targetmask.desi_mask[tracer]
-        #seltar = mock_data[desitarg] & bit > 0
-        #mock_data_tr = mock_data[seltar]
-        #lmockdat_noveto = len(mock_data_tr)
-        #print('length before/after cut to target type '+tracer)
-        #print(ndattot,len(mock_data))
     
         mock_data_tr = Table(fitsio.read(in_data_fn))
         mock_data_tr = unique(mock_data_tr,keys=['TARGETID'])
@@ -172,14 +213,20 @@ for tracer in tracers:
         print('length after cutting to "observed" targets',len(mock_data_tr))
         mock_data_tr.rename_column('RSDZ', 'Z')
         mock_data_tr['WEIGHT_COMP'] = mock_data_tr['WEIGHT_IIP']
-        if 'imaging' in args.veto:
-            if tracer == 'LRG':
-                lrgmask = fitsio.read(args.base_dir+'forFA'+str(args.realization)+'_matched_input_full_lrg_imask.fits')
-                mock_data_tr = join(mock_data_tr,lrgmask,keys=['TARGETID'])
-                print(len(mock_data_tr))
-            ebits = mainp.ebits
-            reccircmasks = mainp.reccircmasks
-            mock_data_tr = apply_imaging_veto(mock_data_tr,reccircmasks,ebits)
+		
+		#apply imaging vetos
+		if tracer == 'LRG':
+			lrgmask = fitsio.read(args.base_dir+'forFA'+str(args.realization)+'_matched_input_full_lrg_imask.fits')
+			mock_data_tr = join(mock_data_tr,lrgmask,keys=['TARGETID'])
+			print(len(mock_data_tr))
+		ebits = mainp.ebits
+		reccircmasks = mainp.reccircmasks
+		mock_data_tr = apply_imaging_veto(mock_data_tr,reccircmasks,ebits)
+        
+        mock_data_tr = common.apply_map_veto_array(mock_data_tr,mapn,maps,mapcuts)
+        print('map data veto done')
+
+
         selz = mock_data_tr['Z'] > zmin
         selz &= mock_data_tr['Z'] < zmax
         mock_data_tr = mock_data_tr[selz]
@@ -196,11 +243,13 @@ for tracer in tracers:
         mock_data_tr['WEIGHT'] = mock_data_tr['WEIGHT_SYS']*mock_data_tr['WEIGHT_COMP']*mock_data_tr['WEIGHT_ZFAIL']
         common.write_LSS(mock_data_tr,out_data_fn)
 
-        splitGC(out_data_froot,'.dat')
+        #splitGC(out_data_froot,'.dat')
 
     ran_samp_cols = ['Z','WEIGHT','WEIGHT_COMP','WEIGHT_SYS','WEIGHT_ZFAIL']
 
     nran = rx-rm
+    ran_fname_base = args.base_dir+tracer+'_ffa_imaging_HPmapcut'
+
     if args.mkran == 'y':
         if args.mkdat == 'n':
             mock_data_tr = fitsio.read(out_data_fn)
@@ -211,27 +260,14 @@ for tracer in tracers:
                 tracerr += 'notqso'
             if tracer == 'ELG':
                 tracerr = 'ELG_LOPnotqso'
-            in_ran_fn = args.random_dir+tracerr+'_'+str(rann)+'_full_noveto.ran.fits' #all noveto have same ra,dec, tracer becomes important for LRG imaging veto
+            in_ran_fn = ran_fname_base+str(rann)+'_full.ran.fits' 
             out_ran_fn = out_data_froot+str(rann)+'_clustering.ran.fits'
-            rcols = ['RA','DEC','TILELOCID','NOBS_G','NOBS_R','NOBS_Z','MASKBITS','TARGETID','NTILE','GOODHARDLOC']
-            if tracerr == 'LRG':
-                rcols.append('lrg_mask')
-            ran = Table(fitsio.read(in_ran_fn,columns=rcols))
-            ran['FRAC_TLOBS_TILES'] = 1.
-        
-            print(len(ran),' random length before good loc cut')
-            goodtl = ran['GOODHARDLOC']#np.isin(ran['TILELOCID'],gtl)
-            ran = ran[goodtl]
-            print(len(ran),' random length after good loc cut')
-            if 'imaging' in args.veto:
-                ebits = mainp.ebits
-                reccircmasks = mainp.reccircmasks
-                ran = apply_imaging_veto(ran,reccircmasks,ebits)
+            rcols = ['RA','DEC','TILELOCID','PHOTSYS','TARGETID','NTILE','FRAC_TLOBS_TILES']
 
-            ran = ran_col_assign(ran,mock_data_tr,ran_samp_cols)
+            ran = ran_col_assign(ran,mock_data_tr,ran_samp_cols,tracer)
             common.write_LSS(ran,out_ran_fn)
             del ran
-            splitGC(out_data_froot,'.ran',rann)
+            #splitGC(out_data_froot,'.ran',rann)
 
         inds = np.arange(nran)
         if args.par == 'y':
@@ -265,64 +301,46 @@ for tracer in tracers:
     
     regions = ['NGC', 'SGC']
 
-    if args.resamp == 'y':
-        
-        for reg in regions:
-            flin = out_data_froot +reg    
-            def _parfun(rannum):
-                ct.clusran_resamp(flin,rannum,rcols=ran_samp_cols,compmd='')#,compmd=args.compmd)#, ntilecut=ntile, ccut=ccut)
+#     if args.resamp == 'y':
+#         
+#         for reg in regions:
+#             flin = out_data_froot +reg    
+#             def _parfun(rannum):
+#                 ct.clusran_resamp(flin,rannum,rcols=ran_samp_cols,compmd='')#,compmd=args.compmd)#, ntilecut=ntile, ccut=ccut)
+# 
+#             inds = np.arange(nran)
+#             if args.par == 'y':
+#                 from multiprocessing import Pool
+#                 with Pool(processes=nproc) as pool:
+#                     res = pool.map(_parfun, inds)
+#             else:
+#                 for rn in range(rm,rx):
+#                     _parfun(rn)
 
-            inds = np.arange(nran)
-            if args.par == 'y':
-                from multiprocessing import Pool
-                with Pool(processes=nproc) as pool:
-                    res = pool.map(_parfun, inds)
-            else:
-                for rn in range(rm,rx):
-                    _parfun(rn)
-
-    allreg = ['NGC', 'SGC']#'N','S',
+    #allreg = ['NGC', 'SGC']#'N','S',
     if args.nz == 'y':
         #this calculates the n(z) and then adds nbar(completeness) and FKP weights to the catalogs
-        for reg in allreg:
-            fb = out_data_froot+reg
-            fcr = fb+'_0_clustering.ran.fits'
-            fcd = fb+'_clustering.dat.fits'
-            fout = fb+'_nz.txt'
-            common.mknz(fcd,fcr,fout,bs=dz,zmin=zmin,zmax=zmax,compmd='')
-            common.addnbar(fb,bs=dz,zmin=zmin,zmax=zmax,P0=P0,nran=nran,compmd='',par=args.par,nproc=nproc)
+        #for reg in allreg:
+		fb = out_data_froot
+		fcr = fb+'0_clustering.ran.fits'
+		fcd = fb+'clustering.dat.fits'
+		fout = fb+'nz.txt'
+		common.mknz(fcd,fcr,fout,bs=dz,zmin=zmin,zmax=zmax,compmd='')
+		common.addnbar(fb,bs=dz,zmin=zmin,zmax=zmax,P0=P0,nran=nran,compmd='',par=args.par,nproc=nproc)
+	
+    if args.splitGC == 'y':
+        splitGC(out_data_froot,'.dat')
+        def _spran(rann):
+            splitGC(out_data_froot,'.ran',rann)
+        inds = np.arange(nran)
+        if args.par == 'y':
+            from multiprocessing import Pool
+            with Pool(processes=nproc) as pool:
+                res = pool.map(_spran, inds)
+        else:
+            for rn in inds:#range(rm,rx):
+                 _spran(rn)
 
     
-    if args.apply_HPmapcut == 'y':
-        import healpy as hp
-        nside = 256
-        lssmapdirout = args.random_dir+'/hpmaps/'
-        mapn = fitsio.read(lssmapdirout+'QSO_mapprops_healpix_nested_nside'+str(nside)+'_N.fits')
-        maps = fitsio.read(lssmapdirout+'QSO_mapprops_healpix_nested_nside'+str(nside)+'_S.fits')
-        mapcuts = mainp.mapcuts
-        outroot = mockdir+tracer+'_ffa_'
-        for reg in allreg:
-            if args.mapcutdata == 'y':
-                fin = out_data_froot+reg+'_clustering.dat.fits'
-                fin = fin.replace('global','dvs_ro')  
-                fout = outroot+reg+'_clustering.dat.fits'  
-                common.apply_map_veto(fin,fout,mapn,maps,mapcuts)
-                print('data veto done, now doing randoms')
-            def _parfunHP(rn):
-                fin = out_data_froot+reg+'_'+str(rn)+'_clustering.ran.fits'
-                fin = fin.replace('global','dvs_ro')   
-                fout = outroot+reg+'_'+str(rn)+'_clustering.ran.fits'        
-                common.apply_map_veto(fin,fout,mapn,maps,mapcuts)
-                print('random veto '+str(rn)+' done')
-            if args.par == 'n':
-                for rn in range(rm,rx):
-                    _parfunHP(rn)
-            else:
-                inds = np.arange(rm,rx)
-                from multiprocessing import Pool
-            
-                #nproc = 9 #try this so doesn't run out of memory
-                with Pool(processes=nproc) as pool:
-                    res = pool.map(_parfunHP, inds)
 
 
