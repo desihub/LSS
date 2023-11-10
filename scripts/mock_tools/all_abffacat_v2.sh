2323c726cab20eb39a08eaf8d99b905e6c81aa7c
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-10 18:04:52 -0500

    Create all_abffacat_v2.sh

---
+++ scripts/mock_tools/all_abffacat_v2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+for (( i=$1;i<=$2;i++ ))
+do
+ srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/ffa2clus_fast.py --mockcatver v2 --outloc prod --add_imsys_ran y --realization $i
+
+done
+
