c4f01f3092f5cfa8ff7cb46f090da86dbd096547
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-11 13:00:04 -0500

    Update copy_v2cat.sh

--- scripts/mock_tools/copy_v2cat.sh
+++ scripts/mock_tools/copy_v2cat.sh
@@ -3,6 +3,7 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
+ mkdir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$i/v2
  cp -r $SCRATCH/AbacusSummit/mock$i/v2/* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$i/v2/
  chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$i/v2/
  chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/mock$i/v2/*
