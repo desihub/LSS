1288cbcc06a3df168123de2723913492da82f141
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-14 12:57:14 -0500

    Update common_tools.py

--- py/LSS/common_tools.py
+++ py/LSS/common_tools.py
@@ -969,7 +969,7 @@ def maskcircandrec(indata,maskfn):
     
     
 
-def apply_veto(fin,fout,ebits=None,zmask=False,maxp=3400,comp_only=False,reccircmasks=None):
+def apply_veto(fin,fout=None,ebits=None,zmask=False,maxp=3400,comp_only=False,reccircmasks=None):
     '''
     fl is a string with the path to the file name to load
     fout is a string with the path to the outpur file
@@ -977,7 +977,7 @@ def apply_veto(fin,fout,ebits=None,zmask=False,maxp=3400,comp_only=False,reccirc
     zmask is whether or not to apply any zmask
     maxp is the maximum priority to keep in the data files
     '''
-    ff = Table(fitsio.read(fin))#+'full_noveto.'+dr+'.fits')
+    ff = Table(fitsio.read(fin.replace('global','dvs_ro')))#+'full_noveto.'+dr+'.fits')
     print('length of input '+str(len(ff)))
     seld = ff['GOODHARDLOC'] == 1
     print('length after cutting to good locations '+str(len(ff[seld])))
@@ -1092,7 +1092,7 @@ def apply_veto(fin,fout,ebits=None,zmask=False,maxp=3400,comp_only=False,reccirc
     if '.ran' in fin:
         print('area is ' + str(len(ff) / 2500))
     #comments = ["'full' LSS catalog without after vetos for priority, good hardware and imaging quality","entries are for targetid that showed up in POTENTIAL_ASSIGNMENTS"]
-    if comp_only == False:
+    if fout is not None:
         write_LSS(ff, fout)#, comments)
     if '.dat' in fin:
         wz = ff['ZWARN'] != 999999
@@ -1102,7 +1102,8 @@ def apply_veto(fin,fout,ebits=None,zmask=False,maxp=3400,comp_only=False,reccirc
         print('assignment completeness is '+str(comp))
         print('sum of 1/(FRACZ_TILELOCID*FRAC_TLOBS_TILES), 1/COMP_TILE, and length of input; should approximately match')
         print(np.sum(1. / (ff[wz]['FRACZ_TILELOCID']*ff[wz]['FRAC_TLOBS_TILES'])), np.sum(1. / ff[wz]['COMP_TILE']), len(ff))
-
+    if fout is None:
+        return ff
     del ff
 #     tmpfn = fout+'.tmp'
 #     if os.path.isfile(tmpfn):
