ad6d71efcced681b3367d5cb191c376fbf46ec08
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-13 20:06:54 -0500

    Update all_ab_amtl_tracer.sh

--- scripts/mock_tools/all_ab_amtl_tracer.sh
+++ scripts/mock_tools/all_ab_amtl_tracer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+t=$($1+$2+1)
+echo t
 for (( i=$1;i<=$2;i++ ))
 do
- j=$i+1
+ j=$($i+1)
  srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/mkCat_SecondGen.py --base_output /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/altmtl{MOCKNUM} --mockver ab_secondgen --mockmin $i --mockmax $j --survey Y1 --add_gtl y --specdata iron --tracer $3 --notqso y --minr 0 --maxr 18 --fulld y --fullr y --apply_map_veto y --apply_veto y --use_map_veto _HPmapcut --mkclusran y --resamp y --nz y --mkclusdat y --targDir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit
 done
 
