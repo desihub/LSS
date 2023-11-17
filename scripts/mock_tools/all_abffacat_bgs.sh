141e36f12c41b050d00f7a049d2239de3f70a481
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-17 16:00:01 -0500

    Create all_abffacat_bgs.sh

---
+++ scripts/mock_tools/all_abffacat_bgs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+for (( i=$1;i<=$2;i++ ))
+do
+ srun -N 1 -C cpu -t 00:20:00 --qos interactive --account desi python scripts/mock_tools/ffa2clus_fast.py --mockver AbacusSummitBGS/SecondGenMocks/AbacusSummit  --realization $i
+
+done
+
