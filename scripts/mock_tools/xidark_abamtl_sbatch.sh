60331481b7f66587e3a0ea8cc9b9a5f4aafd76cd
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-18 18:18:22 -0500

    Update xidark_abamtl_sbatch.sh

--- scripts/mock_tools/xidark_abamtl_sbatch.sh
+++ scripts/mock_tools/xidark_abamtl_sbatch.sh
@@ -10,7 +10,7 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-indir = /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/altmtl$SLURM_ARRAY_TASK_ID/mock$SLURM_ARRAY_TASK_ID/LSScats
+indir=/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/altmtl$SLURM_ARRAY_TASK_ID/mock$SLURM_ARRAY_TASK_ID/LSScats
 
 srun python scripts/xirunpc.py --gpu --tracer LRG --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
 srun python scripts/xirunpc.py --gpu --tracer ELG_LOPnotqso --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
