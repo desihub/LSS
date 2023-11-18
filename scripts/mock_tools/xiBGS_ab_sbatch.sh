0fcd9ddac6e9b5a7a333f68c032ffaee52da953e
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-18 10:29:41 -0500

    Update xiBGS_ab_sbatch.sh

--- scripts/mock_tools/xiBGS_ab_sbatch.sh
+++ scripts/mock_tools/xiBGS_ab_sbatch.sh
@@ -10,4 +10,4 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun python scripts/xirunpc.py --gpu --tracer BGS_BRIGHT-21.5_ffa --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/SecondGenMocks/AbacusSummit/mock$i/ --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/SecondGenMocks/AbacusSummit/mock$i/xi/
+srun python scripts/xirunpc.py --gpu --tracer BGS_BRIGHT-21.5_ffa --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/SecondGenMocks/AbacusSummit/mock$SLURM_ARRAY_TASK_ID/ --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/SecondGenMocks/AbacusSummit/mock$SLURM_ARRAY_TASK_ID/xi/
