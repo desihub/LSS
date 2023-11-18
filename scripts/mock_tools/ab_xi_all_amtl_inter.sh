dd135bfa2a0e068a9ce3f599bc4b455713320a11
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-18 18:41:21 -0500

    bug fix

--- scripts/mock_tools/ab_xi_all_amtl_inter.sh
+++ scripts/mock_tools/ab_xi_all_amtl_inter.sh
@@ -5,9 +5,9 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
  indir=/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/altmtl$i/mock$i/LSScats
- srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer LRG --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
- srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer ELG_LOPnotqso --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
- srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer QSO --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
+ srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer LRG --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
+ srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer ELG_LOPnotqso --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
+ srun -N 1 -C gpu -t 00:20:00 --gpus 4 --qos debug --account desi_g python scripts/xirunpc.py --gpu --tracer QSO --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
 
 done
 
--- scripts/mock_tools/xidark_abamtl_sbatch.sh
+++ scripts/mock_tools/xidark_abamtl_sbatch.sh
@@ -12,6 +12,6 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 indir=/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit/altmtl$SLURM_ARRAY_TASK_ID/mock$SLURM_ARRAY_TASK_ID/LSScats
 
-srun python scripts/xirunpc.py --gpu --tracer LRG --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
-srun python scripts/xirunpc.py --gpu --tracer ELG_LOPnotqso --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
-srun python scripts/xirunpc.py --gpu --tracer QSO --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir indir/xi/
+srun python scripts/xirunpc.py --gpu --tracer LRG --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
+srun python scripts/xirunpc.py --gpu --tracer ELG_LOPnotqso --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
+srun python scripts/xirunpc.py --gpu --tracer QSO --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --basedir $indir --outdir $indir/xi/
