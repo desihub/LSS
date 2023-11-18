0720d4f5cb0f68a05a53eba0ec89344e3f6c05b3
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-18 15:49:51 -0500

    Update run1_EZmock_LSS.sh

--- scripts/mock_tools/run1_EZmock_LSS.sh
+++ scripts/mock_tools/run1_EZmock_LSS.sh
@@ -2,4 +2,4 @@
 python scripts/readwrite_pixel_bitmask.py --tracer lrg --input $1 --cat_type Y1EZmock
 python scripts/mock_tools/ffa2clus_fast.py --mockver EZmock/FFA --realization $1
 cp $SCRATCH/EZmock/FFA/mock$1/* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/EZmock/FFA/mock$1/
-chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/EZmock/FFA/mock$1/*
+chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/EZmock/FFA/mock$1/*clustering*
