05a0268406965ae7d9f0a3e5d1430ef8b040d80a
AlbertoRosado1 <ar652820@ohio.edu>
2023-11-28 14:00:21 -0800

    added scripts to run SYSNet regression on ELGs with more specific hyper parameters

---
+++ scripts/Y1_sysnetELG_zbins_new.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+export LSSDIR=$HOME
+#export SYSNETDIR=$HOME/desicode
+export LSSBASE=/global/cfs/cdirs/desi/survey/catalogs/
+PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py
+
+VERSION=$1
+
+# Some NN parameters for North
+LR_N=0.009    # learning rate
+NBATCH_N=256  # Powers of 2
+NCHAIN_N=5    # chains
+NEPOCH_N=200  # number of epochs
+NNS_N=(3 10)  # NN structure (# layers, # units)
+
+# Some NN parameters for South
+LR_S=0.007    # learning rate
+NBATCH_S=1024 # Powers of 2
+NCHAIN_S=5    # chains
+NEPOCH_S=200  # number of epochs
+NNS_S=(4 20)  # NN structure (# layers, # units)
+
+BASEDIR=$LSSBASE/Y1/LSS/iron/LSScats/
+RUN_SYSNET=$LSSDIR/LSS/scripts/run_sysnetELG_cd_new.sh
+
+# If using the allsky randoms option when preparing for sysnet mkCat_main.py might not work without salloc or job
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --prepsysnet y --imsys_zbin y --fulld n --survey Y1 --verspec iron --version $VERSION --use_allsky_rands y
+
+# Find learning rate for North
+$RUN_SYSNET N ELG_LOPnotqso0.8_1.1 true false $NBATCH_N 0.003 dnnp pnll $VERSION $BASEDIR $NCHAIN_N $NEPOCH_N ${NNS_N[@]}
+$RUN_SYSNET N ELG_LOPnotqso1.1_1.6 true false $NBATCH_N 0.003 dnnp pnll $VERSION $BASEDIR $NCHAIN_N $NEPOCH_N ${NNS_N[@]}
+# Find learning rate for South
+$RUN_SYSNET S ELG_LOPnotqso0.8_1.1 true false $NBATCH_S 0.003 dnnp pnll $VERSION $BASEDIR $NCHAIN_S $NEPOCH_S ${NNS_S[@]}
+$RUN_SYSNET S ELG_LOPnotqso1.1_1.6 true false $NBATCH_S 0.003 dnnp pnll $VERSION $BASEDIR $NCHAIN_S $NEPOCH_S ${NNS_S[@]}
+
+# Run SYSNet for North
+$RUN_SYSNET N ELG_LOPnotqso0.8_1.1 false true $NBATCH_N $LR_N dnnp pnll $VERSION $BASEDIR $NCHAIN_N $NEPOCH_N ${NNS_N[@]}
+$RUN_SYSNET N ELG_LOPnotqso1.1_1.6 false true $NBATCH_N $LR_N dnnp pnll $VERSION $BASEDIR $NCHAIN_N $NEPOCH_N ${NNS_N[@]} 
+# Run SYSNet for South
+$RUN_SYSNET S ELG_LOPnotqso0.8_1.1 false true $NBATCH_S $LR_S dnnp pnll $VERSION $BASEDIR $NCHAIN_S $NEPOCH_S ${NNS_S[@]}
+$RUN_SYSNET S ELG_LOPnotqso1.1_1.6 false true $NBATCH_S $LR_S dnnp pnll $VERSION $BASEDIR $NCHAIN_S $NEPOCH_S ${NNS_S[@]}
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --add_sysnet y --imsys_zbin y --fulld n --survey Y1 --verspec iron --version $VERSION
+
+python scripts/validation/validation_improp_full.py --tracer ELG_LOPnotqso --version $VERSION
+
