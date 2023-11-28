710da5f65fbe60dcf26d9ca847b26319760ef2fa
ashleyjross <ashley.jacob.ross@gmail.com>
2023-11-28 11:48:18 -0500

    Update add_linweight_ran_abffa.sh

--- scripts/mock_tools/add_linweight_ran_abffa.sh
+++ scripts/mock_tools/add_linweight_ran_abffa.sh
@@ -4,7 +4,7 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
  srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/addsys.py --tracer ${3}_ffa --imsys n --add_imsys_ran y --par y --realization $i --mockcatver v2
- python scripts/validation/validation_improp_mock.py --tracer $3 --mockn $i --weight_col WEIGHT_IMLIN --mockcatver v2
+ python scripts/validation/validation_improp_mock_FFA.py --tracer $3 --mockn $i --weight_col WEIGHT_IMLIN --mockcatver v2
 
 done
 
