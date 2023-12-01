9d6a4cd356e074b9922b991e82582d37f900371b
ashleyjross <ashley.jacob.ross@gmail.com>
2023-12-01 12:07:49 -0500

    Update common_tools.py

--- py/LSS/common_tools.py
+++ py/LSS/common_tools.py
@@ -560,6 +560,9 @@ def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6,P0=10000,add_data=True,ran_sw=
             nl[ii] = nzd[zind]
     mean_comp = len(fd)/np.sum(fd['WEIGHT_COMP'])
     print('mean completeness '+str(mean_comp))
+    if 'NTILE' is not in list(fd.dtype.names):
+        fd['NTILE'] = np.ones(len(fd))
+        print('added NTILE = 1 column because column did not exist')
     ntl = np.unique(fd['NTILE'])
     comp_ntl = np.ones(len(ntl))
     weight_ntl = np.ones(len(ntl))
@@ -623,6 +626,11 @@ def addnbar(fb,nran=18,bs=0.01,zmin=0.01,zmax=1.6,P0=10000,add_data=True,ran_sw=
         #ft['NZ'] = nl
         #ff['LSS'].insert_column('NZ',nl)
         #fd['NZ'] = nl
+        print('mean completeness '+str(mean_comp))
+        if 'NTILE' is not in list(fd.dtype.names):
+            fd['NTILE'] = np.ones(len(fd))
+            print('added NTILE = 1 column because column did not exist')
+
         fd['NX'] = nl*comp_ntl[fd['NTILE']-1]
         wt = fd['WEIGHT_COMP']*fd['WEIGHT_SYS']*fd['WEIGHT_ZFAIL']
         if compmd == 'ran':
