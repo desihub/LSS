0aef2a45c2ffdfeeaeb4608ffb9c68d6c57cde71
Aurelio Carnero Rosell <aurelio.crosell@gmail.com>
2023-12-01 05:36:54 -0800

    changes

--- scripts/mock_tools/abamtl_cat_sbatch.sh
+++ scripts/mock_tools/abamtl_cat_sbatch.sh
@@ -3,10 +3,10 @@
 #SBATCH --qos=regular
 #SBATCH --nodes=1
 #SBATCH --constraint=cpu
-#SBATCH --array=0-24
+#SBATCH --array=10-24
 #SBATCH --account=desi
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun scripts/mock_tools/run1_AMTLmock_LSS.sh $SLURM_ARRAY_TASK_ID
+srun /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/run1_AMTLmock_LSS.sh $SLURM_ARRAY_TASK_ID
