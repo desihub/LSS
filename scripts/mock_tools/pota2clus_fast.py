8ff3d5b6bf8a63bf838f054ed5c319986fc3fc40
ashleyjross <ashley.jacob.ross@gmail.com>
2023-12-01 11:49:55 -0500

    Update pota2clus_fast.py

--- scripts/mock_tools/pota2clus_fast.py
+++ scripts/mock_tools/pota2clus_fast.py
@@ -27,6 +27,20 @@ import logging
 logger = logging.getLogger('mkCat')
 logger.setLevel(level=logging.INFO)
 
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+logger.info('run started')
 
 #import LSS.mkCat_singletile.fa4lsscat as fa
 #from LSS.globals import main
