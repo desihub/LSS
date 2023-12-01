1098650e56c1c718498456d27ea256ee39ea9c27
ashleyjross <ashley.jacob.ross@gmail.com>
2023-12-01 14:44:37 -0500

    Update process2genabv3_pota2clus.sh

--- scripts/mock_tools/process2genabv3_pota2clus.sh
+++ scripts/mock_tools/process2genabv3_pota2clus.sh
@@ -3,10 +3,10 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
- srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --realization $i 
- mv $SCRATCH/AbacusSummit_v3/mock$1/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$1/
- mv $SCRATCH/AbacusSummit_v3/mock$1/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$1/
- rm $SCRATCH/AbacusSummit_v3/mock$1/*
- chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$1/*clustering*
- chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$1/*nz*
+ srun -N 1 -C cpu -t 00:30:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --realization $i 
+ mv $SCRATCH/AbacusSummit_v3/mock$i/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$i/
+ mv $SCRATCH/AbacusSummit_v3/mock$i/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$i/
+ rm $SCRATCH/AbacusSummit_v3/mock$i/*
+ chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$i/*clustering*
+ chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/mock$i/*nz*
 done
