8bf795133afc89ad02e251eed7588512132ee5cb
ashleyjross <ashley.jacob.ross@gmail.com>
2023-12-07 22:29:40 -0500

    Update Y1all_xi.sh

--- scripts/Y1all_xi.sh
+++ scripts/Y1all_xi.sh
@@ -5,10 +5,10 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer QSO --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+srun -N 1 -C gpu -t 00:30:00 --gpus 4 --qos interactive --account desi_g python scripts/xirunpc.py --tracer QSO --survey Y1 --nran 3 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP  --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer LRG --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+srun -N 1 -C gpu -t 00:30:00 --gpus 4 --qos interactive --account desi_g python scripts/xirunpc.py --tracer LRG --survey Y1 --nran 5 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP  --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer ELG_LOPnotqso --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+srun -N 1 -C gpu -t 00:30:00 --gpus 4 --qos interactive --account desi_g python scripts/xirunpc.py --tracer ELG_LOPnotqso --nran 6 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP  --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/xirunpc.py --tracer BGS_BRIGHT-21.5 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --nthreads 128 --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
+srun -N 1 -C gpu -t 00:30:00 --gpus 4 --qos interactive --account desi_g python scripts/xirunpc.py --tracer BGS_BRIGHT-21.5 --nran 1 --survey Y1 --verspec iron --version $1 --region NGC SGC --corr_type smu --weight_type default_FKP --njack 0 --outdir /global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/LSScats/$1/xi/
