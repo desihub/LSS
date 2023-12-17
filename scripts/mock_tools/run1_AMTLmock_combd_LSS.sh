b6564d53ae41d46205463b1ac579e64188c8560a
Aurelio Carnero Rosell <aurelio.crosell@gmail.com>
2023-12-17 05:21:06 -0800

    changes

--- scripts/mock_tools/run1_AMTLmock_combd_LSS.sh
+++ scripts/mock_tools/run1_AMTLmock_combd_LSS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/altmtl{MOCKNUM} --mockver ab_secondgen --mocknum $1  --survey Y1 --add_gtl y --specdata iron --tracer bright --targDir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS --combd y
+python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_test/altmtl{MOCKNUM} --mockver ab_secondgen --mocknum $1  --survey Y1 --add_gtl y --specdata iron --tracer dark --targDir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_test --combd y
 
 #mv $SCRATCH/Y1/mocks/SecondGenMocks/AbacusSummit_v3/altmtl$1/mock$1/* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/altmtl$1/mock$1/
 #mv $SCRATCH/Y1/mocks/SecondGenMocks/AbacusSummit_v3/altmtl$1/fba$1/* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3/altmtl$1/fba$1/
--- scripts/mock_tools/run_Y1SecondGen_initialledger_batch.sh
+++ scripts/mock_tools/run_Y1SecondGen_initialledger_batch.sh
@@ -1,7 +1,9 @@
 SeconGenVer=AbacusSummit_v3 #AbacusSummit
-for j in {15..24}
-do
-	echo $j
-	echo $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/forFA$j.fits $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/altmtl$j/initled
-	python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/run_mtl_ledger.py $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/forFA$j.fits $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/altmtl$j/initled DARK
-done
+#for j in {15..24}
+#do
+j=0
+echo $j
+#echo $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/forFA$j.fits $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/altmtl$j/initled
+python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/run_mtl_ledger.py /pscratch/sd/a/acarnero/CutSky_v3_1/forFA$j.fits /pscratch/sd/a/acarnero/CutSky_v3_1/altmtl$j/initled DARK
+#python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/run_mtl_ledger.py $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/forFA$j.fits $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/altmtl$j/initled DARK
+#done
