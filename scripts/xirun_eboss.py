8f9ab8169dd46804aa2fd5cde1d50ff1f17a4eee
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-03 23:27:44 -0500

    Update xirun_eboss.py

--- scripts/xirun_eboss.py
+++ scripts/xirun_eboss.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
                 logger.info('Computing correlation function {} in region {} in redshift range {}.'.format(corr_type, region, (zmin, zmax)))
         edges = get_edges(corr_type=corr_type, bin_type=args.bin_type)
             
-        result = compute_correlation_function(corr_type, edges=edges, zlim=(zmin,zmax), region=region,nthreads=nthreads, gpu=gpu, njack=args.njack, mpicomm=mpicomm, mpiroot=mpiroot)
+        result = compute_correlation_function(corr_type, edges=edges, zlim=(zmin,zmax), region=region,nthreads=nthreads, gpu=gpu, njack=args.njack, mpicomm=mpicomm, mpiroot=mpiroot,tracer=tracer)
                 # Save pair counts
         if mpicomm is None or mpicomm.rank == mpiroot:
             
