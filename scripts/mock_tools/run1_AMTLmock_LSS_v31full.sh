ddfd5218f0c58fa88400470b54beeda05efa10d8
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-08 15:53:33 -0500

    Update run1_AMTLmock_LSS_v31full.sh

--- scripts/mock_tools/run1_AMTLmock_LSS_v31full.sh
+++ scripts/mock_tools/run1_AMTLmock_LSS_v31full.sh
@@ -9,4 +9,4 @@ python scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output $OUTBASE --mockv
 python scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output $OUTBASE --mockver ab_secondgen --mocknum $1  --survey Y1 --add_gtl y --specdata iron --tracer QSO --notqso n --minr 0 --maxr 18 --fulld y --fullr y --apply_veto y --use_map_veto _HPmapcut --mkclusran y  --nz y --mkclusdat y --splitGC y --targDir /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3_1
 
 mv $SCRATCH/Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/altmtl$1/mock$1/LSScats/* /global/cfs/cdirs/desi/survey/catalogs//Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/altmtl$1/mock$1/LSScats/
-chmod 775 /global/cfs/cdirs/desi/survey/catalogs//Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/altmtl$1/mock$1/LSScats/*clustering*
+chmod 775 /global/cfs/cdirs/desi/survey/catalogs//Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/altmtl$1/mock$1/LSScats/*
