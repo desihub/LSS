a240c8ea4ba82735524e7c3c3d487256fd8196d2
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-16 16:15:50 -0500

    Update testEZcov.py

--- scripts/mock_tools/testEZcov.py
+++ scripts/mock_tools/testEZcov.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import sys
 import fitsio
 import os
+
+
+
 
 def cat_ells(xis,indrange=None):
     if indrange is None:
@@ -147,7 +151,8 @@ def compchi2stats(tracer,zmin,zmax,smin,smax,rec='',ells=[0,2,4]):
 
 figs = []
 
-
+smin=20
+smax=200
 recl = ['recon_recsym','']
 ellsl = [[0,2,4],[0,2],[0]]
 
@@ -156,27 +161,38 @@ zrl = [(0.8,2.1)]
 for rec in recl:
     for zr in zrl:
         for ells in ellsl:
-            fig = compchi2stats(tp,zr[0],zr[1],20,200,rec=rec,ells=ells)
+            fig = compchi2stats(tp,zr[0],zr[1],smin,smax,rec=rec,ells=ells)
             figs.append(fig)
 
+outdir = '/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/'
+with PdfPages(outdir+'testEZmockcov_'+tp+_smin'+str(smin)+'smax'+str(smax)+'.pdf') as pdf:
+    for fig in figs:
+        pdf.savefig(fig)
+        plt.close()
+
+figs = []
 tp = 'ELG_LOP'
 zrl = [(0.8,1.1),(1.1,1.6)]
 for rec in recl:
     for zr in zrl:
         for ells in ellsl:
-            fig = compchi2stats(tp,zr[0],zr[1],20,200,rec=rec,ells=ells)
+            fig = compchi2stats(tp,zr[0],zr[1],smin,smax,rec=rec,ells=ells)
             figs.append(fig)
-
+with PdfPages(outdir+'testEZmockcov_'+tp+_smin'+str(smin)+'smax'+str(smax)+'.pdf') as pdf:
+    for fig in figs:
+        pdf.savefig(fig)
+        plt.close()
+            
+figs = []
 tp = 'LRG'
 zrl = [(0.4,0.6),(0.6,0.8),(0.8,1.1)]
 for rec in recl:
     for zr in zrl:
         for ells in ellsl:
-            fig = compchi2stats(tp,zr[0],zr[1],20,200,rec=rec,ells=ells)
+            fig = compchi2stats(tp,zr[0],zr[1],smin,smax,rec=rec,ells=ells)
             figs.append(fig)
 
-outdir = '/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/'
-with PdfPages(outdir+'testEZmockcov_smin20smax200.pdf') as pdf:
+with PdfPages(outdir+'testEZmockcov_'+tp+_smin'+str(smin)+'smax'+str(smax)+'.pdf') as pdf:
     for fig in figs:
         pdf.savefig(fig)
         plt.close()
