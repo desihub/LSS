52841148576008adc73beecf8307988dcb655f5e
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-18 16:55:50 -0500

    fix altmtl v3_1 LSS catalogs

--- scripts/mock_tools/abv31amtl_cat_sbatch.sh
+++ scripts/mock_tools/abv31amtl_cat_sbatch.sh
@@ -3,10 +3,10 @@
 #SBATCH --qos=regular
 #SBATCH --nodes=1
 #SBATCH --constraint=cpu
-#SBATCH --array=2-24
+#SBATCH --array=1-24
 #SBATCH --account=desi
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun scripts/mock_tools/run1_AMTLmock_LSS_3_1.sh $SLURM_ARRAY_TASK_ID
+srun scripts/mock_tools/run1_AMTLmock_LSS_3_1fix.sh $SLURM_ARRAY_TASK_ID
