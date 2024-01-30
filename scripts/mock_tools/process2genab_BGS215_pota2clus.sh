6300b93c81557fdf63123a5149040cabc6a3300f
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-30 16:02:23 -0500

    Update process2genab_BGS215_pota2clus.sh

--- scripts/mock_tools/process2genab_BGS215_pota2clus.sh
+++ scripts/mock_tools/process2genab_BGS215_pota2clus.sh
@@ -3,7 +3,7 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
- srun -N 1 -C cpu -t 00:45:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --mockver AbacusSummitBGS --tracer BGS_BRIGHT-21.5 --realization $i 
+ srun -N 1 -C cpu -t 00:45:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --mockver AbacusSummitBGS --tracer BGS_BRIGHT-21.5 --realization $i --prog BRIGHT
  mv $SCRATCH/AbacusSummitBGS/mock$i/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/mock$i/
  mv $SCRATCH/AbacusSummitBGS/mock$i/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS/mock$i/
  rm $SCRATCH/AbacusSummitBGS/mock$i/*
