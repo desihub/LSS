efa5ed5c2913ac9cce2002fd918d654d26c6b397
ashleyjross <ashley.jacob.ross@gmail.com>
2024-01-31 14:26:42 -0500

    Create add_RFweight_abLRGELGffa.sh

---
+++ scripts/mock_tools/add_RFweight_abLRGELGffa.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+for (( i=$1;i<=$2;i++ ))
+do
+  srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/addsys.py --tracer LRG_ffa --regressis y --add_regressis y --add_regressis_ran y --par y --realization $i --mockcatver v2
+  srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mock_tools/addsys.py --tracer ELG_LOP_ffa --regressis y --add_regressis y --add_regressis_ran y --par y --realization 1 --mockcatver v2
+
+done
+
