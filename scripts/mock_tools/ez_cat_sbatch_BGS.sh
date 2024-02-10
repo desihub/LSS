a1821fa928536810cdb849602cd08492a1713ebe
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-10 10:42:53 -0500

    Update ez_cat_sbatch_BGS.sh

--- scripts/mock_tools/ez_cat_sbatch_BGS.sh
+++ scripts/mock_tools/ez_cat_sbatch_BGS.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=regular
 #SBATCH --nodes=1
 #SBATCH --constraint=cpu
-#SBATCH --array=201-320
+#SBATCH --array=321-550
 #SBATCH --account=desi
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
