2160a65ad75b415cf83511c9dddd6be490c81519
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-10 10:52:28 -0500

    Create process2genabversion_pota2clus.sh

---
+++ scripts/mock_tools/process2genabversion_pota2clus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+for (( i=$1;i<=$2;i++ ))
+do
+ srun -N 1 -C cpu -t 00:45:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --realization $i --mockver AbacusSummit_$3
+ mv $SCRATCH/AbacusSummit_$3/mock$i/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$3/mock$i/
+ mv $SCRATCH/AbacusSummit_$3/mock$i/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$3/mock$i/
+ rm $SCRATCH/AbacusSummit_$3/mock$i/*
+ chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$3/mock$i/*clustering*
+ chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$3/mock$i/*nz*
+done
