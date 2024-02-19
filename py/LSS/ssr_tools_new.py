94706f9998ef8acf2bb2f048c7e22aa8c8dfc608
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-19 14:58:50 -0500

    Update ssr_tools_new.py

--- py/LSS/ssr_tools_new.py
+++ py/LSS/ssr_tools_new.py
@@ -172,8 +172,8 @@ class model_ssr_zfac:
         self.maxz = maxz
         
         self.res_mod_slp = self.get_slpfunc()
-        fo = open(self.outdir+outfn_root+'slp_wzfac.txt','w')
-        fo.write('self.res_mod_slp')
+        fo = open(outdir+outfn_root+'slp_wzfac.txt','w')
+        fo.write(str(self.res_mod_slp))
         #fo.write('#a b c chi2\n')
         #for par in pars:
         #   fo.write(str(par)+' ')
