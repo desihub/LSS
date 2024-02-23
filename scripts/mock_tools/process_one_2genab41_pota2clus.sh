ff6655b47ffe40c761e53b99cd6154b08926bcbf
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-23 12:14:15 -0500

    Update process_one_2genab41_pota2clus.sh

--- scripts/mock_tools/process_one_2genab41_pota2clus.sh
+++ scripts/mock_tools/process_one_2genab41_pota2clus.sh
@@ -4,6 +4,7 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 ver=v4_1
 
+python scripts/readwrite_pixel_bitmask.py --tracer lrg --input $1 --cat_type 'Ab2ndgen' --secgen_ver $ver
 python scripts/mock_tools/pota2clus_fast.py --realization $1 --mockver AbacusSummit_$ver
 mv $SCRATCH/AbacusSummit_$ver/mock$1/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$ver/mock$1/
 mv $SCRATCH/AbacusSummit_$ver/mock$1/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$ver/mock$1/
