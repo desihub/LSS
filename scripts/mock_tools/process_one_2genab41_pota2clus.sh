eb737bf06e5bc25eb2fe1dfe42a50295467ac86a
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-22 22:20:07 -0500

    Update process_one_2genab41_pota2clus.sh

--- scripts/mock_tools/process_one_2genab41_pota2clus.sh
+++ scripts/mock_tools/process_one_2genab41_pota2clus.sh
@@ -4,7 +4,7 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
 ver=v4_1
 
-srun -N 1 -C cpu -t 00:45:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --realization $1 --mockver AbacusSummit_$ver
+python scripts/mock_tools/pota2clus_fast.py --realization $1 --mockver AbacusSummit_$ver
 mv $SCRATCH/AbacusSummit_$ver/mock$1/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$ver/mock$1/
 mv $SCRATCH/AbacusSummit_$ver/mock$1/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_$ver/mock$1/
 rm $SCRATCH/AbacusSummit_$ver/mock$1/*
