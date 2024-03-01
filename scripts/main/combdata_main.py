5259635c366b26d5542b3ff04f9fd266bf8666b6
ashleyjross <ashley.jacob.ross@gmail.com>
2024-02-29 23:01:41 -0500

    Update combdata_main.py

--- scripts/main/combdata_main.py
+++ scripts/main/combdata_main.py
@@ -545,11 +545,11 @@ if specrel == 'daily' and args.dospec == 'y' and args.survey == 'main':
             #tj.write(outfs,format='fits', overwrite=True)
             print('joined to spec data and wrote out to '+outfs)
 
-        #if uptileloc:
-        #    print('counting tiles')
-        #    tc = ct.count_tiles_better('dat',tp+notqso,specrel=specrel) 
-        #    print('writing tile counts')
-        #    tc.write(outtc,format='fits', overwrite=True)
+        if uptileloc:
+            print('counting tiles')
+            tc = ct.count_tiles_better('dat',tp+notqso,specrel=specrel) 
+            print('writing tile counts')
+            tc.write(outtc,format='fits', overwrite=True)
 
 
 if args.get_petalsky == 'y':
