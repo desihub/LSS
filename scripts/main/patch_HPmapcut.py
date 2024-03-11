c3cdade1a6fa0ea69db473be9a5d97ef5d00eb6e
ashleyjross <ashley.jacob.ross@gmail.com>
2024-03-11 17:01:26 -0400

    Update patch_HPmapcut.py

--- scripts/main/patch_HPmapcut.py
+++ scripts/main/patch_HPmapcut.py
@@ -35,8 +35,10 @@ if args.tracers == 'all':
 for tp in tps:
     mainp = main(tp,args.verspec,survey=args.survey)
     df_cutdisk = Table(fitsio.read(indir+tp+'_full_HPmapcut.dat.fits'))
-    df_cutdisk.remove_columns(['BITWEIGHTS','PROB_OBS'])
     df_cutdisk_cols = list(df_cutdisk.dtype.names)
+    if 'BITWEIGHTS' in df_cutdisk_cols:
+        df_cutdisk.remove_columns(['BITWEIGHTS','PROB_OBS'])
+    
     df = Table(fitsio.read(indir+tp+'_full.dat.fits'))
     df_cols = list(df.dtype.names)
     for name in df_cols:
