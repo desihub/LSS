9b009094daf102755b6ded2b3d1545767ec2224b
ashleyjross <ashley.jacob.ross@gmail.com>
2024-03-25 22:55:17 -0400

    Update patch_rand_targetid_data.py

--- scripts/patch_rand_targetid_data.py
+++ scripts/patch_rand_targetid_data.py
@@ -29,12 +29,12 @@ if args.tracers != 'all':
 for tracer in tracers:
     # load catalogs
     for reg in regl:
-        data_cat_fn = indir tracer+'_'+reg+'_clustering.dat.fits'
+        data_cat_fn = indir +tracer+'_'+reg+'_clustering.dat.fits'
         data = Table(fitsio.read(data_cat_fn))
         data_cat.rename_column('TARGETID', 'TARGETID_DATA')
         data.keep_columns(['Z','WEIGHT_COMP','TARGETID_DATA'])
         for rann in range(0,args.nran):
-            ran_cat_fn = indir tracer+'_'+reg+'_'+str(rann)+'_clustering.dat.fits'
+            ran_cat_fn = indir +tracer+'_'+reg+'_'+str(rann)+'_clustering.dat.fits'
             ran = Table(fitsio.read(ran_cat_fn))
             # remove current random TARGETID_DATA
             ran.remove_column('TARGETID_DATA')
