ba196668546ec64b3a6e49b4126b4b655aa1683c
ashleyjross <ashley.jacob.ross@gmail.com>
2024-03-29 17:18:54 -0400

    Create comp_amtl_asgn.py

---
+++ scripts/comp_amtl_asgn.py
@@ -0,0 +1,102 @@
+import numpy as np
+import fitsio
+import glob
+from astropy.table import Table
+
+import LSS.common_tools as common
+from LSS.globals import main
+
+#just start with the mock 1 v3_1 altmtl as an example
+
+alltids = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/forFA1.fits',columns=['TARGETID'])
+alltids = np.unique(alltids['TARGETID'])
+
+def removeLeadingZeros(num): 
+  
+    # traverse the entire string 
+    for i in range(len(num)): 
+  
+        # check for the first non-zero character 
+        if num[i] != '0': 
+            # return the remaining string 
+            res = num[i::]; 
+            return res; 
+          
+    # If the entire string is traversed 
+    # that means it didn't have a single 
+    # non-zero character, hence return "0" 
+    return "0";
+
+def get_all_asgn(indir):
+    fls = glob.glob(indir+'/*/fba*.fits')
+    assign_list = []
+    for fl in fls:
+        asgn = Table(fitsio.read(fl,columns=['FIBER', 'TARGETID', 'LOCATION']))
+        sp = fl.split('-')
+        tid = int(removeLeadingZeros(sp[-1].strip('.fits')))
+        #print(tid)
+        asgn['TILEID'] = tid
+        sel = asgn['TARGETID'] > 0
+        assign_list.append(asgn[sel])
+    all_asgn = np.concatenate(assign_list)
+    return all_asgn
+
+#get the list of good tilelocid
+
+mainp = main('LRG','iron')
+pdir = 'dark'
+mt = mainp.mtld
+tiles = mainp.tiles
+
+
+tsnrcut = mainp.tsnrcut
+dchi2 = mainp.dchi2
+tnsrcol = mainp.tsnrcol        
+badfib = mainp.badfib
+
+
+wd = mt['SURVEY'] == 'main'
+wd &= mt['ZDONE'] == 'true'
+wd &= mt['FAPRGRM'] == pdir
+wd &=mt['ZDATE'] < 20220900 #Y1 cutoff
+
+mtld = mt[wd]
+
+specfo = ldirspec+'datcomb_'+pdir+'_spec_zdone.fits'
+logger.info('loading specf file '+specfo)
+specf = Table(fitsio.read(specfo))
+sel = np.isin(specf['TILEID'],mtld['TILEID'])
+specf = specf[sel]
+specf['TILELOCID'] = 10000*specf['TILEID'] +specf['LOCATION']
+    
+logger.info('loaded specf file '+specfo)
+specfc = common.cut_specdat(specf,badfib=mainp.badfib,tsnr_min=tsnrcut,tsnr_col=tnsrcol)
+gtl = np.unique(specfc['TILELOCID'])
+
+assign_real_dic = {}
+
+def get_good_real(real_num)
+    indir = '/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v3_1/altmtl1_R64/Univ'+str(real_num).zfill(3)+'/fa/MAIN'
+    all_asgn = get_all_asgn(indir)
+    asgn_tloc = 10000*all_asgn['TILEID'] +all_asgn['LOCATION']
+    good_asgn = np.isin(asgn_tloc,gtl)
+    good_tids = all_asgn['TARGETID'][gtl]
+    asgn_real = np.isin(alltids,good_tids)
+    assign_real_dic[real_num] = asgn_real
+
+from multiprocessing import Pool
+Nreal = 64
+inds = np.arange(0,Nreal)
+pool = sharedmem.MapReduce(np=6)
+with pool:
+    pool.map(get_good_real,inds)
+
+print('got all realizations')
+
+probl = np.zeros(len(alltids))
+for i in range(0,len(alltids)):
+    nt = 0
+    for real in inds:
+        nt += assign_real_dic[real][i]
+    prob = nt/Nreal
+    probl[i] = prob
