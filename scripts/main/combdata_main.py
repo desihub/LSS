f4d3d6c8f2b5d489310c1fd06acc944f4e7b10b8
ashleyjross <ashley.jacob.ross@gmail.com>
2024-04-07 12:55:16 -0400

    Update combdata_main.py

--- scripts/main/combdata_main.py
+++ scripts/main/combdata_main.py
@@ -275,14 +275,15 @@ if specrel == 'daily' and args.survey == 'DA2':
     if args.par == 'y':
         #test of what goes in parallel
         tid = tiles4comb['TILEID'][0]
-        sel = tiles4comb['TILEID'] = tid
+        sel = tiles4comb['TILEID'] == tid
         tl_tab = tiles4comb[sel]
+        print(tl_tab)
         tab = ct.get_tiletab(tl_tab)
         from multiprocessing import Process, Manager
         manager = Manager()
         tile_list = manager.list()
         def _tab2list(tlist,tid):
-            sel = tiles4comb['TILEID'] = tid
+            sel = tiles4comb['TILEID'] == tid
             tl_tab = tiles4comb[sel]
             tab = ct.get_tiletab(tl_tab)
             tlist.append(tab)
