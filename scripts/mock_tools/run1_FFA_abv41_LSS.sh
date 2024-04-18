488b5c0f763fbd2c13d3a228c842fbc8b1fb9927
ashleyjross <ashley.jacob.ross@gmail.com>
2024-04-18 14:45:41 -0400

    Update run1_FFA_abv41_LSS.sh

--- scripts/mock_tools/run1_FFA_abv41_LSS.sh
+++ scripts/mock_tools/run1_FFA_abv41_LSS.sh
@@ -4,5 +4,5 @@ python scripts/mock_tools/ffa2clus_fast.py --mockver AbacusSummit_v4_1 --realiza
 mv $SCRATCH/AbacusSummit_v4_1/mock$1/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/
 mv $SCRATCH/AbacusSummit_v4_1/mock$1/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/
 rm $SCRATCH/AbacusSummit_v4_1/mock$1/*
-chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/*clustering*
-chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/*nz*
+chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/*ffa*clustering*
+chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4_1/mock$1/*ffa*nz*
