871e1a4bc49ba38ba976f8964b3f49cbf1e31161
ashleyjross <ashley.jacob.ross@gmail.com>
2024-04-21 13:05:24 -0400

    Update test_fafiles.py

--- scripts/test_fafiles.py
+++ scripts/test_fafiles.py
@@ -2,7 +2,23 @@ import numpy as np
 import fitsio
 from glob import glob
 
-fls = glob('/dvs_ro/cfs/cdirs/desi/survey/fiberassign/main/*/*.fits')
+fls = glob('/dvs_ro/cfs/cdirs/desi/survey/fiberassign/main/*/targ.fits')
+
+for fl in fls:
+	try:
+		f = fitsio.read(fl,rows=1)
+	except:
+	    print(fl)
+	    
+fls = glob('/dvs_ro/cfs/cdirs/desi/survey/fiberassign/main/*/sky.fits')
+
+for fl in fls:
+	try:
+		f = fitsio.read(fl,rows=1)
+	except:
+	    print(fl)
+	    
+fls = glob('/dvs_ro/cfs/cdirs/desi/survey/fiberassign/main/*/sncd.fits')
 
 for fl in fls:
 	try:
