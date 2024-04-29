5042c7095f67d35052bd6e5ac3017b676c268fae
ashleyjross <ashley.jacob.ross@gmail.com>
2024-04-29 14:08:44 -0400

    Create ELG_LSScat_pipe_altmtl.sh

---
+++ scripts/ELG_LSScat_pipe_altmtl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+source /global/common/software/desi/desi_environment.sh main
+export LSSCODE=$HOME
+PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
+
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/main/mkCat_main.py --type ELG_LOP --notqso y --fulld n --survey Y1 --verspec iron --version $1 --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_SN --basedir /global/cfs/cdirs/desi/survey/catalogs/ --compmd altmtl
+
