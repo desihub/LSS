e13237e50d0aab32611496dae52470451a90b804
ashleyjross <ashley.jacob.ross@gmail.com>
2024-04-29 14:20:06 -0400

    Update QSO_LSScat_pipe_altmtl.sh

--- scripts/QSO_LSScat_pipe_altmtl.sh
+++ scripts/QSO_LSScat_pipe_altmtl.sh
@@ -7,5 +7,5 @@ export LSSCODE=$HOME
 PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
 
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/main/mkCat_main.py --type ELG_LOP --fulld n --survey Y1 --verspec iron --version $1 --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_RF --basedir /global/cfs/cdirs/desi/survey/catalogs/ --compmd altmtl
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/main/mkCat_main.py --type QSO --fulld n --survey Y1 --verspec iron --version $1 --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_RF --basedir /global/cfs/cdirs/desi/survey/catalogs/ --compmd altmtl
 
