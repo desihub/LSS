29d39b2a85b0deb45e5be52b81fb32a0ee92e18a
ashleyjross <ashley.jacob.ross@gmail.com>
2024-05-01 12:06:35 -0400

    Update pota2clus_fast.py

--- scripts/mock_tools/pota2clus_fast.py
+++ scripts/mock_tools/pota2clus_fast.py
@@ -63,7 +63,7 @@ parser.add_argument("--data_dir",help="where to find the data randoms",default='
 parser.add_argument("--specdata_dir",help="where to find the spec data ",default='/global/cfs/cdirs/desi/survey/catalogs/Y1/LSS/iron/')
 parser.add_argument("--minr", help="minimum number for random files",default=0,type=int)
 parser.add_argument("--maxr", help="maximum for random files, default is all 18)",default=18,type=int) 
-parser.add_argument("--mockver", default='AbacusSummit_v3_1', help = "which mocks to use")
+parser.add_argument("--mockver", default='AbacusSummit_v4_1fixran', help = "which mocks to use")
 parser.add_argument("--mockcatver", default=None, help = "if not None, gets added to the output path")
 
 parser.add_argument("--tracer", default = 'all')
@@ -128,13 +128,14 @@ def splitGC(flroot,datran='.dat',rann=0):
 
 def ran_col_assign(randoms,data,sample_columns,tracer):
     data.rename_column('TARGETID', 'TARGETID_DATA')
+    rng = np.random.default_rng(seed=seed)
     def _resamp(selregr,selregd):
         for col in sample_columns:
             randoms[col] =  np.zeros_like(data[col],shape=len(randoms))
         rand_sel = [selregr,~selregr]
         dat_sel = [ selregd,~selregd]
         for dsel,rsel in zip(dat_sel,rand_sel):
-            inds = np.random.choice(len(data[dsel]),len(randoms[rsel]))
+            inds = rng.choice(len(data[dsel]),len(randoms[rsel]))
             #logger.info(str(len(data[dsel]),len(inds),np.max(inds))
             dshuf = data[dsel][inds]
             for col in sample_columns:
