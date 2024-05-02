5254ff83aa3206666500f6917dc3f69dcc9848d3
ashleyjross <ashley.jacob.ross@gmail.com>
2024-05-01 22:16:31 -0400

    Update abv41complete_cat_sbatch.sh

--- scripts/mock_tools/abv41complete_cat_sbatch.sh
+++ scripts/mock_tools/abv41complete_cat_sbatch.sh
@@ -3,10 +3,10 @@
 #SBATCH --qos=regular
 #SBATCH --nodes=1
 #SBATCH --constraint=cpu
-#SBATCH --array=2-24
+#SBATCH --array=1-24
 #SBATCH --account=desi
 
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-srun scripts/mock_tools/process_one_2genab41_pota2clus.sh $SLURM_ARRAY_TASK_ID
+srun scripts/mock_tools/process_one_2genab41ranfix_pota2clus.sh $SLURM_ARRAY_TASK_ID
