8ffef0e26111942a9b92b41dbe4f36d325d61a0f
ashleyjross <ashley.jacob.ross@gmail.com>
2024-05-01 22:15:33 -0400

    Create process_one_2genab41ranfix_pota2clus.sh

---
+++ scripts/mock_tools/process_one_2genab41ranfix_pota2clus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+ver=v4_1ranfix
+
+python scripts/mock_tools/pota2clus_fast.py --realization $1 --mockver AbacusSummit_$ver --outloc prod
