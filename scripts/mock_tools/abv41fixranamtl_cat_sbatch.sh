66e41d39ac3c0439990ee4e93c6c6b95b921d889
ashleyjross <ashley.jacob.ross@gmail.com>
2024-05-02 14:21:39 -0400

    Create abv41fixranamtl_cat_sbatch.sh

---
+++ scripts/mock_tools/abv41fixranamtl_cat_sbatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --constraint=cpu
+#SBATCH --array=1-24
+#SBATCH --account=desi
+
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
+
+srun scripts/mock_tools/run1_AMTLmock_LSS_v4_1fixran.sh $SLURM_ARRAY_TASK_ID
