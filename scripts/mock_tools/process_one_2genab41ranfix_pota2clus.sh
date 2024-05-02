952641dfdfc6a54f1c28650be3ff94ca981f5594
ashleyjross <ashley.jacob.ross@gmail.com>
2024-05-01 22:35:55 -0400

    Update process_one_2genab41ranfix_pota2clus.sh

--- scripts/mock_tools/process_one_2genab41ranfix_pota2clus.sh
+++ scripts/mock_tools/process_one_2genab41ranfix_pota2clus.sh
@@ -2,6 +2,6 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 
-ver=v4_1ranfix
+ver=v4_1fixran
 
 python scripts/mock_tools/pota2clus_fast.py --realization $1 --mockver AbacusSummit_$ver --outloc prod
