07822aca3fdadf82b2b1048387b052c840f07d44
AlbertoRosado1 <ar652820@ohio.edu>
2024-05-16 17:44:10 -0700

    update validation_improp_full.py

--- scripts/validation/validation_improp_full.py
+++ scripts/validation/validation_improp_full.py
@@ -239,6 +239,7 @@ for tp in tps:
         dosky_g = 'n'
         do_ebvnew_diff = 'y'
         do_lrgmask = 'n'
+        do_ebvnocib_diff = 'y'
         print('doing validation for '+tp)
         
     if tp[:3] == 'ELG' or tp[:3] == 'BGS':
