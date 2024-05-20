aa0fb7ec4206447e43b47dd09a1333c9d203c18c
Aurelio Carnero Rosell <aurelio.crosell@gmail.com>
2024-05-20 07:41:35 -0700

    removing autoexecutable

--- bin/runAltMTLRealizations.py
+++ bin/runAltMTLRealizations.py
@@ -1,4 +1,6 @@
-#!/global/common/software/desi/perlmutter/desiconda/20230111-2.1.0/conda/bin/python -u
+#/global/common/software/desi/perlmutter/desiconda/20240425-2.2.0/conda/bin/python -u
+
+#/global/common/software/desi/perlmutter/desiconda/20230111-2.1.0/conda/bin/python -u
 from desiutil.iers import freeze_iers
 freeze_iers()
 
