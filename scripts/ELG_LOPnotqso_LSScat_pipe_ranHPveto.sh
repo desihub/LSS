922c85b62bfdf9e8087f2cd586cff8f6b14f27a1
ashleyjross <ashley.jacob.ross@gmail.com>
2024-06-05 14:01:09 -0400

    scripts to process selected pipeline steps

--- scripts/ELG_LOPnotqso_LSScat_pipe_ranHPveto.sh
+++ scripts/ELG_LOPnotqso_LSScat_pipe_ranHPveto.sh
@@ -8,10 +8,6 @@ PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
 
 TRACER='ELG_LOP'
 
-srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main_ran.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/   --verspec iron --survey Y1 --add_tl y --apply_veto y --maxr 18 --version $1
-
-python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --verspec iron --survey Y1  --mkHPmaps y --version $1
-
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --ranonly y --apply_map_veto y --imsys y --survey Y1 --verspec iron --imsys_zbin y --use_map_veto _HPmapcut --version $1
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi scripts/Y1_sysnetELG_zbins_new.sh $1 #note, this loads cosmodesi to run
