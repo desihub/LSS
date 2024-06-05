b359e34b01cf6e4855ef09188da7f7830cb10548
ashleyjross <ashley.jacob.ross@gmail.com>
2024-06-05 12:35:00 -0400

    Update ELG_LOPnotqso_LSScat_pipe_ranveto.sh

--- scripts/ELG_LOPnotqso_LSScat_pipe_ranveto.sh
+++ scripts/ELG_LOPnotqso_LSScat_pipe_ranveto.sh
@@ -12,7 +12,7 @@ srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkC
 
 python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --verspec iron --survey Y1  --mkHPmaps y --version $1
 
-python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --imsys y --survey Y1 --verspec iron --imsys_zbin y --use_map_veto _HPmapcut --version $1
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER --notqso y --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n ranonly y --apply_map_veto y --imsys y --survey Y1 --verspec iron --imsys_zbin y --use_map_veto _HPmapcut --version $1
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi scripts/Y1_sysnetELG_zbins_new.sh $1 #note, this loads cosmodesi to run
 
