5266750022b2311a42e23315da78fa9c64e5f24a
ashleyjross <ashley.jacob.ross@gmail.com>
2024-07-04 13:05:42 -0400

    Create Y3_sysnetELG_zbins.sh

---
+++ scripts/Y3_sysnetELG_zbins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+export LSSDIR=$HOME
+#export SYSNETDIR=$HOME/desicode
+export LSSBASE=/global/cfs/cdirs/desi/survey/catalogs/
+PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py
+
+verspec=jura-v1
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --prepsysnet y --imsys_zbin y --fulld n --survey DA2 --verspec $verspec --version $1
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELG_LOPnotqso0.8_1.1 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELG_LOPnotqso0.8_1.1 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELG_LOPnotqso0.8_1.1 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELG_LOPnotqso0.8_1.1 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELG_LOPnotqso1.1_1.6 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELG_LOPnotqso1.1_1.6 true false 1024 0.003 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELG_LOPnotqso1.1_1.6 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELG_LOPnotqso1.1_1.6 false true 1024 0.004 dnnp pnll $1 $LSSBASE/Y1/LSS/$verspec/LSScats/
+
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG_LOP --notqso y --add_sysnet y --imsys_zbin y --fulld n --survey DA2 --verspec $verspec --version $1
+
+python scripts/validation/validation_improp_full.py --tracers ELG_LOPnotqso --version $1
