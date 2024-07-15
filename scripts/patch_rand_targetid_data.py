6c196d176dce0fd768164ced8dc1acc8fdf1e28d
AlbertoRosado1 <ar652820@ohio.edu>
2024-07-15 11:36:45 -0700

    added 'par' option

--- scripts/patch_rand_targetid_data.py
+++ scripts/patch_rand_targetid_data.py
@@ -18,6 +18,7 @@ parser.add_argument("--verspec",help="version for redshifts",default='iron')
 parser.add_argument("--data",help="LSS or mock directory",default='LSS')
 parser.add_argument("--blinded",help="blinded or unblinded catalogs",default='unblinded')
 parser.add_argument("--nran",help="number of randoms to process",default=1,type=int)
+parser.add_argument("--par",help="process randoms in parallel?",default='n')
 args = parser.parse_args()
 
 
@@ -37,9 +38,11 @@ for tracer in tracers:
         data.keep_columns(['Z','WEIGHT_COMP','TARGETID_DATA','WEIGHT_SYS'])
         datal.append(data)
     data = vstack(datal)
-    for reg in regl:
-        for rann in range(0,args.nran):
+    
+    def _fix_tid_ran(rann):
+        for reg in regl:
             ran_cat_fn = indir +tracer+'_'+reg+'_'+str(rann)+'_clustering.ran.fits'
+            print(f"loading {ran_cat_fn}")
             ran = Table.read(ran_cat_fn,memmap=True)
             # remove current random TARGETID_DATA
             ran.remove_column('TARGETID_DATA')
@@ -58,6 +61,37 @@ for tracer in tracers:
                 print(f"number of null TARGETID_DATA in joined catalog = {len_mask}")
                 print(f"length of original random catalog = {len(ran)}")
                 print(f"length of new joined catalog  ... = {len(join_cat)}")
+
+    inds = np.arange(0,args.nran)
+    if args.par == 'n':
+        for rn in inds:
+            _fix_tid_ran(rn)
+    if args.par == 'y':
+        from multiprocessing import Pool
+        with Pool() as pool:
+            res = pool.map(_fix_tid_ran, inds)
+    
+    #for reg in regl:
+    #    for rann in range(0,args.nran):
+    #        ran_cat_fn = indir +tracer+'_'+reg+'_'+str(rann)+'_clustering.ran.fits'
+    #        ran = Table.read(ran_cat_fn,memmap=True)
+    #        # remove current random TARGETID_DATA
+    #        ran.remove_column('TARGETID_DATA')
+    #        # join catalogs
+    #        join_cat = join(ran, data, keys = ['Z','WEIGHT_COMP','WEIGHT_SYS'], join_type ='left')
+
+    #        len_mask = len(join_cat[join_cat['TARGETID_DATA'] == ma.masked])
+    #        if len_mask == 0 and len(ran) == len(join_cat):
+    #            common.write_LSS(join_cat,ran_cat_fn.replace('dvs_ro','global'))
+    #            
+    #            print(tracer,reg,str(rann))
+    #            print('SUCCESS')
+    #        else:
+    #            print(tracer,reg,str(rann))
+    #            print('FAILURE!')
+    #            print(f"number of null TARGETID_DATA in joined catalog = {len_mask}")
+    #            print(f"length of original random catalog = {len(ran)}")
+    #            print(f"length of new joined catalog  ... = {len(join_cat)}")
 
 
 
