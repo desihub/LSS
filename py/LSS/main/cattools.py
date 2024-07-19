02881cbcee58135507c39f96cd4cb5bdfdf8ade4
ashleyjross <ashley.jacob.ross@gmail.com>
2024-07-18 21:15:23 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -3627,7 +3627,7 @@ def mkclusdat(fl,weighttileloc=True,zmask=False,tp='',dchi2=9,tsnrcut=80,rcut=No
         wz &= ff['ZWARN'] != 999999
         if dchi2 is not None:
             wz &= ff['o2c'] > dchi2
-            common.printlog('length after oII cut '+str(len(ff[wz])),ogger)
+            common.printlog('length after oII cut '+str(len(ff[wz])),logger)
         wz &= ff['LOCATION_ASSIGNED'] == 1
         common.printlog('length after also making sure location assigned '+str(len(ff[wz])),logger)
         if not ismock:
