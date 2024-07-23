ccd4474283403a058f29df55592fc26b853ebe5f
ashleyjross <ashley.jacob.ross@gmail.com>
2024-07-23 18:30:15 -0400

    Update process2genabv2_BGS215_pota2clus.sh

--- scripts/mock_tools/process2genabv2_BGS215_pota2clus.sh
+++ scripts/mock_tools/process2genabv2_BGS215_pota2clus.sh
@@ -4,9 +4,9 @@ PYTHONPATH=$PYTHONPATH:$HOME/LSS/py
 for (( i=$1;i<=$2;i++ ))
 do
  srun -N 1 -C cpu -t 00:45:00 --qos interactive --account desi python scripts/mock_tools/pota2clus_fast.py --mockver AbacusSummitBGS_v2 --tracer BGS_BRIGHT-21.5 --realization $i --prog BRIGHT
- mv $SCRATCH/AbacusSummitBGS/mock$i/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/
- mv $SCRATCH/AbacusSummitBGS/mock$i/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/
- rm $SCRATCH/AbacusSummitBGS/mock$i/*
+ mv $SCRATCH/AbacusSummitBGS_v2/mock$i/*GC* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/
+ mv $SCRATCH/AbacusSummitBGS_v2/mock$i/*nz* /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/
+ rm $SCRATCH/AbacusSummitBGS_v2/mock$i/*
  chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/*clustering*
  chmod 775 /global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummitBGS_v2/mock$i/*nz*
 done
