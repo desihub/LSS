1f99cb28f83122a17e27f626c8d329bee14d468d
ashleyjross <ashley.jacob.ross@gmail.com>
2024-08-21 15:53:15 -0400

    Update add_bitweights_2full.py

--- scripts/add_bitweights_2full.py
+++ scripts/add_bitweights_2full.py
@@ -58,7 +58,7 @@ for tp in tpl:
             infl = join(infl,bitf,keys=['TARGETID'],join_type='left')
             lij = len(infl)
             if li == lij:
-                common.write_LSS(infl,inflnm)
+                common.write_LSS_scratchcp(infl,inflnm)
             else:
                 print('mismatch after join!')
                 print(tp,li,lij)    
