5e7648592a6a697a8dd63f1400e26fa7f9af17e6
ashleyjross <ashley.jacob.ross@gmail.com>
2024-08-23 20:42:32 -0400

    Update cosmodesi_io_tools.py

--- py/LSS/cosmodesi_io_tools.py
+++ py/LSS/cosmodesi_io_tools.py
@@ -456,13 +456,13 @@ def read_full_positions_weights(name='data', weight_type='default', fibered=Fals
     def read_positions_weights(name):
         positions, weights = [], []
         for reg in region:
-            cat_fn = catalog_fn(ctype='full_HPmapcut', name=name, **kwargs).replace('PIP/','')
+            cat_fn = catalog_fn(ctype='full_HPmapcut', name=name, **kwargs)
             #cat_fn = catalog_fn(ctype='full', name=name, **kwargs)
             logger.info('Loading {}.'.format(cat_fn))
             if isinstance(cat_fn, (tuple, list)):
-                catalog = vstack([Table.read(fn) for fn in cat_fn])
+                catalog = vstack([Table.read(fn.replace('PIP/','')) for fn in cat_fn])
             else:
-                catalog = Table.read(cat_fn)
+                catalog = Table.read(cat_fn.replace('PIP/',''))
             p, w = get_full_positions_weights(catalog, name=name, weight_type=weight_type, fibered=fibered, region=reg, weight_attrs=weight_attrs)
             positions.append(p)
             weights.append(w)
