37eb10e826a9ef41481ea41f628b8bfdaa97ff39
ashleyjross <ashley.jacob.ross@gmail.com>
2024-10-07 15:43:08 -0400

    Update get_dNdz_allmonths.py

--- Sandbox/get_dNdz_allmonths.py
+++ Sandbox/get_dNdz_allmonths.py
@@ -23,7 +23,7 @@ def dndz_monthall(yearmonths,tp,zcol='Z_not4clus'):
     mainp = main(tp,'daily')
     if tp == 'QSO':
         dt = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/main/LSS/daily/datcomb_QSO_tarspecwdup_zdone.fits')
-        print(dt.dtype.names)
+        #print(dt.dtype.names)
         dt = common.cut_specdat(dt,mainp.badfib)
         sel = dt['PRIORITY'] == 3400 #select QSO on 1st obs
         dt = dt[sel]
@@ -33,8 +33,8 @@ def dndz_monthall(yearmonths,tp,zcol='Z_not4clus'):
         arz['TILEID'] = arz['TILEID'].astype(int)
 
         dt = join(dt,arz,keys=['TARGETID','TILEID','LOCATION'],join_type='left',uniq_col_name='{col_name}{table_name}',table_names=['','_QF'])
-        print(dt.dtype.names)
-        zcol = 'Z_QF'
+        #print(dt.dtype.names)
+        zcol = 'Z'
         
     elif tp != 'ELGnotqso' and tp != 'ELGandQSO':
         dt = fitsio.read('/global/cfs/cdirs/desi/survey/catalogs/main/LSS/daily/LSScats/test/'+tp+'_full.dat.fits')
