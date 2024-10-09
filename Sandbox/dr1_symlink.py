974f45c6eddc61c1c8bae59b3f60ad4ccb53bff4
ashleyjross <ashley.jacob.ross@gmail.com>
2024-10-09 14:48:06 -0400

    Update dr1_symlink.py

--- Sandbox/dr1_symlink.py
+++ Sandbox/dr1_symlink.py
@@ -24,10 +24,10 @@ for ver in versions:
 
 fnames = ['LSS/collisions-BRIGHT.fits','LSS/collisions-DARK.fits','tiles-BRIGHT.fits','tiles-DARK.fits']
 for fname in fnames:
-    os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+    os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
-os.sys('ln -s '+indir+'/LSS/altmtl/JL_Y1Run2DARK '+pubdir+'/LSS/altmtl/DARK')
-os.sys('ln -s '+indir+'/LSS/altmtl/JL_Y1Run3BRIGHT '+pubdir+'/LSS/altmtl/BRIGHT')
+os.system('ln -s '+indir+'/LSS/altmtl/JL_Y1Run2DARK '+pubdir+'/LSS/altmtl/DARK')
+os.system('ln -s '+indir+'/LSS/altmtl/JL_Y1Run3BRIGHT '+pubdir+'/LSS/altmtl/BRIGHT')
 
 progl = ['DARK','BRIGHT']
 ranl = np.arange(0,18)
@@ -35,35 +35,35 @@ ranl = np.arange(0,18)
 for rann in ranl:
     for prog in progl:
         fname = 'LSS/random'+str(rann)+'/pota-'+prog+'.fits'
-        os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+        os.system('ln -s '+indir+fname+' ' +pubdir+fname)
         fname = 'LSS/random'+str(rann)+'/'+args.specrel+'/rancomb_'+str(rann)+prog.lower()+'wdupspec_zdone.fits'
-        os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+        os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
 fname = 'LSS/'+args.specrel+'/unique_badfibers.txt'
-os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
 tracers = ['BGS_ANY','BGS_BRIGHT','LRG','ELG_LOPnotqso','QSO']
 for tr in tracers:
     fname = 'LSS/'+args.specrel+'/datcomb_'+tr+'_tarspecwdup_zdone.fits'
-    os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+    os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
 for prog in progl:
     fname = 'LSS/'+args.specrel+'/datcomb_'+prog.lower()+'_spec_zdone.fits'
-    os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+    os.system('ln -s '+indir+fname+' ' +pubdir+fname)
     fname = 'LSS/'+args.specrel+'/datcomb_'+prog.lower()+'_zmtl_zdone.fits'
-    os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+    os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
 fname = 'LSS/'+args.specrel+'/emlin_catalog.fits'
-os.sys('ln -s '+indir+fname+' ' +pubdir+fname)
+os.system('ln -s '+indir+fname+' ' +pubdir+fname)
 
 
 for ver in version:
 	fnames = glob.glob(indir+'/LSS/'+args.specrel+'/LSScats/'+ver+'/*.fits')
 	for fname in fnames:
-		os.sys('ln -s '+fname +' ' +pubdir+fname.replace(indir,''))
+		os.system('ln -s '+fname +' ' +pubdir+fname.replace(indir,''))
 	fnames = glob.glob(indir+'/LSS/'+args.specrel+'/LSScats/'+ver+'/*nz.txt')
 	for fname in fnames:
-		os.sys('ln -s '+fname +' ' +pubdir+fname.replace(indir,''))
+		os.system('ln -s '+fname +' ' +pubdir+fname.replace(indir,''))
 	dirname = '/LSS/'+args.specrel+'/LSScats/'+ver+'/hpmaps'
-	os.sys('ln -s '+indir+dirname+' ' +pubdir+dirname)
+	os.system('ln -s '+indir+dirname+' ' +pubdir+dirname)
 	
