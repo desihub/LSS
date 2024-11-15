5f5cd4b039e6f48e087c6c5c6aa5943fddf58fe4
ashleyjross <ashley.jacob.ross@gmail.com>
2024-11-15 16:06:45 -0500

    Create QSO_loa_v11.sh

---
+++ scripts/QSO_loa_v11.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+source /global/common/software/desi/desi_environment.sh main
+export LSSCODE=$HOME
+PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
+
+verspec=loa-v1
+echo $verspec
+survey=DA2
+TRACER='QSO'
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER  --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec $verspec --survey $survey --version $1
+
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER  --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec $verspec --survey $survey --maxr 0 --version $1
+
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/LSS/scripts/main/mkCat_main.py --type $TRACER  --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_map_veto y --maxr 0 --verspec $verspec --survey $survey --version $1
+
+#12 minutes
+python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --imsys y --survey $survey --verspec $verspec --imsys_zbin y --use_map_veto _HPmapcut --version $1
+
+#~3 minutes
+python $LSSCODE/LSS/scripts/main/patch_HPmapcut.py --tracers QSO --survey $survey --verspec $verspec --version $1
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --fulld n --survey $survey --verspec $verspec --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_IMLIN --basedir /global/cfs/cdirs/desi/survey/catalogs/ --version $1 --extra_clus_dir 'nonKP/'
