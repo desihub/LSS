daf53f7c6fd1a1d1db9c50c3130131e3c7a4b0a7
ashleyjross <ashley.jacob.ross@gmail.com>
2024-11-25 20:38:26 -0500

    Create Y3_sysnetELGtot_zbins.sh

---
+++ scripts/Y3_sysnetELGtot_zbins.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+export LSSDIR=$HOME
+#export SYSNETDIR=$HOME/desicode
+export LSSBASE=/global/cfs/cdirs/desi/survey/catalogs/
+PYTHONPATH=$PYTHONPATH:$LSSDIR/LSS/py
+
+verspec=loa-v1
+survey=DA2
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG --notqso y --prepsysnet y --imsys_zbin y --fulld n --survey $survey --verspec $verspec --version $1
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELGnotqso0.8_1.1 true false 1024 0.003 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELGnotqso0.8_1.1 true false 1024 0.003 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi $LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELGnotqso0.8_1.1 false true 1024 0.004 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi $LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELGnotqso0.8_1.1 false true 1024 0.004 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELGnotqso1.1_1.6 true false 1024 0.003 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+$LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELGnotqso1.1_1.6 true false 1024 0.003 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi $LSSDIR/LSS/scripts/run_sysnet_cd.sh N ELGnotqso1.1_1.6 false true 1024 0.004 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi $LSSDIR/LSS/scripts/run_sysnet_cd.sh S ELGnotqso1.1_1.6 false true 1024 0.004 dnnp pnll $1 $LSSBASE/$survey/LSS/$verspec/LSScats/
+
+
+python scripts/main/mkCat_main.py --basedir /global/cfs/cdirs/desi/survey/catalogs/ --type ELG --notqso y --add_sysnet y --imsys_zbin y --fulld n --survey $survey --verspec $verspec --version $1
+
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/validation/validation_improp_full.py --tracers ELGnotqso --version $1 --verspec $verspec --survey $survey --weight_col WEIGHT_SN
