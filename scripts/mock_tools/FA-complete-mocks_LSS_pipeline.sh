2ddb1a5f38b575785c1f14939aff3eed14d98928
Elena Fernández-García <87361846+elenaferga@users.noreply.github.com>
2024-12-02 20:12:36 +0100

    Update FA-complete-mocks_LSS_pipeline.sh

--- scripts/mock_tools/FA-complete-mocks_LSS_pipeline.sh
+++ scripts/mock_tools/FA-complete-mocks_LSS_pipeline.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
-source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+echo "Please, choose the tracer: LRG, QSO or ELG"
+read -p "Tracer: " TRACER
+
+if [[ "$TRACER" != "LRG" && "$TRACER" != "QSO" && "$TRACER" != "ELG" ]]; then
+    echo "Invalid tracer. Please, chose among LRG, QSO or ELG."
+    exit 1
+fi
+
 source /global/common/software/desi/desi_environment.sh main
+VARIABLE="/global/cfs/cdirs/desi/mocks/cai/Uchuu-SHAM/Y3-v1.0/0000/"
+VARIABLE2="Uchuu-SHAM_${TRACER}_Y3-v1_0000_clustering.dat"
 
-# Change this with your mock name and path!!!
-VARIABLE="path/to/mock" #/pscratch/sd/e/efdez/Uchuu/Y1/
-VARIABLE2="mock_name" #uchuu-desi-y1_v2_0p65
-VARIABLE3="output_file_name" 
-
-
-
-############DO NOT CHANGE ANYTHING HERE############
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python prepare_mocks_Y3_LRG.py --survey DA2 --tracer LRG --input_mockpath "$VARIABLE" --input_mockfile "$VARIABLE2" --output_fullpathfn output1.fits --nproc 128
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python prepare_mocks_Y3.py \
+    --survey DA2 --tracer "$TRACER" --input_mockpath "$VARIABLE" --input_mockfile "$VARIABLE2" \
+    --output_fullpathfn output1.fits --nproc 128
 
 export PATH=/global/cfs/cdirs/desi/users/raichoor/fiberassign-rerun-main/fiberassign_main_godesi23.10/bin:$PATH
 export PYTHONPATH=/global/cfs/cdirs/desi/users/raichoor/fiberassign-rerun-main/fiberassign_main_godesi23.10/py:$PYTHONPATH
 export SKYHEALPIXS_DIR=$DESI_ROOT/target/skyhealpixs/v1
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python getpota_input.py --input output1.fits --output output2.fits --realization 1
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python getpota_input_Y3.py \
+    --input output1.fits --output output2.fits --realization 1 -prog 'DARK'
 
-source /global/common/software/desi/desi_environment.sh main
+if [[ "$TRACER" == "LRG" ]]; then
+    source /global/common/software/desi/desi_environment.sh main
+    srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python readwrite_pixel_bitmask_ttype_Y3.py \
+        -t LRG -i /pscratch/sd/e/efdez/Uchuu/LSS/scripts/mock_tools/output1
+    srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python pota2clus_fast_Y3.py \
+        --pota_fn output2.fits --tracer LRG --in_tarf output1_LRGimask.fits --mockver 'Uchuu'
 
+elif [[ "$TRACER" == "QSO" ]]; then
+    source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+    srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python pota2clus_fast_Y3.py \
+        --pota_fn output2.fits --tracer QSO --mockver 'Uchuu' -mk_inputran y
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python readwrite_pixel_bitmask_ttype.py -t LRG -i /pscratch/sd/e/efdez/Uchuu/LSS/scripts/mock_tools/output1
+elif [[ "$TRACER" == "ELG" ]]; then
+    srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python pota2clus_fast_Y3.py \
+        --pota_fn output2.fits --tracer ELG --mockver 'Uchuu'
+fi
 
-srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python pota2clus_fast.py --pota_fn output2.fits --tracer LRG --in_tarf output1_LRGimask.fits --mockver 'Uchuu'
-
-rm -f output1.fits output2.fits output2_LRGimask.fits ##you can remove this line if you want to keep any of the output files
-####################################################
+#optional
+rm -f output1.fits output2.fits output2_LRGimask.fits
