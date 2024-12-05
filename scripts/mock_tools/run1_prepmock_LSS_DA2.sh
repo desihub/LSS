ff3bc5b54a8555c89e580c9deb522d5a43747023
Elena Fernández-García <87361846+elenaferga@users.noreply.github.com>
2024-12-05 12:12:40 +0100

    Update run1_prepmock_LSS_DA2.sh

--- scripts/mock_tools/run1_prepmock_LSS_DA2.sh
+++ scripts/mock_tools/run1_prepmock_LSS_DA2.sh
@@ -23,7 +23,7 @@ if [ "$PROG" == "DARK" ]; then
 	srun -n 1 -C cpu -t 00:40:00 --cpus-per-task $cpus_needed --qos interactive --account desi python $LSSCODE/scripts/mock_tools/prepare_mocks_Y3_dark.py --mockver ab_secondgen --mockpath /global/cfs/cdirs/desi/cosmosim/SecondGenMocks/AbacusSummit/CutSky_v4_1 --realmin $first_rea --realmax $last_rea --isProduction y --split_snapshot y --new_version AbacusSummit_v4_1 --prog dark
 
 elif [ "$PROG" == "BRIGHT" ]; then
-	srun -n 1 -C cpu -t 00:40:00 --cpus-per-task $cpus_needed --qos interactive --account desi python $LSSCODE/scripts/mock_tools/prepare_mocks_Y3_bright.py  --mockver ab_secondgen_cosmosim --realmin $1 --realmax $num --prog bright --isProduction y --rbandcut 19.5
+	srun -n 1 -C cpu -t 00:40:00 --cpus-per-task $cpus_needed --qos interactive --account desi python $LSSCODE/scripts/mock_tools/prepare_mocks_Y3_bright.py  --mockver ab_secondgen_cosmosim --realmin $first_rea --realmax $last_rea --prog bright --isProduction y --rbandcut 19.5
     
 else
     echo "Need to define DARK or BRIGHT"
