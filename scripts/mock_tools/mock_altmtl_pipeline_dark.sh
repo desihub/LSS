285eff8fe55ac17a96f23676a3a10f6cb69d65a1
Elena Fernández-García <87361846+elenaferga@users.noreply.github.com>
2025-01-14 17:20:11 +0100

    Update mock_altmtl_pipeline_dark.sh

--- scripts/mock_tools/mock_altmtl_pipeline_dark.sh
+++ scripts/mock_tools/mock_altmtl_pipeline_dark.sh
@@ -28,14 +28,16 @@ srun -N 1 -C cpu -t 02:00:00 --qos interactive --account desi python $LSSCODE/LS
 #get LRG clustering catalogs (--mkclusdat y --mkclusran y), split them NGC/SGC (--splitGC y), refactor/add FKP weights (--nz y)
 srun -N 1 -C cpu -t 02:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1/altmtl$mocknum --mockver ab_secondgen --mocknum $mocknum --survey DA2  --specdata $specrel --tracer LRG --targDir /dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1 --mkclusdat y --mkclusran y --splitGC y --nz y --par y --outmd notscratch
 
-#switch to cosmodesi environment for QSO
+
+#get QSO fulld (--fulld y), and masked data (--apply_veto y) and randoms (--apply_veto_ran y), and add tileloc info to randoms (--add_tlcomp y)
+srun -N 1 -C cpu -t 02:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1/altmtl$mocknum --mockver ab_secondgen --mocknum $mocknum --survey DA2 --specdata $specrel --tracer QSO --targDir /dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1 --fulld y --apply_veto y --apply_veto_ran y --add_tlcomp y --par y --outmd notscratch
+
+
+#switch to cosmodesi environment for the rest of the codes
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 export LSSCODE=$HOME #change to wherever LSS code got cloned
 PYTHONPATH=$PYTHONPATH:$LSSCODE/LSS/py
 
-
-#get QSO fulld (--fulld y), and masked data (--apply_veto y) and randoms (--apply_veto_ran y), and add tileloc info to randoms (--add_tlcomp y)
-srun -N 1 -C cpu -t 02:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1/altmtl$mocknum --mockver ab_secondgen --mocknum $mocknum --survey DA2 --specdata $specrel --tracer QSO --targDir /dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1 --fulld y --apply_veto y --apply_veto_ran y --add_tlcomp y --par y --outmd notscratch
 
 #get QSO clustering catalogs (--mkclusdat y --mkclusran y), split them NGC/SGC (--splitGC y), refactor/add FKP weights (--nz y)
 srun -N 1 -C cpu -t 02:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/mock_tools/mkCat_SecondGen_amtl.py --base_output /global/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1/altmtl$mocknum --mockver ab_secondgen --mocknum $mocknum --survey DA2  --specdata $specrel --tracer QSO --targDir /dvs_ro/cfs/cdirs/desi/survey/catalogs/DA2/mocks/SecondGenMocks/AbacusSummit_v4_1 --mkclusdat y --mkclusran y --splitGC y --nz y --par y --outmd notscratch
