53dc7c3d0c8002830623b7815f59668c0a599c23
ashleyjross <ashley.jacob.ross@gmail.com>
2025-04-04 12:05:26 -0400

    Update mkCat_subsamp.py

--- scripts/mkCat_subsamp.py
+++ scripts/mkCat_subsamp.py
@@ -2,7 +2,7 @@
 #Example submission creates Mr < 20.5 catalog based on fastspecfit plus add hoc e correction with a further selection of 35% lowest star formation (percentile at which data looked bimodel)
 #
 #srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python scripts/mkCat_subsamp.py --input_tracer BGS_ANY --mkfulldat y --clusd y --clusran y --nz y --splitGC y --ccut FSFABSmagwecorr-R-20.5-SFRlper-35 --imsys_clus y --imsys_clus_ran y
-#Up to imaging systematics regression takes ~7 minutes 
+#Up to imaging systematics regression takes ~7 minutes ; imaging systematics takes another ~3 minutes
 #
 #standard python
 import sys
