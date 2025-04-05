3aae964419baed00f1ba6bf285f5ebfee6b099dd
ashleyjross <ashley.jacob.ross@gmail.com>
2025-04-05 01:14:01 -0400

    Update mkCat_SecondGen_amtl.py

--- scripts/mock_tools/mkCat_SecondGen_amtl.py
+++ scripts/mock_tools/mkCat_SecondGen_amtl.py
@@ -864,6 +864,7 @@ if args.mkclusran == 'y':
         common.printlog(str(rann)+' length after PRIORITY mask '+str(len(datain)),logger=logger)
         
         datain = ct.add_tlobs_ran_array(datain,tlf,logger)
+        
 
         ct.mkclusran(datain, os.path.join(dirout, finaltracer) + '_', rann, add_tlobs='y',rcols=rcols, ebits=mainp.ebits, clus_arrays=clus_arrays, use_map_veto=args.use_map_veto, compmd=nzcompmd, logger=logger)
         #TEMPct.mkclusran(os.path.join(readdir, finaltracer) + '_', os.path.join(dirout, finaltracer) + '_', rann, rcols=rcols, tsnrcut= -1, tsnrcol=tsnrcol, ebits=mainp.ebits, clus_arrays=clus_arrays, use_map_veto=args.use_map_veto, compmd=nzcompmd,logger=logger)
