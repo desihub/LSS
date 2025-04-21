d41dd76d1bc4c3ea1beee997a7af65f4dc15723b
ashleyjross <ashley.jacob.ross@gmail.com>
2025-04-21 14:28:24 -0400

    Update validation_crosscorr.py

--- scripts/validation/validation_crosscorr.py
+++ scripts/validation/validation_crosscorr.py
@@ -22,9 +22,9 @@ parser.add_argument("--data", help="Data type (LSS or mock)", default='LSS')
 parser.add_argument("--verspec", help="Spectroscopic version", default='iron')
 parser.add_argument("--tracers", help="Tracer type or 'all'", default='ELG_LOPnotqso')  # Currently works for only 1 tracer. 'all' will only return results for LRG. Needs to be updated later.
 parser.add_argument("--nran",help="number of random files to use",default=1,type=int)
-parser.add_argument("--sys_wts", help="Whether to use imaging systematic weights", default='False')
+parser.add_argument("--sys_wts", help="Whether to use imaging systematic weights", default=False,dtype=bool)
 parser.add_argument("--mapmd", help="set of maps to use",default='default')
-parser.add_argument("--norm", help="whether to normalize the maps before cross correlation",default='False')
+parser.add_argument("--norm", help="whether to normalize the maps before cross correlation",default=False,dtype=bool)
 
 args = parser.parse_args()
 
