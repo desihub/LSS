7ab75b3f82f8b3313d86dd8abfa4342b9a44c72e
ashleyjross <ashley.jacob.ross@gmail.com>
2025-04-29 10:54:24 -0400

    Update cosmodesi_io_tools.py

--- py/LSS/cosmodesi_io_tools.py
+++ py/LSS/cosmodesi_io_tools.py
@@ -231,7 +231,7 @@ def get_clustering_positions_weights(catalog, distance, zlim=(0., np.inf),fac_nt
     #if 'SN' in weight_type:
     #    weights *= catalog['WEIGHT_SN'][mask]
     #    print('multiplying weights by WEIGHT_SN')
-    if 'swapinLIN' in weight_type or 'IMLIN' in weight_type:
+    if 'swapinLIN' in weight_type or 'IMLIN_FID' in weight_type:
         #assumes default already added the rest of the weights and that SN was used as default weight
         weights *=  catalog['WEIGHT_IMLIN'][mask]/catalog['WEIGHT_SYS'][mask]
 
