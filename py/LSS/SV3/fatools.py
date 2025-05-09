ebdf8b4545e674ed0b8e112e91f368615a7fcb5c
LNapolitano <lucasnap1@gmail.com>
2025-05-08 20:10:02 -0700

    Fixing environment specification for FAVER>5.00 tiles

--- py/LSS/SV3/fatools.py
+++ py/LSS/SV3/fatools.py
@@ -433,12 +433,14 @@ def get_fba_fromnewmtl(tileid,mtldir=None,getosubp=False,outdir=None,faver=None,
                 fo.write("module swap fiberassign/"+fht['FA_VER'][:3]+'.0'+"\n")
         elif faver > 2.4 and faver < 4.0:
             fo.write("module swap fiberassign/"+fht['FA_VER']+"\n")
-        else:
-            assert faver >= 4.0
+        elif faver > 4.0 and faver < 5.0:
             #fo.write("module swap fiberassign/5.0.0\n")
             fo.write("export PATH=/global/cfs/cdirs/desi/users/raichoor/fiberassign-rerun-main/fiberassign_main_godesi23.10/bin:$PATH\n")
             fo.write("export PYTHONPATH=/global/cfs/cdirs/desi/users/raichoor/fiberassign-rerun-main/fiberassign_main_godesi23.10/py:$PYTHONPATH\n")
             fo.write("export SKYHEALPIXS_DIR=$DESI_ROOT/target/skyhealpixs/v1\n")
+        else:
+            #with fiberassign refactor as of May 2025, no longer necessary to specify a module swap for faver > 5.00
+            assert faver >= 5.0
     else:
         fo.write("module swap fiberassign/"+str(faver)+"\n")
         faver = float(faver[:3])
