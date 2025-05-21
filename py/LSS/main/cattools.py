e125a90fc7dcca9e3aa11a77bd2af26dd37ce3e5
ashleyjross <ashley.jacob.ross@gmail.com>
2025-05-20 22:11:20 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -3180,7 +3180,7 @@ def mkfulldat(zf,imbits,ftar,tp,bit,outf,ftiles,maxp=3400,azf='',azfm='cumul',de
         else:
             print(specf)
         fs = fitsio.read(specf)
-        common.printlog('badfib type is '+str(type(badfib)))
+        common.printlog('badfib type is '+str(type(badfib)),logger)
         fs = common.cut_specdat(fs,badfib,tsnr_min=min_tsnr2,tsnr_col=tscol,fibstatusbits=badfib_status,remove_badfiber_spike_nz=True,mask_petal_nights=True,logger=logger)
         fs = Table(fs)
         fs['TILELOCID'] = 10000*fs['TILEID'] +fs['LOCATION']
