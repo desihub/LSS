9815865dd7b77589a8e8e5ed493441c2faecebad
zdplayground <zd585612@ohio.edu>
2025-05-22 18:34:39 -0700

    remove permutation for targetid

--- scripts/mock_tools/prepare_mocks_Y3.py
+++ scripts/mock_tools/prepare_mocks_Y3.py
@@ -131,7 +131,8 @@ n=len(targets)  ##A Ashley le falta estoo!
 
 del data
 
-targets['TARGETID'] = (np.random.permutation(np.arange(1,n+1))+1e8*desitar[type_]/norm[type_]).astype(int) #different tracer types need to have different targetids
+#targets['TARGETID'] = (np.random.permutation(np.arange(1,n+1))+1e8*desitar[type_]/norm[type_]).astype(int) #different tracer types need to have different targetids
+targets['TARGETID'] = (np.arange(1,n+1) + 1e8*desitar[type_]/norm[type_]).astype(int)
 print(len(targets),' in Y5 area')
 selY3 = is_point_in_desi(tiletab,targets['RA'],targets['DEC'])
 targets = targets[selY3]
