822a678d2d60975e4a44d53792f80a46cb0bb00a
ashleyjross <ashley.jacob.ross@gmail.com>
2025-07-08 11:37:43 -0400

    Update dark_LSScat_pipe.sh

--- scripts/dark_LSScat_pipe.sh
+++ scripts/dark_LSScat_pipe.sh
@@ -99,7 +99,7 @@ python $LSSCODE/LSS/scripts/main/patch_HPmapcut.py --tracers LRG --survey $surve
 #~5 minutes
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --fulld n --survey $survey --verspec $verspec --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_IMLIN --basedir /global/cfs/cdirs/desi/survey/catalogs/ --version $1 --extra_clus_dir 'nonKP/'
 
-#$LSSCODE/LSS/scripts/Y3_sysnetELG_zbins.sh $1
+#srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi $LSSCODE/LSS/scripts/Y3_sysnetELG_zbins_new.sh $1
 
 #srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type ELG_LOP --notqso y  --fulld n --survey $survey --verspec $verspec --clusd y --clusran y --splitGC y --nz y --par y --imsys_colname WEIGHT_SN --basedir /global/cfs/cdirs/desi/survey/catalogs/ --version $1 --extra_clus_dir 'nonKP/'
 
