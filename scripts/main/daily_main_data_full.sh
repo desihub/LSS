1891a9864b573f60a2462c13c5b93b46a10310f4
Xinyi Chen <xinyi.chen@yale.edu>
2025-07-28 09:44:59 -0700

    changed QSO run to use interactive node

--- scripts/main/daily_main_data_full.sh
+++ scripts/main/daily_main_data_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #script to run main/daily data through up to veto, do all tracer types
 
-python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec daily 
+srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type QSO --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec daily 
 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec daily 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/LSS/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld y --verspec daily 
