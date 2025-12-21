ebedb33895abc08d3bf4ed8bca46d365a3cc3f97
ashleyjross <ashley.jacob.ross@gmail.com>
2025-12-21 12:45:53 -0500

    Update copy_amtl2cfs.sh

--- scripts/mock_tools/copy_amtl2cfs.sh
+++ scripts/mock_tools/copy_amtl2cfs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+source /global/common/software/desi/desi_environment.sh main
+module load LSS/DR2-mocks-v0
+
+
 for ((i=$1;i<=$2;i++ ))
 do
  python scripts/mock_tools/mkCat_amtl.py --mocknum $i --tracer LRG --simName $3 --transfer_cfs
