d171e8cbb5b8d4c38bd5f98e5bdc7d6fd81d40a0
zjdingastro <zd585612@ohio.edu>
2025-12-22 02:42:28 -0800

    adding script to discover new holi mocks

---
+++ scripts/mock_tools/discover_holi.py
@@ -0,0 +1,24 @@
+import os
+
+all_realizations = list(range(1000))
+
+
+parent_path = '/global/cfs/cdirs/desi/mocks/cai/LSS/DA2/mocks/holi_v1'
+holi_path = '/global/cfs/cdirs/desi/mocks/cai/holi/'
+
+
+vQSO = 'v4.00'
+vELG = 'v5.0_Y5'
+vLRG = 'v4.00'
+
+todo = []
+
+for i in all_realizations:
+    seednum = str(i).zfill(4)
+    if not os.path.isdir(os.path.join(parent_path, 'altmtl%d' % i)):
+        if os.path.isdir(os.path.join(holi_path, vQSO, 'seed%s' % seednum)) and os.path.isdir(os.path.join(holi_path, vELG, 'seed%s' % seednum)):
+            todo.append(i)
+
+print(todo)
+print(len(todo))
+
