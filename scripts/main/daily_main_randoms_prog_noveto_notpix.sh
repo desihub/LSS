e8e134e1a89a655bba731d80c08343189c871803
ashleyjross <ashley.jacob.ross@gmail.com>
2026-01-06 20:29:05 -0500

    Update daily_main_randoms_prog_noveto_notpix.sh

--- scripts/main/daily_main_randoms_prog_noveto_notpix.sh
+++ scripts/main/daily_main_randoms_prog_noveto_notpix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/scripts/main/mkCat_main_ran.py  --type dark1b  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily  --fullr y
 srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/scripts/main/mkCat_main_ran.py  --type dark  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily  --fullr y
 srun -N 1 -C cpu -t 04:00:00 -q interactive python $LSSCODE/scripts/main/mkCat_main_ran.py  --type bright  --basedir /global/cfs/cdirs/desi/survey/catalogs/ --verspec daily  --fullr y
 
