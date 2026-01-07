c865095422428da28bd37f4e4826f526c597dbbc
ashleyjross <ashley.jacob.ross@gmail.com>
2026-01-07 11:31:52 -0500

    Update clean_dr2_amtltarg_holi_scratch.sh

--- scripts/mock_tools/clean_dr2_amtltarg_holi_scratch.sh
+++ scripts/mock_tools/clean_dr2_amtltarg_holi_scratch.sh
@@ -3,7 +3,7 @@
 
 for ((i=$1;i<=$2;i++ ))
 do
- echo $SCRATCH/DA2/mocks/holi_v1/altmtl$1/Univ000/fa/MAIN/
- rm $SCRATCH/DA2/mocks/holi_v1/altmtl$1/Univ000/fa/MAIN/*/*targ.fits
+ echo $SCRATCH/DA2/mocks/holi_v1/altmtl$i/Univ000/fa/MAIN/
+ rm $SCRATCH/DA2/mocks/holi_v1/altmtl$i/Univ000/fa/MAIN/*/*targ.fits
 done
 
