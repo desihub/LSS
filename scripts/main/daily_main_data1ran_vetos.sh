8ab6013e82fb4fbb5366cb0b6139298c7a1c86c1
ashleyjross <ashley.jacob.ross@gmail.com>
2026-01-06 23:10:11 -0500

    Update daily_main_data1ran_vetos.sh

--- scripts/main/daily_main_data1ran_vetos.sh
+++ scripts/main/daily_main_data1ran_vetos.sh
@@ -6,6 +6,6 @@ srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/sc
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/scripts/main/mkCat_main.py --type ELG_LOP --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 --notqso y
 python $LSSCODE/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
-python $LSSCODE/scripts/main/mkCat_main.py --type LRG --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
+python $LSSCODE/scripts/main/mkCat_main.py --type LGE --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
 python $LSSCODE/scripts/main/mkCat_main.py --type BGS_BRIGHT --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
 srun -N 1 -C cpu -t 04:00:00 --qos interactive --account desi python $LSSCODE/scripts/main/mkCat_main.py --type BGS_ANY --basedir /global/cfs/cdirs/desi/survey/catalogs/  --fulld n --apply_veto y --verspec daily --maxr 1 
