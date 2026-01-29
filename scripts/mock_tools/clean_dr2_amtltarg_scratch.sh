3808203b3774dc4262bb41ee6ac675961ff35a45
ashleyjross <ashley.jacob.ross@gmail.com>
2026-01-29 16:29:17 -0500

    Update clean_dr2_amtltarg_scratch.sh

--- scripts/mock_tools/clean_dr2_amtltarg_scratch.sh
+++ scripts/mock_tools/clean_dr2_amtltarg_scratch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ((i=$1;i<=$2;i++ ))
+for ((i=$2;i<=$3;i++ ))
 do
  echo $SCRATCH/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/
  rm $SCRATCH/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*targ.fits
