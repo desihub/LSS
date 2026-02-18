bf9cb7d1eb1eb6fbefa923cab15be750b61b2234
ashleyjross <ashley.jacob.ross@gmail.com>
2026-02-18 09:55:09 -0500

    Update holi_input_pk.sh

--- scripts/mock_tools/holi_input_pk.sh
+++ scripts/mock_tools/holi_input_pk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for ((i=$1;i<=$2;i++ ))
 do
- srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.5 0.85 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/box10_mock$i/ --combine --nran 18 --boxsize 10000
+ srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.5 0.85 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/box10_mock$i/ --combine --nran 18 --boxsize 10000 --meshsize 700
 
- srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.4 1.1 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/box10_mock$i/ --combine --nran 18 --boxsize 10000
+ srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.4 1.1 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/box10_mock$i/ --combine --nran 18 --boxsize 10000 --meshsize 700
 done
