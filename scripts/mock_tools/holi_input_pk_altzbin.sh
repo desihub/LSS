52d0a88c02622e8ef943efcbbd366083d7a96a92
ashleyjross <ashley.jacob.ross@gmail.com>
2026-02-26 12:44:17 -0500

    Update holi_input_pk_altzbin.sh

--- scripts/mock_tools/holi_input_pk_altzbin.sh
+++ scripts/mock_tools/holi_input_pk_altzbin.sh
@@ -3,6 +3,6 @@
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 for ((i=$1;i<=$2;i++ ))
 do
- srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.665 0.74 0.74 0.82 0.9 0.9 0.98 0.98 1.1 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/altzbin_mock$i/ --combine --nran 18 --boxsize 10000 --cellsize 10
+ srun -N 1 -n 4 clustering-stats --tracer LRG_input --zrange 0.665 0.74 0.74 0.82 0.82 0.9 0.9 0.98 0.98 1.1 --stats mesh2_spectrum --cat_dir /pscratch/sd/a/ajross/holi-webjax/mock$i/ --stats_dir /pscratch/sd/a/ajross/holi-webjax/altzbin_mock$i/ --combine --nran 18 --boxsize 10000 --cellsize 10
 
 done
