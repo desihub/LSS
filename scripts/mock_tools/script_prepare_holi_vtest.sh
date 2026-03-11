d7ffe6b28b7fdb927b15abaa6fda0f3ff96f1be1
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-11 15:53:01 -0400

    Update script_prepare_holi_vtest.sh

--- scripts/mock_tools/script_prepare_holi_vtest.sh
+++ scripts/mock_tools/script_prepare_holi_vtest.sh
@@ -30,6 +30,7 @@ if [ -f "/global/cfs/cdirs/desi/mocks/cai/holi/$version/$seed/$tracer/forFA0_Y3_
     echo "/global/cfs/cdirs/desi/mocks/cai/holi/$version/$seed/$tracer/forFA0_Y3_noimagingmask_applied.fits exists"
 else
     python prepare_mocks_Y3_test1.py --survey DA2 --specdata loa-v1 --mockname holi --input_mockpath /global/cfs/cdirs/desi/mocks/cai/holi/$version/$seed/ --input_mockfile holi_ELG_v4.81_GCcomb_clustering.dat.h5 --tracer ELG --zrsdcol Z --output_fullpathfn /global/cfs/cdirs/desi/mocks/cai/holi/$version/$seed/"$tracer"/forFA0_Y3_noimagingmask_applied.fits --save_mock_nz y --nzfilename $nzname --need_nz_calib y
+fi
 
 for ((i=$1;i<=$2;i++ )); do
     seed=$(printf "seed%04d" "$i")
