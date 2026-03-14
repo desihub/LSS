1136662b657d7d35256b437a048b000a8b585f03
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-14 16:01:11 -0400

    Update script_init_holi_v3_inter_sequence.sh

--- scripts/mock_tools/script_init_holi_v3_inter_sequence.sh
+++ scripts/mock_tools/script_init_holi_v3_inter_sequence.sh
@@ -6,6 +6,6 @@ module load LSS/main
 for ((i=$1;i<=$2;i++ ))
 do
   echo $i
-  srun -N 1 -C cpu --cpus-per-task=64 -t 00:40:00 --ntasks=1 --qos interactive --account desi python $LSS/scripts/initialize_amtl_mocks_da2.py /pscratch/sd/d/desica/DA2/mocks/holi_v3/forFA$i.fits /pscratch/sd/d/desica/DA2/mocks/holi_v3/altmtl$i/ DARK
+  srun -N 1 -C cpu --cpus-per-task=64 -t 00:40:00 --ntasks=1 --qos interactive --account desi python $LSS/scripts/mock_tools/initialize_amtl_mocks_da2.py /pscratch/sd/d/desica/DA2/mocks/holi_v3/forFA$i.fits /pscratch/sd/d/desica/DA2/mocks/holi_v3/altmtl$i/ DARK
 done
 	 
