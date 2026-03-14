af72142c3d3bfa4824a7a974cf09e59d581e8b85
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-14 16:44:36 -0400

    Update run_456_holi_v3_inparallel_nooverw.sh

--- scripts/mock_tools/run_456_holi_v3_inparallel_nooverw.sh
+++ scripts/mock_tools/run_456_holi_v3_inparallel_nooverw.sh
@@ -8,6 +8,7 @@ job_count=0
 for ((i=$1;i<=$2;i++ ))
 do
  if [ ! -f "/pscratch/sd/d/desica/DA2/mocks/holi_v3/forFA$i.fits" ]; then
+     echo /pscratch/sd/d/desica/DA2/mocks/holi_v3/forFA$i.fits
      bash scripts/mock_tools/run1_456.sh $i webjax_v4.81 webjax_v4.80 webjax_v4.80 /pscratch/sd/d/desica/DA2/mocks/holi_v3 holi &
 
      ((job_count++))
