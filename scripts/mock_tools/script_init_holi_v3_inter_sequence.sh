6dc208173129a1fb7d77f12aaab9b03c907c2ebd
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-14 15:56:21 -0400

    Create script_init_holi_v3_inter_sequence.sh

---
+++ scripts/mock_tools/script_init_holi_v3_inter_sequence.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /global/common/software/desi/desi_environment.sh main
+module load desitarget/3.0.0
+
+for ((i=$1;i<=$2;i++ ))
+do
+  echo $i
+  srun -N 1 -C cpu --cpus-per-task=64 -t 00:40:00 --ntasks=1 --qos interactive --account desi python initialize_amtl_mocks_da2.py /pscratch/sd/d/desica/DA2/mocks/holi_v3/forFA$i.fits /pscratch/sd/d/desica/DA2/mocks/holi_v3/altmtl$i/ DARK
+done
+	 
