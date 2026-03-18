1fbdb652bd343a9b3219c98510efdf06ec184211
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-18 11:47:46 -0400

    Create submit_holi_v3_maskbit.sh for batch submissions
    Added a bash script to submit batch jobs for maskbits in increments of 25.

--- scripts/mock_tools/submit_holi_v3_maskbit.sh
+++ scripts/mock_tools/submit_holi_v3_maskbit.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#submit batch scripts for maskbits in 25 mock increments
+
+for i in {450..975..25}; do
+    let maxi=$i+25
+    python $HOME/LSScode/LSS/scripts/mock_tools/prep_bitmask.py --qsomin $i --qsomax $maxi --lrgmin $i --lrgmax $maxi --elgmin $i --elgmax $maxi --subname qsolrgelg$maxi
+    sbatch $HOME/BRICKMASKcode/brickmask/qsolrgelg$maxi.sbatch
+done
 
