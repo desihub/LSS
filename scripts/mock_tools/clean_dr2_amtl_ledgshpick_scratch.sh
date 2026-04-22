ab55d3d9cfd8c80e015a65a6b19c21e71d2f6d88
ashleyjross <ashley.jacob.ross@gmail.com>
2026-04-22 11:16:35 -0400

    Create clean_dr2_amtl_ledgshpick_scratch.sh

---
+++ scripts/mock_tools/clean_dr2_amtl_ledgshpick_scratch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for ((i=$2;i<=$3;i++ ))
+do
+ echo $SCRATCH/DA2/mocks/$1/altmtl$i/
+ rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/main
+ rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.sh
+ rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.pickle
+done
+
