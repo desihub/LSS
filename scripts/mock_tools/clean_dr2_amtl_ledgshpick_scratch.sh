f915111a35dfb5fa83c188b0b89e83c28116f22c
ashleyjross <ashley.jacob.ross@gmail.com>
2026-04-22 11:19:46 -0400

    Update clean_dr2_amtl_ledgshpick_scratch.sh

--- scripts/mock_tools/clean_dr2_amtl_ledgshpick_scratch.sh
+++ scripts/mock_tools/clean_dr2_amtl_ledgshpick_scratch.sh
@@ -3,7 +3,7 @@
 for ((i=$2;i<=$3;i++ ))
 do
  echo $SCRATCH/DA2/mocks/$1/altmtl$i/
- rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/main
+ rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/Univ000/main
  rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.sh
  rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.pickle
 done
