678d4bf3cace266b1d628706d2cce65e9cbdc655
ashleyjross <ashley.jacob.ross@gmail.com>
2026-05-11 23:05:38 -0400

    Update combdata_rel.py

--- scripts/main/combdata_rel.py
+++ scripts/main/combdata_rel.py
@@ -525,6 +525,7 @@ if specrel != 'daily' and args.dospec:
             kc += ['OII_FLUX', 'OII_FLUX_IVAR']
             del specfe
         specf.keep_columns(kc)
+        specfo = ldirspec+'datcomb_'+prog+'_zmtl_zdone.fits'
         fzmtl = fitsio.read(specfo)
         specf = join(specf, fzmtl, keys=['TARGETID', 'TILEID'])
         specf.write(outfs, format='fits', overwrite=True)    
