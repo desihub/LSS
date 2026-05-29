7ab9358d7cac03f6d1dd535ecb1627557d0b1aa2
ashleyjross <ashley.jacob.ross@gmail.com>
2026-05-29 09:58:07 -0400

    Update ab2ndgen_bgsbright02_interactive.sh

--- scripts/mock_tools/ab2ndgen_bgsbright02_interactive.sh
+++ scripts/mock_tools/ab2ndgen_bgsbright02_interactive.sh
@@ -8,6 +8,6 @@ scriptdir=/global/homes/d/desica/LSScode/LSS/scripts
 for ((i=$1;i<=$2;i++ ))
 do
   echo $i
-  python $scriptdir/mock_tools/mkCat_SecondGen_amtl.py --mocknum $i -tracer BGS_BRIGHT-02 --absmagmd redshiftdep --simName SecondGenMocks/AbacusSummitBGS_v2 --outmd cfs --specdata kibo-v1 --mkclusran y --mkclusdat y --nz y --splitGC y --par y
+  python $scriptdir/mock_tools/mkCat_SecondGen_amtl.py --mocknum $i --tracer BGS_BRIGHT-02 --absmagmd redshiftdep --simName SecondGenMocks/AbacusSummitBGS_v2 --outmd cfs --specdata kibo-v1 --mkclusran y --mkclusdat y --nz y --splitGC y --par y
 done
 
