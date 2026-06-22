73a8c25dc0d7194351a37f0c2e0a162df6c30bcd
Aurelio Carnero Rosell <aurelio.crosell@gmail.com>
2026-06-22 03:37:22 -0700

    adding init for da3

---
+++ scripts/mock_tools/initialize_amtl_mocks_da3.py
@@ -0,0 +1,138 @@
+import desitarget
+from desitarget import mtl
+import sys
+import glob
+import os
+import errno
+from LSS.SV3 import altmtltools as amtl
+from astropy.table import Table, vstack
+import yaml
+
+def create_dirs(value):
+    if not os.path.exists(value):
+        try:
+            os.makedirs(value, 0o755)
+            print('made ' + value)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+
+
+#python /pscratch/sd/a/acarnero/codes/LSS/scripts/mock_tools/run_mtl_ledger.py $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/forFA$j.fits $DESI_ROOT/survey/catalogs/Y1/mocks/SecondGenMocks/$SeconGenVer/altmtl$j DARK
+
+par=True
+if desitarget.__version__ > "3.0.0":
+    numproc=12
+    print('desitarget is greater than 3.0.0 using nproc=12')
+else:
+    numproc=32
+    print('desitarget is 3.0.0 using nproc=32')
+
+
+recreate_tileTrack = False
+aux_datapath = {'DARK': '/global/cfs/cdirs/desi/users/acarnero/altmtlTracker/DA3/mainsurvey-DARKobscon-TileTracker.ecsv', 'BRIGHT': '/global/cfs/cdirs/desi/users/acarnero/altmtlTracker/DA3/mainsurvey-BRIGHTobscon-TileTracker.ecsv'}
+
+arg1 = sys.argv[1].replace('global','dvs_ro') #Input mock
+arg2 = sys.argv[2] #Output path
+obscon = sys.argv[3] #DARK or BRIGHT
+
+
+initledger_path = os.path.join(arg2, 'initled')
+
+altmtl_path = os.path.join(arg2, 'Univ000')
+
+if not os.path.isdir(initledger_path):
+#    pass
+
+    print('Running initial ledgers')
+    if par:
+        mtl.make_ledger(arg1, initledger_path, obscon=obscon.upper(), numproc=numproc)
+        #mtl.make_ledger(arg1, initledger_path, obscon=obscon.upper(), numproc=32)
+    else:
+        mtl.make_ledger(arg1, initledger_path, obscon=obscon.upper())
+
+print('Creating list of tiles to be processed by AltMTL mock production')
+
+path = os.path.join(initledger_path, 'main', obscon.lower())
+
+ff = glob.glob(os.path.join(path, 'mtl-{obscon}-hp-*.ecsv'.format(obscon=obscon.lower())))
+
+
+dd=[]
+
+for f in ff:
+    dd.append(int(f.split('hp-')[-1].split('.ecsv')[0]))
+tosave = ','.join(map(str, sorted(dd)))
+
+savepath = os.path.join(initledger_path, 'hpxlist_{obscon}.txt'.format(obscon = obscon.lower()))
+
+ff = open(savepath, 'w')
+ff.write(tosave)
+ff.close()
+
+print('saving list of HP ledgers in '+savepath)
+
+
+path_to_altmtl = os.path.join(altmtl_path, 'main', obscon.lower())
+
+print('Copying initial ledgers to altmtl directory ', path_to_altmtl)
+
+create_dirs(path_to_altmtl)
+
+os.system('cp %s %s' % (os.path.join(path,'*'), path_to_altmtl))
+
+print('Creating tileTracker file and tilestatus file')
+
+
+#yaml_path = amtl.initializeYAML_HPTracker(obscon, altmtl_path)
+
+#with open(yaml_path) as f:
+#    HPYaml = yaml.safe_load(f)
+
+#HPList = np.array(HPYaml['Initial']).astype(int)
+
+startDateShort = 19990101
+endDate='20260605' #2024-04-18T00:00:00+00:00'
+##endDate='20260415' #2024-04-18T00:00:00+00:00'
+if ('T' in endDate) & ('-' in endDate):
+    endDateShort = int(endDate.split('T')[0].replace('-', ''))
+else:
+    endDateShort = int(endDate)
+
+if recreate_tileTrack:
+
+
+
+    amtl.makeTileTracker(altmtl_path, survey = 'main', obscon = obscon.upper(), startDate = startDateShort, endDate = endDateShort, overwrite = True)
+else:
+    
+    os.system('cp %s %s' %(aux_datapath[obscon.upper()], altmtl_path))
+
+ztilefile = '/global/cfs/cdirs/desi/survey/ops/surveyops/trunk/ops/tiles-specstatus.ecsv'
+
+ztilefn = ztilefile.split('/')[-1]
+
+if not os.path.isfile(os.path.join(altmtl_path, ztilefn)):
+    amtl.processTileFile(ztilefile, os.path.join(altmtl_path, ztilefn), None, endDate)
+
+
+
+'''
+if addextra:
+    extratiles = Table.read('/global/cfs/cdirs/desi/survey/catalogs/Y1/mocks/SecondGenMocks/AbacusSummit_v4/aux_data/extra_{obscon}.ecsv'.format(obscon = obscon.lower()), format='ascii.ecsv')
+
+    tileref = extratiles['TILEID'][-1]
+#print(tileref)
+    input_track = os.path.join(altmtl_path, 'mainsurvey-{OBSCON}obscon-TileTracker.ecsv'.format(OBSCON = obscon.upper()))
+    tiles = Table.read(input_track, format='ascii.ecsv')
+    tiles.meta['amtldir'] = altmtl_path
+
+    if tiles['TILEID'][-1] != tileref:
+        print('Adding extra tiles')
+        newtable = vstack([tiles, extratiles])
+        newtable.meta = tiles.meta
+        newtable.write(input_track, overwrite=True)
+    else:
+        print('It has already been merged')
+'''        
