6be8be52dc8fb7634382c92b7e82105abfe72c32
ashleyjross <ashley.jacob.ross@gmail.com>
2026-07-01 10:19:35 -0400

    Update calibrate_nz_prep.py

--- scripts/mock_tools/calibrate_nz_prep.py
+++ scripts/mock_tools/calibrate_nz_prep.py
@@ -3,8 +3,8 @@ import os
 from numpy.random import Generator, PCG64
 rng = Generator(PCG64())
 
-#path_to_nz = '/global/homes/d/desica/desi-cutsky-mock/nz_files/DA2/'
-path_to_nz = '/pscratch/sd/a/acarnero/codes/desi-cutsky-mock/nz_files/DA2/'
+path_to_nz = '/global/homes/d/desica/desi-cutsky-mock/nz_files/DA2/'
+#path_to_nz = '/pscratch/sd/a/acarnero/codes/desi-cutsky-mock/nz_files/DA2/'
 
 def get_nz_bgs(z_cat, tracer_type, nzfile, factor=None):
     ''' The function where the n(z) is read and the NZ column is computed for the given redshifts.   '''
