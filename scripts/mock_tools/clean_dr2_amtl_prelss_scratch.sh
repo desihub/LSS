f0bf193d6c2c3be477af5df6bab7fb385695261b
ashleyjross <ashley.jacob.ross@gmail.com>
2026-07-05 22:16:53 -0400

    Create clean_dr2_amtl_prelss_scratch.sh

---
+++ scripts/mock_tools/clean_dr2_amtl_prelss_scratch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for ((i=$2;i<=$3;i++ ))
+do
+ echo $SCRATCH/DA2/mocks/$1/altmtl$i/initled
+ rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/initled
+ rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*targ.fits
+ rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.sh
+ rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.pickle
+done
+
