ee00b9ea96d81e81440446393a192179309473cb
ashleyjross <ashley.jacob.ross@gmail.com>
2026-07-07 10:48:47 -0400

    break LSS into pieces for debug mulitnode run

---
+++ scripts/mock_tools/holi_darkv4_DA3DA2_lss_imlin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+source /global/common/software/desi/desi_environment.sh main
+module load LSS/main
+export LSSCODE=$LSS
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+mocknum=$1
+scriptdir=/global/homes/d/desica/LSScode/LSS/scripts
+sim=holi_v4/altmtl
+survey=DA3
+surveycat=DA2 #this will make it process the DR2 footprint
+#PYTHONPATH=/global/homes/d/desica/LSScode/LSS/py:$PYTHONPATH
+#test
+
+#echo $scriptdir/mock_tools/mkCat_amtl.py --base_altmtl_dir $SCRATCH --simName $sim --mocknum $mocknum --survey $survey --surveycat $surveycat --add_gtl y --specdata loa-v1 --tracer dark --targDir $SCRATCH/$survey/mocks/$sim --combd y --joindspec y --par y --usepota y
+
+python $scriptdir/mock_tools/mkCat_amtl.py --base_altmtl_dir $SCRATCH --simName $sim --mocknum $mocknum --survey $survey --surveycat $surveycat --specdata loa-v1 --tracer QSO  --doimlin y --replace_syscol --par y --imsys_zbin split
+python $scriptdir/mock_tools/mkCat_amtl.py --base_altmtl_dir $SCRATCH --simName $sim --mocknum $mocknum --survey $survey --surveycat $surveycat --specdata loa-v1 --tracer LRG  --doimlin y --replace_syscol --par y --imsys_zbin fine
+
+
+
+
+
+
+
