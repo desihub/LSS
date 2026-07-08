26e903da74c90585edece5d25181c4ad25015bd8
ashleyjross <ashley.jacob.ross@gmail.com>
2026-07-08 18:46:56 -0400

    Update clean_dr2_amtl_scratch.sh

--- scripts/mock_tools/clean_dr2_amtl_scratch.sh
+++ scripts/mock_tools/clean_dr2_amtl_scratch.sh
@@ -2,12 +2,12 @@
 
 for ((i=$2;i<=$3;i++ ))
 do
- echo $SCRATCH/DA2/mocks/$1/altmtl$i/
- rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/Univ000/main
- rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.sh
- rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.pickle
- rm -r $SCRATCH/DA2/mocks/$1/altmtl$i/initled
- rm /pscratch/sd/d/desica/DA2/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*targ.fits
+ echo $SCRATCH/DA3/mocks/$1/altmtl$i/
+ rm -r $SCRATCH/DA3/mocks/$1/altmtl$i/Univ000/main
+ rm /pscratch/sd/d/desica/DA3/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.sh
+ rm /pscratch/sd/d/desica/DA3/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*.pickle
+ rm -r $SCRATCH/DA3/mocks/$1/altmtl$i/initled
+ rm /pscratch/sd/d/desica/DA3/mocks/$1/altmtl$i/Univ000/fa/MAIN/*/*targ.fits
 
 done
 
