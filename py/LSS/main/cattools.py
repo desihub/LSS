8dee7f2a732efbacfbc41b220e3e903cb653642e
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-02 15:04:06 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -5035,7 +5035,6 @@ def randomtiles_allmain_pix_2step(tiles,dirout='/global/cfs/cdirs/desi/survey/ca
     import desimodel.focalplane
     import desimodel.footprint
     import LSS.common_tools as common
-    common.printlog('making random target files for '+str(len(tiles))+' tiles',logger)
     trad = desimodel.focalplane.get_tile_radius_deg()*1.1 #make 10% greater just in case
     #print(trad)
 
