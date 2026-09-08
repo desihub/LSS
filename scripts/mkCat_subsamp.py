f32413abf2974aef05868168ca63e86f78b7216a
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-08 12:11:43 -0400

    Update mkCat_subsamp.py

--- scripts/mkCat_subsamp.py
+++ scripts/mkCat_subsamp.py
@@ -292,7 +292,7 @@ if args.compmd == 'altmtl':
     weightileloc = False
 if mkclusdat:
     common.printlog('about to start mkclusdat',logger)
-    ct.mkclusdat(args.outdir+'/'+tracer_out,weightileloc,tp=tracer_out,dchi2=dchi2,zmin=zmin,zmax=zmax,use_map_veto=args.use_map_veto,logger=logger)
+    ct.mkclusdat(args.outdir+'/'+tracer_out,weighttileloc=weightileloc,tp=tracer_out,dchi2=dchi2,zmin=zmin,zmax=zmax,use_map_veto=args.use_map_veto,logger=logger)
 
 #make clustering catalogs for randoms
 nzcompmd = 'ran'
