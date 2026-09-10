ef37644d8086374a954ba158596cbb380821324c
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-10 14:26:12 -0400

    Update cattools.py

--- py/LSS/main/cattools.py
+++ py/LSS/main/cattools.py
@@ -3495,8 +3495,16 @@ def mkfulldat(zf,imbits,ftar,tp,bit,outf,ftiles,mode1b=0,maxp=3400,azf='',azfm='
         if tp == 'LGE':
             prog = 'dark1b'
         specdat = fitsio.read(dailydir+'datcomb_'+prog+'_spec_zdone.fits',columns=spec_cols)
+        if mode1b == 2:
+            logger.info('adding 1b spec info')
+            fs1b = fitsio.read(specf1b,columns=spec_cols)
+            fs1b = fs1b[[b for b in list(specdat.dtype.names)]] #need same columns in same order before concatenating
+            specdat = np.concatenate([specdat,fs1b])
+            del fs1b
+        
         dz = join(dz,specdat,keys=['TARGETID','TILEID','LOCATION'],join_type='left')
-    
+        logger.info('joined specdat')
+        del specdat
     if len(imbits) > 0:
         dz = common.cutphotmask(dz,imbits,logger=logger)
         if logger is not None:
