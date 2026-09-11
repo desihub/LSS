29655a9e089cf62f0be54466c85d7bd3479ebb01
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-11 12:23:43 -0400

    Update holi_bgs_bmask_DA3DA2_lss_pipeline_run1.sh

--- scripts/mock_tools/holi_bgs_bmask_DA3DA2_lss_pipeline_run1.sh
+++ scripts/mock_tools/holi_bgs_bmask_DA3DA2_lss_pipeline_run1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /global/common/software/desi/desi_environment.sh main
-module load LSS/main
+#module load LSS/main
 export LSSCODE=$LSS
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 mocknum=$1
@@ -8,7 +8,7 @@ scriptdir=/global/homes/d/desica/LSScode/LSS/scripts
 sim=holi_bgs_bmask
 survey=DA3
 surveycat=DA2 #this will make it process the DR2 footprint
-#PYTHONPATH=/global/homes/d/desica/LSScode/LSS/py:$PYTHONPATH
+PYTHONPATH=/global/homes/d/desica/LSScode/LSS/py:$PYTHONPATH
 #test
 
 python $scriptdir/mock_tools/mkCat_amtl.py --base_altmtl_dir $SCRATCH --simName $sim --mocknum $mocknum --survey $survey --surveycat $surveycat --add_gtl y --specdata loa-v1 --tracer bright --targDir $SCRATCH/$survey/mocks/$sim --combd y --joindspec y --par y --usepota y
