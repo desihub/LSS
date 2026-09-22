e265f9ab423fe9b6f1e09275fe8385cd5437e09a
ashleyjross <ashley.jacob.ross@gmail.com>
2026-09-22 19:02:50 -0400

    Update pkBGS_BRIGHT-02kiboloa.sh

--- scripts/mock_tools/pkBGS_BRIGHT-02kiboloa.sh
+++ scripts/mock_tools/pkBGS_BRIGHT-02kiboloa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /global/common/software/desi/desi_environment.sh main
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
 for ((i=$1;i<=$2;i++ ))
 do
